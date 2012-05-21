997403bdb8df66e56d17883eab96ba1c2adc9758
SethKaufman <seth@collectiveaccess.org>
2012-05-21 19:30:02 -0400

    HSP fixes

--- themes/hsp/views/About/faq.php
+++ themes/hsp/views/About/faq.php
@@ -2,16 +2,22 @@
 <div class="textContent">
 	<div>
 		<b><i>How do I purchase an image I see in the Digital Library?</i></b><br/>
-		Digital images, archival prints, and permission to reproduce, publish, exhibit, or distribute these materials are available, for a fee, through our Rights and Reproductions department.  To purchase images, please <?php print caNavLink($this->request, "Login/Register", "", "", "LoginReg", "form"); ?> with a new or existing Digital Library account.  Once you are logged in, add images to your gallery, check off the requested services for each image, and click the “Purchase or Inquire about Items” link to submit your order.  
+		Digital images, archival prints, and permission to reproduce, publish, exhibit, or distribute these materials are available, for a fee, through our Rights and Reproductions department.  To purchase images, please <?php print caNavLink($this->request, "Login/Register", "", "", "LoginReg", "form"); ?> with a new or existing Digital Library account.  Once you are logged in, add images to your gallery, check off the requested services for each image, and click the “Purchase or Inquire about Items” link to submit your order.
+		<A href="#messagingvideo">This video demonstration</a> provides an example of how the communication process for most orders and queries work with the digital library.  
 	</div>
 	<div>
 		<b><i>How do I create a Digital Library account?</i></b><br/>
-		To create image galleries, send and receive messages from the R&R Associate, and purchase reproductions, you must have a Digital Library account.  To create an account, click on the “<?php print caNavLink($this->request, "Login/Register", "", "", "LoginReg", "form"); ?>” link at the top of the page.  You will be prompted to enter your name and e-mail address and create a password.  Once you have an account, you can create image galleries and save images using the “Save Images to Gallery” link.  
+		To create image galleries, send and receive messages from the R&R Associate, and purchase reproductions, you must have a Digital Library account.  To create an account, click on the “<?php print caNavLink($this->request, "Login/Register", "", "", "LoginReg", "form"); ?>” link at the top of the page.  You will be prompted to enter your name and e-mail address and create a password.  Once you have an account, you can create image galleries and save images using the “Save Images to Gallery” link.<p>
+  		You can also watch <A HREF="#accountcreatevideo">this brief video tutorial</a> to assist you in creating an account.
+	</div>
+	<div>
+		<b><i>How can I find and view materials in the Digital Library?</i></b></br>
+		These two videos will help show you how to locate and use images within the Digital Libraray.  <A HREF="#imagesearchvideo">The first video teaches how to use search to find images to add to your gallery</a>. Please watch <A href="#imageviewervideo">this short video for a tutorial on using the image viewer and its functions</a>.
 	</div>
 	<div>
 		<b><i>How do I use my gallery?</i></b><br/>
-		When viewing an image record in the Digital Library, click the “Save Images to Gallery” link underneath an image to add it to one of your image galleries.  For records with multiple images, you can select specific images by clicking the “change” link once an image is added to your gallery.  When you click the “change” link, an image viewer will appear so you can select the images you are interested in via checkboxes on the right-hand side of the screen.  Once you have made your selections, click the X on the upper right-hand side of the screen to close the viewer.  Your selections are automatically saved as you make them, so no need to worry about losing your selections.
-
+		When viewing an image record in the Digital Library, click the “Save Images to Gallery” link underneath an image to add it to one of your image galleries.  For records with multiple images, you can select specific images by clicking the “change” link once an image is added to your gallery.  When you click the “change” link, an image viewer will appear so you can select the images you are interested in via checkboxes on the right-hand side of the screen.  Once you have made your selections, click the X on the upper right-hand side of the screen to close the viewer.  Your selections are automatically saved as you make them, so no need to worry about losing your selections.<p>
+		<A HREF="#imagesearchvideo">This video can also help show you how to use search and find images to add to your gallery</a>.
 	</div>
 	<div>
 		<b><i>Why are prices listed for some services and not for others?</i></b><br/>
@@ -31,8 +37,9 @@
 	</div>	
 	<div>
 		<b><i>What if I can’t find what I’m looking for?</i></b><br/>
-		The images in our Digital Library are a small fraction of HSP’s vast manuscript and graphics collections, many of which are not yet digitized.  To purchase materials that do not appear in the Digital Library, <?php print caNavLink($this->request, "Login/Register", "", "", "LoginReg", "form"); ?> with a new or existing Digital Library account and create a new gallery to message our R&R Associate.   
-		<br/><br/>For additional information about HSP’s collections, check out our online catalog, finding aids, and other research tools at <a href="http://hsp.org/collections/catalogs-research-tools">http://hsp.org/collections/catalogs-research-tools</a>.
+		The images in our Digital Library are a small fraction of HSP’s vast manuscript and graphics collections, many of which are not yet digitized.  To purchase materials that do not appear in the Digital Library, <?php print caNavLink($this->request, "Login/Register", "", "", "LoginReg", "form"); ?> with a new or existing Digital Library account and create a new gallery to message our R&R Associate.
+		<A href="#messagingvideo">This video demonstration</a> provides an example of how the communication process for most orders and queries work with the digital library.  
+			<br/><br/>For additional information about HSP’s collections, check out our online catalog, finding aids, and other research tools at <a href="http://hsp.org/collections/catalogs-research-tools">http://hsp.org/collections/catalogs-research-tools</a>.
 	</div>
 	<div>
 		<b><i>How do I get photocopies or research assistance?</i></b><br/>
@@ -41,4 +48,9 @@
 	<div>
 		<b><i>For additional information about our Rights and Reproductions services, visit <a href="http://hsp.org/collections/library-services/rights-and-reproductions">http://hsp.org/collections/library-services/rights-and-reproductions</a>.
 	</div>
-		
+	<div>
+		<b><i>Video tutorials for using HSP's Digital Library</i></b><br/>
+		<A NAME="accountcreatevideo">How to Create an Account Video</a><br/><iframe width="420" height="315" src="http://www.youtube.com/embed/4Wl3xNpY6do" frameborder="0" allowfullscreen></iframe></p>
+		<A name="imagesearchvideo">Video on how to search for images</a><br/><iframe width="420" height="315" src="http://www.youtube.com/embed/4rHuVb1Wpmc" frameborder="0" allowfullscreen></iframe><p>
+		<A name="imageviewervideo">Video on how to use the image viewer</a><br/><iframe width="420" height="315" src="http://www.youtube.com/embed/WK_-z0eLLyc" frameborder="0" allowfullscreen></iframe><p>
+		<A name="messagingvideo">Video on how to send messages and communicate</a><br/><iframe width="420" height="315" src="http://www.youtube.com/embed/RlXRMpT8Hq8" frameborder="0" allowfullscreen></iframe><p>
