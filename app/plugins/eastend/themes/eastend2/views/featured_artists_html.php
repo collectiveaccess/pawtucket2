9191825dc385dcbab378670395a9d54893a5fd3a
mariamotorina <maria@whirl-i-gig.com>
2012-10-26 20:36:37 -0400

    AOEE - many changes prelaunch/ prestorm commit

--- app/plugins/eastend/themes/eastend2/views/featured_artists_html.php
+++ app/plugins/eastend/themes/eastend2/views/featured_artists_html.php
@@ -4,13 +4,30 @@
 	if(is_array($va_featured_artists) && (sizeof($va_featured_artists) > 0)){
 		print "<div id='featuredArtistSlideShow'>";
 		foreach($va_featured_artists as $vn_entity_id => $va_featured_artist){
-			print "<div class='ab_feature'>";
+			print "<div class='ab_feature".(($va_featured_artist["vaga_class"] ? " ".$va_featured_artist["vaga_class"] : ""))."'>";
 			print "<div class='ab_feature_img'>".caNavLink($this->request, $va_featured_artist["image"], '', 'Detail', 'Entity', 'Show', array('entity_id' => $vn_entity_id))."</div>";
 			print "<p><span class='caption caps'>".caNavLink($this->request, $va_featured_artist["name"], '', 'Detail', 'Entity', 'Show', array('entity_id' => $vn_entity_id))." ".$va_featured_artist["lifespan"]."</span><br />";
-			print "<span class='captionSmall obl'>".$va_featured_artist["indexing_notes"]."</span>";
+			print "<span class='captionSmall'>".$va_featured_artist["indexing_notes"]."</span>";
+			if($va_featured_artist["caption"]){
+				print "<br/><br/><span class='captionSmall obl'>";
+				if($va_featured_artist["vaga_class"]){
+					print "<a href='http://www.vagarights.com' target='_blank'>";
+					$vn_vaga_disclaimer_output = 1;
+				}
+				print $va_featured_artist["caption"];
+				if($va_featured_artist["vaga_class"]){
+					print "</a>";
+				}
+				print "</span>";
+			}
 			print "<br /></p></div><!--end ab_feature-->";
 		}
 		print "</div><!-- featuredArtistSlideShow -->";
+	}
+	if($vn_vaga_disclaimer_output){
+		TooltipManager::add(
+			".vagaDisclaimer", "<div style='width:250px;'>Reproduction of this image, including downloading, is prohibited without written authorization from VAGA, 350 Fifth Avenue, Suite 2820, New York, NY 10118. Tel: 212-736-6666; Fax: 212-736-6767; e-mail:info@vagarights.com; web: <a href='www.vagarights.com' target='_blank'>www.vagarights.com</a></div>"
+		);
 	}
 ?>
 
--- themes/eastend2/views/Splash/splash_html.php
+++ themes/eastend2/views/Splash/splash_html.php
@@ -31,13 +31,35 @@
 	$va_item_ids = $this->getVar('featured_content_slideshow_id_list');
 	$va_item_media = $t_object->getPrimaryMediaForIDs($va_item_ids, array("hpFeatured"));
 	$va_item_labels = $t_object->getPreferredDisplayLabelsForIDs($va_item_ids);
+	$qr_hits = ca_objects::createResultSet($va_item_ids);
+	$va_item_captions = array();
+	$va_item_vaga = array();
+	$va_item_do_not_show = array();
+	if($qr_hits->numHits()){
+		while($qr_hits->nextHit()){
+			# --- vaga/ARS no image - remove from slideshow
+			if($qr_hits->get("ca_objects.object_status") == 348){
+				$va_item_do_not_show[$qr_hits->get("ca_objects.object_id")] = 1;
+			}
+			$vs_caption = "";
+			if($qr_hits->get("ca_objects.object_status") == 349){
+				$vs_caption = "<a href='http://www.vagarights.com' target='_blank'>".$qr_hits->get("ca_objects.caption")."</a>";
+				$va_item_vaga[$qr_hits->get("ca_objects.object_id")] = 1;
+			}else{
+				$vs_caption = $qr_hits->get("ca_objects.caption");
+			}
+			$va_item_captions[$qr_hits->get("ca_objects.object_id")] = $vs_caption;
+		}
+	}
  ?>
 	<div id="hpFeatured">
 <?php
 	foreach ($va_item_media as $vn_object_id => $va_media) {
-		$vs_image_tag = $va_media["tags"]["hpFeatured"];
-		print "<div><div id='hpFeaturedImg'>".caNavLink($this->request, $vs_image_tag, '', 'Detail', 'Object', 'Show', array('object_id' => $vn_object_id))."</div>";
-		print "<div id='hpFeaturedScrollCaption'>".caNavLink($this->request, $va_item_labels[$vn_object_id], '', 'Detail', 'Object', 'Show', array('object_id' => $vn_object_id))."</div></div>";
+		if(!$va_item_do_not_show[$vn_object_id]){
+			$vs_image_tag = $va_media["tags"]["hpFeatured"];
+			print "<div><div id='hpFeaturedImg'".(($va_item_vaga[$vn_object_id]) ? " class='vagaDisclaimer'" : "").">".caNavLink($this->request, $vs_image_tag, '', 'Detail', 'Object', 'Show', array('object_id' => $vn_object_id))."</div>";
+			print "<div id='hpFeaturedScrollCaption'".(($va_item_vaga[$vn_object_id]) ? " class='vagaDisclaimer'" : "").">".caNavLink($this->request, $va_item_labels[$vn_object_id].(($va_item_captions[$vn_object_id]) ? "<br/>".$va_item_captions[$vn_object_id] : ""), '', 'Detail', 'Object', 'Show', array('object_id' => $vn_object_id))."</div></div>";
+		}
 	}
 ?>
 	</div>
@@ -46,7 +68,7 @@
 			East End Stories, presented by the Parrish Art Museum, explores the enduring presence of artists on the East End of Long Island.
 		</div>
 		<div id="hpText">
-			Begin your journey by browsing the catalog of artists, finding hubs of activity on the map, or exploring the chronology.
+			Begin your journey by browsing the catalog of artists<!--, finding hubs of activity on the map, --> or exploring the chronology.
 		</div>			
 	</div>
 <script type="text/javascript">
