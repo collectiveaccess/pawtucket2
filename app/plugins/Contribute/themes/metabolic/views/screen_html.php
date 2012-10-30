f32d4ac4c2173adfc5c52a4992774c20f3989580
Whirl-I-Gig Admins <whirligig@alicia.metabolicstudio.org>
2012-10-30 08:14:22 -0700

    Metabolic updates

--- app/plugins/Contribute/themes/metabolic/views/screen_html.php
+++ app/plugins/Contribute/themes/metabolic/views/screen_html.php
@@ -100,4 +100,12 @@
 				}
 			});
 		});
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+}); 
 	</script>
