d8e585d2dcdc4c13a264a73f99a10c1861c5d2e3
sophie <sophie@whirl-i-gig.com>
2012-11-29 12:22:07 -0500

    theme changes

---
+++ app/plugins/educational/views/ns11mm/intro_text_html.php
@@ -0,0 +1,6 @@
+<div style="font-size:15px; line-height:1.4em;">
+	Each gallery below consists of a set of related artifacts accompanied by a series of questions. Together they offer a way to explore 9/11 and its aftermath through themes grounded in objects from our collection. 
+</div>
+<div>	
+	Click an image below to get started.
+</div>
