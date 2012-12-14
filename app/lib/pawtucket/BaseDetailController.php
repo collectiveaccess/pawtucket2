f574e5d84cd131ef93f4758a66d7ee3c321abb35
mariamotorina <maria@whirl-i-gig.com>
2012-12-14 12:17:41 -0500

    revision to PDF summary download

--- app/lib/pawtucket/BaseDetailController.php
+++ app/lib/pawtucket/BaseDetailController.php
@@ -574,7 +574,7 @@
 			$vs_output_filename = mb_substr($vs_output_filename, 0, 30);
 
 			require_once(__CA_LIB_DIR__.'/core/Parsers/dompdf/dompdf_config.inc.php');
-			$vs_output_file_name = preg_replace("/[^A-Za-z0-9\-]+/", '_', $vs_output_filename);
+			$vs_output_file_name = mb_substr(preg_replace("/[^A-Za-z0-9\-]+/", '_', $vs_output_filename), 0, 40);
 			header("Content-Disposition: attachment; filename=".$vs_output_file_name.".pdf");
 			header("Content-type: application/pdf");
 			$vs_content = $this->render('downloadTemplates/'.$this->ops_tablename.'_pdf_html.php');
