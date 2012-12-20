82b4d74d47f490bba9abcfd2a57383fcb18ab40d
Seth <seth@whirl-i-gig.com>
2012-12-20 10:11:51 -0500

    Sophie's plugin tweaks

--- app/plugins/educational/views/ns11mm/intro_text_html.php
+++ app/plugins/educational/views/ns11mm/intro_text_html.php
@@ -1,6 +1,3 @@
 <div style="font-size:15px; line-height:1.4em;">
-	Each gallery below consists of a set of related artifacts accompanied by a series of questions. Together they offer a way to explore 9/11 and its aftermath through themes grounded in objects from our collection. 
-</div>
-<div>	
-	Click an image below to get started.
-</div>
+	Each gallery has a set of related artifacts accompanied by a series of questions. They offer a way to explore 9/11 and its aftermath through themes inspired by objects from the museum collection.  Enlarge images by clicking them. 
+</div> 
