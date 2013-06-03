468e56180bbaf5b60cdb2f6c18d2411ba7a14dcf
Seth <seth@whirl-i-gig.com>
2013-06-03 08:22:50 -0400

    Remove D3; add Providence files

--- js/d3/src/core/transition-duration.js
+++
@@ -1,6 +0,0 @@
-d3_transitionPrototype.duration = function(value) {
-  var groups = this;
-  return groups.each(typeof value === "function"
-      ? function(d, i, j) { groups[j][i].duration = Math.max(1, value.apply(this, arguments) | 0); }
-      : (value = Math.max(1, value | 0), function(d, i, j) { groups[j][i].duration = value; }));
-};
