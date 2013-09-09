ed57e4189708aa89c31328dc73cf356cb17c4461
Seth <seth@whirl-i-gig.com>
2013-09-09 11:28:43 -0400

    NOJIRA basic template directories

---
+++ themes/default/views/pageFormat/pageFooter.php
@@ -0,0 +1,9 @@
+		<div style="clear:both; height:1px;"><!-- empty --></div></div><!-- end pageArea -->
+		<div id="footer">
+			[<?php print $this->request->session->elapsedTime(4).'s'; ?>/<?php print caGetMemoryUsage(); ?>]
+		</div><!-- end footer -->
+<?php
+	print TooltipManager::getLoadHTML();
+?>
+	</body>
+</html>
