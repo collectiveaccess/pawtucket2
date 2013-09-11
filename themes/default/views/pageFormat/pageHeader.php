3fc2dcb977909314be4789885dfdaa8a9fa1a164
Seth <seth@whirl-i-gig.com>
2013-09-11 00:43:50 -0400

    NOJIRA shell (barely) templates

--- themes/default/views/pageFormat/pageHeader.php
+++ themes/default/views/pageFormat/pageHeader.php
@@ -5,8 +5,47 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
 
 	<?php print MetaTagManager::getHTML(); ?>
-	<?php print JavascriptLoadManager::getLoadHTML($this->request->getBaseUrlPath()); ?>
+	<?php print AssetLoadManager::getLoadHTML($this->request->getBaseUrlPath()); ?>
 
 	<title><?php print $this->request->config->get('html_page_title'); ?></title>
 </head>
 <body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?php print $this->request->getBaseUrlPath(); ?>/">Pawtucket</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form pull-right" action="<?php print caNavUrl($this->request, '', 'Search', 'Index'); ?>">
+			<div class="input-group">
+				<input type="search" class="form-control" placeholder="Search" name="search"/>
+				<span class="input-group-btn">
+					<button type="submit" class="btn btn-default">Search</button>
+				</span> 
+			</div>
+		</form>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
