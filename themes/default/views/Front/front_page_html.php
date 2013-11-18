41a0f4b6b7c0e613507cd3ffd3617dab3235331a
Maria <maria@whirl-i-gig.com>
2013-11-18 17:10:52 -0500

    a simple homepage placeholder

--- themes/default/views/Front/front_page_html.php
+++ themes/default/views/Front/front_page_html.php
@@ -30,4 +30,46 @@
  * ----------------------------------------------------------------------
  */
 ?>
-home page
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <div style="width:1024px; height:300px; background-color:#828282;"></div>
+           <div class="container">
+            <div class="carousel-caption">
+              <h1>Example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+             </div>
+          </div>
+        </div>
+        <div class="item">
+          <div style="width:1024px; height:300px; background-color:#828282;"></div>
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+             </div>
+          </div>
+        </div>
+        <div class="item">
+          <div style="width:1024px; height:300px; background-color:#828282;"></div>
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div><!-- /.carousel -->
+    <H1>Welcome</H1>
+    <p>
+    	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate, orci quis vehicula eleifend, metus elit laoreet elit, vitae pretium orci purus venenatis orci. Nullam lectus ante, pellentesque luctus commodo rutrum, imperdiet et nunc. Nulla lacus turpis, commodo sit amet faucibus ac, sodales sed urna. Vestibulum porta pharetra feugiat. Donec tempor leo at elit pulvinar vehicula. Aliquam eu rhoncus mauris, ullamcorper malesuada est. Nullam porttitor est neque, ac ornare turpis condimentum ut. Cras id mollis diam. Curabitur et ante risus. Aliquam molestie metus eu lorem sagittis feugiat. Proin vel urna vel turpis iaculis pulvinar vel a mi. Phasellus aliquet lectus viverra condimentum eleifend. Integer aliquam molestie pharetra. Etiam non mollis arcu. Sed massa sem, auctor vulputate enim rhoncus, faucibus varius orci. 
+    </p>
