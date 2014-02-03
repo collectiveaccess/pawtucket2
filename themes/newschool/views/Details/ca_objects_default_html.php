7733ec9c2ad6e031e2a2e2e9e314903ec6d6fd0b
collectiveaccess <collectiveaccess@digitalarchives.library.newschool.edu>
2014-02-03 01:09:14 -0500

    NOJIRA NewSchool-originated mods

--- themes/newschool/views/Details/ca_objects_default_html.php
+++ themes/newschool/views/Details/ca_objects_default_html.php
@@ -26,20 +26,41 @@
 					<H1>{{{<unit relativeTo="ca_collections" delimiter="<br/>">^ca_collections.hierarchy.preferred_labels.name%returnAsLink=1%delimiter=_➔_</unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</H1>
 					<H2>{{{<unit>^ca_objects.type_id</unit>}}}</H2>
 					<HR>
+					{{{<ifdef code="ca_objects.dateSet.setDisplayValue"><H3>Date:</H3>^ca_objects.dateSet.setDisplayValue<br/></ifdev>}}}
+					{{{<ifdef code="ca_objects.wtDrawings"><H3>Work Type:</H3>^ca_objects.wtDrawings<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtPhotographic"><H3>Work Type:</H3>^ca_objects.wtPhotographic<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtPosters"><H3>Work Type:</H3>^ca_objects.wtPosters<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtClippings"><H3>Work Type:</H3>^ca_objects.wtClippings<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtPlans"><H3>Work Type:</H3>^ca_objects.wtPlans<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtCourse"><H3>Work Type:</H3>^ca_objects.wtCourse<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtExhibition"><H3>Work Type:</H3>^ca_objects.wtExhibition<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtBooks"><H3>Work Type:</H3>^ca_objects.wtBooks<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtPeriodicals"><H3>Work Type:</H3>^ca_objects.wtPeriodicals<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtScores "><H3>Work Type:</H3>^ca_objects.wtScores<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtScrapbooks"><H3>Work Type:</H3>^ca_objects.wtScrapbooks<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtPhotoAlbums"><H3>Work Type:</H3>^ca_objects.wtPhotoAlbums<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtSketchbooks"><H3>Work Type:</H3>^ca_objects.wtSketchbooks<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtTextual"><H3>Work Type:</H3>^ca_objects.wtTextual<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtProofs"><H3>Work Type:</H3>^ca_objects.wtProofs<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtThree"><H3>Work Type:</H3>^ca_objects.wtThree<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.wtOther"><H3>Work Type:</H3>^ca_objects.wtOther<br/></ifdef>}}}
 					
-					{{{<ifdef code="ca_objects.measurementSet.measurements">^ca_objects.measurementSet.measurements (^ca_objects.measurementSet.measurementsType)</ifdef><ifdef code="ca_objects.measurementSet.measurements,ca_objects.measurementSet.measurements"> x </ifdef><ifdef code="ca_objects.measurementSet.measurements2">^ca_objects.measurementSet.measurements2 (^ca_objects.measurementSet.measurementsType2)</ifdef>}}}
 					
 					
-					{{{<ifdef code="ca_objects.idno"><H3>Identifer:</H3>^ca_objects.idno<br/></ifdef>}}}
-					{{{<ifdef code="ca_objects.containerID"><H3>Box/series:</H3>^ca_objects.containerID<br/></ifdef>}}}
+					
+					{{{<ifdef code="ca_objects.measurementSet.measurements"><H3>Measurements:</H3>^ca_objects.measurementSet.measurements (^ca_objects.measurementSet.measurementsType)</ifdef><ifdef code="ca_objects.measurementSet.measurements,ca_objects.measurementSet.measurements"> x </ifdef><ifdef code="ca_objects.measurementSet.measurements2">^ca_objects.measurementSet.measurements2 (^ca_objects.measurementSet.measurementsType2)</ifdef>}}}
+					
+					
+					{{{<ifdef code="ca_objects.idno"><H3>Identifier:</H3>^ca_objects.idno<br/></ifdef>}}}
+					{{{<ifdef code="ca_objects.containerID"><H3>Location:</H3>^ca_objects.containerID<br/></ifdef>}}}
 					
 					{{{<ifdef code="ca_objects.description">^ca_objects.description<br/></ifdef>}}}
 					
 					
-					{{{<ifdef code="ca_objects.dateSet.setDisplayValue"><H3>Date:</H3>^ca_objects.dateSet.setDisplayValue<br/></ifdev>}}}
 					
-					{{{<ifcount code="ca_entities" min="1" max="1"><h3>Related person</h3></ifcount>}}}
-					{{{<ifcount code="ca_entities" min="2"><h3>Related people</h3></ifcount>}}}
+					
+					{{{<ifcount code="ca_entities" min="1" max="1"><h3>Related person or organization</h3></ifcount>}}}
+					{{{<ifcount code="ca_entities" min="2"><h3>Related people and organizations</h3></ifcount>}}}
 					{{{<unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit><br/><br/>}}}
 					
 					
