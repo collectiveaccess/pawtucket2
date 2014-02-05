26f78b047ecbd817cb645a216db86148cf4d7d88
pinkfishies <sophie@whirl-i-gig.com>
2014-02-05 16:51:26 -0500

    NOJIRA Fix multisearch scroll load issues: 1. Sometimes loads part of larger page in results track 2. Sorting doesn't work on results that fit within a single load set
    Also removed hard coded "sort with" text

--- themes/default/views/Search/ca_objects_search_subview_html.php
+++ themes/default/views/Search/ca_objects_search_subview_html.php
@@ -73,7 +73,7 @@
 						preloadCount: <?php print $vn_count; ?>,
 						itemWidth: jQuery('.{{{block}}}Result').outerWidth(true),
 						itemsPerLoad: <?php print $vn_hits_per_block; ?>,
-						itemLoadURL: '<?php print ($vb_has_more ? caNavUrl($this->request, '*', '*', '*', array('block' => $vs_block, 'search'=> $vs_search)) : ''); ?>',
+						itemLoadURL: '<?php print caNavUrl($this->request, '*', '*', '*', array('block' => $vs_block, 'search'=> $vs_search)); ?>',
 						itemContainerSelector: '.blockResultsScroller',
 						sortParameter: '{{{block}}}Sort',
 						sortControlSelector: '#{{{block}}}_sort',
