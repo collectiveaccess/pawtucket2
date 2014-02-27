bb1da0e678ff756a1b8b29a924b733c7d6db3922
Maria <maria@whirl-i-gig.com>
2014-02-27 16:19:07 -0500

    tweaks to newschool after stylesheet cleanup for pawtucket

--- themes/newschool/views/Gallery/set_item_info_html.php
+++ themes/newschool/views/Gallery/set_item_info_html.php
@@ -1,8 +1,8 @@
 <H3>{{{<unit relativeTo="ca_collections" delimiter="<br/>">^ca_collections.hierarchy.preferred_labels.name%returnAsLink=1%delimiter=_➔_</unit>}}}</H3>
-<H5>{{{ca_objects.preferred_labels.name}}}</H5>
 
 {{{<unit relativeTo="ca_collections" delimiter="<br/>"><ifdef code="ca_collections.CollectionNote.NoteContent%[NoteType=abstract]"><b>About the Collection</b>: ^ca_collections.CollectionNote.NoteContent%[NoteType=abstract]<br/><br/></ifdef></unit>}}}
 
+<H5>{{{ca_objects.preferred_labels.name}}}</H5>
 
 {{{<ifcount code="ca_entities" min="1" max="1"><b>Related person: </b></ifcount>}}}
 {{{<ifcount code="ca_entities" min="2"><b>Related people: </b></ifcount>}}}
