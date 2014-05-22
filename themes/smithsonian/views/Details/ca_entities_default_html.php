b98a0d93430ab0c6458e662593c6b5ff738adc09
pinkfishies <sophie@whirl-i-gig.com>
2014-05-22 10:18:18 -0400

    theme changes

--- themes/smithsonian/views/Details/ca_entities_default_html.php
+++ themes/smithsonian/views/Details/ca_entities_default_html.php
@@ -83,7 +83,6 @@
 			<div class='col-md-6 col-lg-6'>
 				{{{<ifcount code="ca_objects" min="1" max="1"><H6>Related object</H6><unit relativeTo="ca_objects" delimiter=" "><l>^ca_object_representations.media.small</l><br/><l>^ca_objects.preferred_labels.name</l><br/></unit></ifcount>}}}
 				
-				Map could go here, or call out related object like portrait
 				<div id="detailTools">
 					<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><span class="glyphicon glyphicon-comment"></span>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
 					<div id='detailComments'>{{{itemComments}}}</div><!-- end itemComments -->
