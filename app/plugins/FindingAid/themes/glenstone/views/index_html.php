2295103e27619a683f91e594cd159fe544ab42c7
pinkfishies <sophie@whirl-i-gig.com>
2014-06-04 12:42:38 -0400

    theme

--- app/plugins/FindingAid/themes/glenstone/views/index_html.php
+++ app/plugins/FindingAid/themes/glenstone/views/index_html.php
@@ -4,14 +4,18 @@
 	
 	$qr_top_level_collections = ca_collections::find(array('parent_id' => null), array('returnAs' => 'searchResult'));
 	
-	while($qr_top_level_collections->nextHit()) {
-		$vn_top_level_collection_id = $qr_top_level_collections->get('ca_collections.collection_id');
-		//print $qr_top_level_collections->get('ca_collections.preferred_labels.name')."<br>\n";
+	if ($qr_top_level_collections) {
+		while($qr_top_level_collections->nextHit()) {
+			$vn_top_level_collection_id = $qr_top_level_collections->get('ca_collections.collection_id');
+			//print $qr_top_level_collections->get('ca_collections.preferred_labels.name')."<br>\n";
 		
-		$va_hierarchy = $t_collection->hierarchyWithTemplate($ps_template, array('collection_id' => $vn_top_level_collection_id));
+			$va_hierarchy = $t_collection->hierarchyWithTemplate($ps_template, array('collection_id' => $vn_top_level_collection_id));
 		
-		foreach($va_hierarchy as $vn_i => $va_hierarchy_item) {
-			print "<div style='margin-left: ".($va_hierarchy_item['level'] * 25)."px'>{$va_hierarchy_item['display']}</div>\n";
+			foreach($va_hierarchy as $vn_i => $va_hierarchy_item) {
+				print "<div style='margin-left: ".($va_hierarchy_item['level'] * 25)."px'>{$va_hierarchy_item['display']}</div>\n";
+			}
 		}
+	} else {
+		print _t('No collections available');
 	}
 ?>
