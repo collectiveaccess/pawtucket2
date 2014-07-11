4093e378da76b000194798a08496e48d26588fdb
Maria <maria@whirl-i-gig.com>
2014-07-11 10:58:11 -0400

    hgsh revisions

--- themes/hgsh/views/Details/ca_objects_default_html.php
+++ themes/hgsh/views/Details/ca_objects_default_html.php
@@ -1,6 +1,7 @@
 <?php
 	$t_object = $this->getVar("item");
 	$va_comments = $this->getVar("comments");
+	$va_access_values = $this->getVar("access_values");
 ?>
 <div class="row">
 	<div class='col-xs-1 col-sm-1 col-md-1 col-lg-1'>
@@ -12,6 +13,7 @@
 		<div class="container"><div class="row">
 			<div class='col-sm-6 col-md-6 col-lg-6'>
 				{{{representationViewer}}}
+				<?php print "<p style='clear:both;'>".caObjectRepresentationThumbnails($this->request, $this->getVar("representation_id"), $t_object, array("returnAs" => "bsCols", "linkTo" => "carousel"))."</p>"; ?>	
 				<div id="detailTools">
 					<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><span class="glyphicon glyphicon-comment"></span>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
 					<div id='detailComments'>{{{itemComments}}}</div><!-- end itemComments -->
@@ -31,24 +33,111 @@
 				
 				{{{<ifdef code="ca_objects.description">
 					<span class="trimText">^ca_objects.description</span>
-				</ifdef>}}}
-				<?php print "<p>".caObjectRepresentationThumbnails($this->request, $this->getVar("representation_id"), $t_object, array("returnAs" => "bsCols", "linkTo" => "carousel"))."</p>"; ?>
-				
-					{{{<ifcount code="ca_collections" min="1" max="1"><H6>Related collection</H6></ifcount>}}}
-					{{{<ifcount code="ca_collections" min="2"><H6>Related collections</H6></ifcount>}}}
-					{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.preferred_labels.name</l></unit><br/><br/>}}}
+				</ifdef>}}}			
+<?php
+					$t_object_thumb = new ca_objects();
+					$va_entities = $t_object->get("ca_entities", array("returnAsArray" => true, "checkAccess" => $va_access_values));
+					if(sizeof($va_entities)){
+						if(sizeof($va_entities) == 1){
+							print "<H6>Related person/organisation</H6>";
+						}else{
+							print "<H6>Related people/organisations</H6>";
+						}
+						$t_rel_entity = new ca_entities();
+						foreach($va_entities as $va_entity){
+							$t_rel_entity->load($va_entity["entity_id"]);
+							$t_object_thumb->load($t_rel_entity->get("ca_objects.object_id", array("restrictToRelationshipTypes" => array("cover"), "checkAccess" => $va_access_values)));
+							$vs_thumb = $t_object_thumb->get("ca_object_representations.media.icon", array("checkAccess" => $va_access_values));
+							if($vs_thumb){
+								print "<div class='row'><div class='col-sm-3 col-md-3 col-lg-3'>".$vs_thumb."</div>\n";
+								print "<div class='col-sm-9 col-md-9 col-lg-9'>\n";
+							}
+							print $t_rel_entity->getWithTemplate("<b><l>^ca_entities.preferred_labels.displayname</l></b>");
+							if($vs_brief_description = $t_rel_entity->get("ca_entities.brief_description")){
+								print "<br/>".$vs_brief_description;
+							}
+							if($vs_thumb){
+								print "</div></div><!-- end row -->";
+							}else{
+								print "<br/>";
+							}
+							print "<br/>";
+						}
+					}
+					$va_collections = $t_object->get("ca_collections", array("returnAsArray" => true, "checkAccess" => $va_access_values));
+					if(sizeof($va_collections)){
+						print "<H6>Related collection".((sizeof($va_collections) > 1) ? "s" : "")."</H6>";
+						$t_rel_collection = new ca_collections();
+						foreach($va_collections as $va_collection){
+							$t_rel_collection->load($va_collection["collection_id"]);
+							$t_object_thumb->load($t_rel_collection->get("ca_objects.object_id", array("restrictToRelationshipTypes" => array("cover"), "checkAccess" => $va_access_values)));
+							$vs_thumb = $t_object_thumb->get("ca_object_representations.media.icon", array("checkAccess" => $va_access_values));
+							if($vs_thumb){
+								print "<div class='row'><div class='col-sm-3 col-md-3 col-lg-3'>".$vs_thumb."</div>\n";
+								print "<div class='col-sm-9 col-md-9 col-lg-9'>\n";
+							}
+							print $t_rel_collection->getWithTemplate("<b><l>^ca_collections.preferred_labels.name</l></b>");
+							if($vs_brief_description = $t_rel_collection->get("ca_collections.brief_description")){
+								print "<br/>".$vs_brief_description;
+							}
+							if($vs_thumb){
+								print "</div></div><!-- end row -->";
+							}else{
+								print "<br/>";
+							}
+							print "<br/>";
+						}
+					}
+					$va_places = $t_object->get("ca_places", array("returnAsArray" => true, "checkAccess" => $va_access_values));
+					if(sizeof($va_places)){
+						print "<H6>Related place".((sizeof($va_places) > 1) ? "s" : "")."</H6>";
+						$t_rel_place = new ca_places();
+						foreach($va_places as $va_place){
+							$t_rel_place->load($va_place["place_id"]);
+							$t_object_thumb->load($t_rel_place->get("ca_objects.object_id", array("restrictToRelationshipTypes" => array("cover"), "checkAccess" => $va_access_values)));
+							$vs_thumb = $t_object_thumb->get("ca_object_representations.media.icon", array("checkAccess" => $va_access_values));
+							if($vs_thumb){
+								print "<div class='row'><div class='col-sm-3 col-md-3 col-lg-3'>".$vs_thumb."</div>\n";
+								print "<div class='col-sm-9 col-md-9 col-lg-9'>\n";
+							}
+							print $t_rel_place->getWithTemplate("<b><l>^ca_places.preferred_labels.name</l></b>");
+							if($vs_brief_description = $t_rel_place->get("ca_places.brief_description")){
+								print "<br/>".$vs_brief_description;
+							}
+							if($vs_thumb){
+								print "</div></div><!-- end row -->";
+							}else{
+								print "<br/>";
+							}
+							print "<br/>";
+						}
+					}
+					$va_occurrences = $t_object->get("ca_occurrences", array("returnAsArray" => true, "checkAccess" => $va_access_values));
+					if(sizeof($va_occurrences)){
+						print "<H6>Related event".((sizeof($va_occurrences) > 1) ? "s" : "")."</H6>";
+						$t_rel_occurrence = new ca_occurrences();
+						foreach($va_occurrences as $va_occurrence){
+							$t_rel_occurrence->load($va_occurrence["occurrence_id"]);
+							$t_object_thumb->load($t_rel_occurrence->get("ca_objects.object_id", array("restrictToRelationshipTypes" => array("cover"), "checkAccess" => $va_access_values)));
+							$vs_thumb = $t_object_thumb->get("ca_object_representations.media.icon", array("checkAccess" => $va_access_values));
+							if($vs_thumb){
+								print "<div class='row'><div class='col-sm-3 col-md-3 col-lg-3'>".$vs_thumb."</div>\n";
+								print "<div class='col-sm-9 col-md-9 col-lg-9'>\n";
+							}
+							print $t_rel_occurrence->getWithTemplate("<b><l>^ca_occurrences.preferred_labels.name</l></b>");
+							if($vs_brief_description = $t_rel_occurrence->get("ca_occurrences.brief_description")){
+								print "<br/>".$vs_brief_description;
+							}
+							if($vs_thumb){
+								print "</div></div><!-- end row -->";
+							}else{
+								print "<br/>";
+							}
+							print "<br/>";
+						}
+					}
 					
-					{{{<ifcount code="ca_entities" min="1" max="1"><H6>Related person/organisation</H6></ifcount>}}}
-					{{{<ifcount code="ca_entities" min="2"><H6>Related people/organisations</H6></ifcount>}}}
-					{{{<unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit><br/><br/>}}}
-					
-					{{{<ifcount code="ca_places" min="1" max="1"><H6>Related place</H6></ifcount>}}}
-					{{{<ifcount code="ca_places" min="2"><H6>Related places</H6></ifcount>}}}
-					{{{<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit><br/><br/>}}}
-					
-					{{{<ifcount code="ca_occurrences" min="1" max="1"><H6>Related event</H6></ifcount>}}}
-					{{{<ifcount code="ca_occurrences" min="2"><H6>Related events</H6></ifcount>}}}
-					{{{<unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_occurrences.preferred_labels.name</l></unit><br/><br/>}}}					
+?>				
 			</div><!-- end col -->
 		</div><!-- end row --></div><!-- end container -->
 	</div><!-- end col -->
