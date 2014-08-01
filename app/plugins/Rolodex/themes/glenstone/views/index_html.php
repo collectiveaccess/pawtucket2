90a6f0612cc0c84bc9337c30220568f726038e0b
pinkfishies <sophie@whirl-i-gig.com>
2014-08-01 11:55:19 -0400

    rolodex changes

--- app/plugins/Rolodex/themes/glenstone/views/index_html.php
+++ app/plugins/Rolodex/themes/glenstone/views/index_html.php
@@ -13,9 +13,30 @@
 	print "</form>\n";
 	print "</div>";	
 	if ($o_results = $this->getVar('results')) {
+		print "<h1>Results</h1>";
+		print "<hr>";
 		while($o_results->nextHit()) {
-			print $o_results->getWithTemplate("<l>^ca_entities.preferred_labels.displayname</l><br>\n");
-			print $o_results->getWithTemplate("^ca_entities.address.city<br>\n");
+			print "<div class='row results'>";
+			print "<div class='col-sm-4'>".$o_results->getWithTemplate("<l><b>^ca_entities.preferred_labels.displayname</b></l><br/>\n");
+			if ($o_results->get("ca_entities.job_title")) {
+				print $o_results->getWithTemplate("^ca_entities.job_title<br/>");
+			}
+			print $o_results->getWithTemplate("^ca_entities.affiliation", array('delimiter' => "<br/>"));
+			print "</div><!-- end col -->";
+			print "<div class='col-sm-4'>".$o_results->getWithTemplate("^ca_entities.address.address1<br>\n");
+			print $o_results->getWithTemplate("^ca_entities.address.address2<br>\n");
+			if ($o_results->get("ca_entities.address.city")) {
+				print $o_results->getWithTemplate("^ca_entities.address.city, ");
+			}
+			print $o_results->getWithTemplate("^ca_entities.address.stateprovince ");
+			print $o_results->getWithTemplate("^ca_entities.address.postalcode<br/>");
+			print $o_results->getWithTemplate("^ca_entities.address.country");
+			print "</div><!-- end col -->";
+			print "<div class='col-sm-4'>".$o_results->getWithTemplate("^ca_entities.telephone.telephone2 ^ca_entities.telephone.telephone3<br/>", array('delimiter' => ""));
+			print $o_results->getWithTemplate("^ca_entities.email_address");
+			print "</div><!-- end col -->";
+			print "</div><!-- end row -->";
+			print "<hr>";
 		}
 	}
 ?>
