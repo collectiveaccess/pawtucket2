1c0196fa20d29b401e931812531bfafb58db4b14
Seth <seth@whirl-i-gig.com>
2014-08-18 21:07:41 -0400

    NOJIRA Rolodex design tweaks

--- app/plugins/Rolodex/themes/glenstone/views/index_html.php
+++ app/plugins/Rolodex/themes/glenstone/views/index_html.php
@@ -2,17 +2,20 @@
 
 	<h1>Rolodex</h1>
 <?php
-	if ($this->request->user && ($this->request->user->hasUserRole("founder") || $this->request->user->hasUserRole("collection"))){	
+	if ($this->request->user && ($this->request->user->hasUserRole("founder") || $this->request->user->hasUserRole("collection")  || $this->request->user->hasUserRole("supercurator"))){	
 ?>	
 	<h5>Find contacts using the search fields below</h5> 
 
 <?php
 	print "<div class='row'>";
-	print caFormTag($this->request, 'Index', 'caRolodexForm', null, 'post', 'multipart/form-data', '_top', array('disableUnsavedChangesWarning' => true));
+	print caFormTag($this->request, 'Index', 'caRolodexForm', null, 'post', 'multipart/form-data', '_top', array('disableUnsavedChangesWarning' => true, 'submitOnReturn' => true));
 	print "<div class='col-sm-4'><span class='formLabel'>"._t('First name')."</span>".caHTMLTextInput("forename", array())."</div>";
-	print "<div class='col-sm-4'><span class='formLabel'>"._t('Last name')."</span>".caHTMLTextInput("surname", array())."</div>";
-	print "<div class='col-sm-4'><span class='formLabel'>"._t('Company')."</span>".caHTMLTextInput("company", array());
+	print "<div class='col-sm-4'><span class='formLabel'>"._t('Last name/Organization name')."</span>".caHTMLTextInput("surname", array())."</div>";
+	print "<div class='col-sm-4'><span class='formLabel'>"._t('Affiliation')."</span>".caHTMLTextInput("company", array());
 	print "<div class='submit'>".caFormSubmitLink($this->request, _t('Search  >'), 'searchSubmit', 'caRolodexForm')."</div></div>";
+?>
+	
+<?php
 	print "</form>\n";
 	print "</div>";	
 	if ($o_results = $this->getVar('results')) {
