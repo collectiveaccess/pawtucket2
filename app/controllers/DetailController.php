5bb654e2ca7e19152c087e614e1feb775e9abced
seth <seth@artgallery.gvsu.edu>
2014-08-23 01:17:19 -0400

    NOJIRA add ability to specify more than one map field

--- app/controllers/DetailController.php
+++ app/controllers/DetailController.php
@@ -147,13 +147,25 @@
 			//
 			// map
 			//
-			$vs_map_attribute = caGetOption('map_attribute', $va_options, false);
-			if($vs_map_attribute && $t_table->get($vs_map_attribute)){
+			if (!is_array($va_map_attributes = caGetOption('map_attributes', $va_options, array())) || !sizeof($va_map_attributes)) {
+				if ($vs_map_attribute = caGetOption('map_attribute', $va_options, false)) { $va_map_attributes = array($vs_map_attribute); }
+			}
+			
+			$this->view->setVar("map", "");
+			if(is_array($va_map_attributes) && sizeof($va_map_attributes)) {
 				$o_map = new GeographicMap((($vn_width = caGetOption('map_width', $va_options, false)) ? $vn_width : 285), (($vn_height = caGetOption('map_height', $va_options, false)) ? $vn_height : 200), 'map');
-				$o_map->mapFrom($t_table, $vs_map_attribute);
-				$this->view->setVar("map", $o_map->render('HTML'));
-			}else{
-				$this->view->setVar("map", "");
+					
+				$vn_mapped_count = 0;	
+				foreach($va_map_attributes as $vs_map_attribute) {
+					if ($t_table->get($vs_map_attribute)){
+						$o_map->mapFrom($t_table, $vs_map_attribute);
+						$vn_mapped_count++;
+					}
+				}
+				
+				if ($vn_mapped_count > 0) { 
+					$this->view->setVar("map", $o_map->render('HTML'));
+				}
 			}
 			
  			//
