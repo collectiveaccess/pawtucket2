077a31d00485dffbfcde39dd921dc9260db7f5e4
pinkfishies <sophie@whirl-i-gig.com>
2014-10-23 17:55:20 -0400

    glenstone theme

--- themes/glenstone/views/Browse/browse_results_images_html.php
+++ themes/glenstone/views/Browse/browse_results_images_html.php
@@ -80,10 +80,10 @@
 				$vn_id 					= $qr_res->get("{$vs_table}.{$vs_pk}");
 				if ($qr_res->get('ca_objects.type_id') == 30) {
 					$vs_label_author	 	= "<p class='artist'>".$qr_res->get("ca_entities.preferred_labels.name", array('restrictToRelationshipTypes' => 'author'))."</p>";
-					$vs_label_detail 	= "<p style='text-decoration:underline;'>".$qr_res->get("{$vs_table}.preferred_labels.name")."</p>";
+					$vs_label_detail 	= "<p class='bookTitle'>".$qr_res->get("{$vs_table}.preferred_labels.name")."</p>";
 					$vs_label_pub 	= "<p>".$qr_res->get("ca_objects.publication_description")."</p>";
 					$vs_label_call 	= "<p>".$qr_res->get("ca_objects.call_number")."</p>";
-					$vs_label_status 	= "<p>".$qr_res->get("ca_objects.purchase_status")."</p>";
+					$vs_label_status 	= "<p>".$qr_res->get("ca_objects.purchase_status", array('convertCodesToDisplayText' => true))."</p>";
 					$vs_idno_detail_link 	= "";
 					$vs_library_info = $vs_label_detail.$vs_label_author.$vs_label_pub.$vs_label_call.$vs_label_status;
 
