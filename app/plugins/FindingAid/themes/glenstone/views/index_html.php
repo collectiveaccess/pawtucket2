2778c8f443f2ab684cb439469d48b9b062b61115
Seth <seth@whirl-i-gig.com>
2015-01-18 09:44:25 -0500

    Glenstone plugin fixes

--- app/plugins/FindingAid/themes/glenstone/views/index_html.php
+++ app/plugins/FindingAid/themes/glenstone/views/index_html.php
@@ -3,9 +3,12 @@
 	$ps_template = $this->getVar('display_template');
 	$vs_page_title = $this->getVar('page_title');
 	$vs_intro_text = $this->getVar('intro_text');
+	$vs_open_default = $this->getVar('open_by_default');
+	#AssetLoadMananger::register('readmore');
+	AssetLoadManager::register("readmore");
 	
-	$qr_top_level_collections = ca_collections::find(array('parent_id' => null), array('returnAs' => 'searchResult', 'sort' => 'ca_collections.idno'));
-
+	$qr_top_level_collections = ca_collections::find(array('type_id' => 119), array('returnAs' => 'searchResult', 'sort' => 'ca_collection_labels.name'));
+ 
 ?>
 	<h1><?php print $vs_page_title; ?></h1>
 	<div class='findingIntro'><?php print $vs_intro_text; ?></div>
@@ -18,25 +21,28 @@
 			foreach($va_hierarchy as $vn_i => $va_hierarchy_item) {
 				$t_collection_item = new ca_collections($va_hierarchy_item['id']);
 				if ($t_collection_item->get('ca_collections.fa_access') != 261) {
-					print "<div class='collHeader' style='margin-left: ".($va_hierarchy_item['level'] * 35)."px'>";
-					if (($va_hierarchy_item['level']) == 0 && ($qr_top_level_collections->get('ca_collections.children.collection_id'))) {
-						print "<a href='#'><i class='fa fa-angle-double-down finding-aid down{$vn_top_level_collection_id}'></i></a>";
-					} elseif ($va_hierarchy_item['level'] == 0) {
+					print "<div class='collHeader' >";
+					#if (($va_hierarchy_item['level']) == 0 && ($qr_top_level_collections->get('ca_collections.children.collection_id'))) {
+					#	print "<a href='#'><i class='fa fa-angle-double-down finding-aid down{$vn_top_level_collection_id}'></i></a>";
+					#} elseif ($va_hierarchy_item['level'] == 0) {
 						print "<i class='fa fa-square-o finding-aid' {$va_opacity}></i>";
-					} else {
-						$va_opacity = "style='opacity: .".(90 - ($va_hierarchy_item['level'] * 20))."' ";
-						print "<i class='fa fa-angle-right finding-aid' {$va_opacity}></i>";
-					}
+					#} else {
+					#	$va_opacity = "style='opacity: .".(90 - ($va_hierarchy_item['level'] * 20))."' ";
+					#	print "<i class='fa fa-angle-right finding-aid' {$va_opacity}></i>";
+					#}
 					if ($t_collection_item->get('ca_collections.fa_access') == 262) {
 						print "<div class='text'>".caNavLink($this->request, $t_collection_item->get('ca_collections.preferred_labels')." (".$t_collection_item->get('ca_collections.idno').")", '', '', 'Detail', 'collections/'.$va_hierarchy_item['id'])."</div><br/>";
 					} else {
-						print "<div class='text'>".caNavLink($this->request, $t_collection_item->get('ca_collections.preferred_labels.name')." (".$t_collection_item->get('ca_collections.idno').")", '', '', 'Detail', 'collections/'.$va_hierarchy_item['id'])." <br/><div style='font-weight:200; width: 500px; margin-left: 30px;'>".$t_collection_item->get('ca_collections.component_notes')."</div></div><br/>";
+						print "<div class='text'>".caNavLink($this->request, $t_collection_item->get('ca_collections.preferred_labels.name')." (".$t_collection_item->get('ca_collections.idno').")", '', '', 'Detail', 'collections/'.$va_hierarchy_item['id'])." <br/><div style='font-weight:200; width: 500px; margin-left: 30px;' class='trimText'>".$t_collection_item->get('ca_collections.abstract')."</div></div><br/>";
 					}
 					print "</div><!-- end collHeader-->";
 				}
 				if ($va_hierarchy_item['level'] == 0) {
+					if ($vs_open_default == true) {
+						$vs_open_style = "style='display:none';";
+					}
 				
-					print "<div class='collBlock".$vn_top_level_collection_id."'>";
+					print "<div class='collBlock".$vn_top_level_collection_id."' {$vs_open_style}>";
 ?>				
 					<script>
 						$(function() {
@@ -64,3 +70,12 @@
 	}
 ?>
 	</div><!-- findingAid Cont-->
+	
+<script type='text/javascript'>
+	jQuery(document).ready(function() {
+		$('.trimText').readmore({
+		  speed: 75,
+		  maxHeight: 100
+		});
+	});
+</script>	
