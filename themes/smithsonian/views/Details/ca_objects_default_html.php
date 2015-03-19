51e01293982746fe8594e5ace9c507c4533c3c88
Seth <seth@collectiveaccess.org>
2015-03-19 18:26:40 -0400

    NOJIRA SNI update

--- themes/smithsonian/views/Details/ca_objects_default_html.php
+++ themes/smithsonian/views/Details/ca_objects_default_html.php
@@ -15,17 +15,17 @@
 			</div><!-- end col -->
 			<div class='col-md-6 col-lg-6 metadata'>
 				<H4>{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</H4>
-				<H6>{{{<unit>^ca_objects.type_id</unit>}}}</H6>
+				<!--<H6>{{{<unit>^ca_objects.type_id</unit>}}}</H6>-->
 				<HR>
 <?php
-				if ($t_object->get('ca_objects.generation_video', array('excludeValues' => array('not_specified'))) != "") {
-					print "<div><span class='metaTitle'>Generation</span><span class='meta'>".$t_object->get('ca_objects.generation_video', array('convertCodesToDisplayText' => true, 'excludeValues' => array('not_specified')))."</span></div>";
-				}	
-				if ($t_object->get('ca_objects.generation_supporting', array('excludeValues' => array('not_specified'))) != "") {
-					print "<div><span class='metaTitle'>Generation</span><span class='meta'>".$t_object->get('ca_objects.generation_supporting', array('convertCodesToDisplayText' => true, 'excludeValues' => array('not_specified')))."</span></div>";
-				}					
+				//if ($t_object->get('ca_objects.generation_video', array('excludeValues' => array('not_specified'))) != "") {
+				//	print "<div><span class='metaTitle'>Generation</span><span class='meta'>".$t_object->get('ca_objects.generation_video', array('convertCodesToDisplayText' => true, 'excludeValues' => array('not_specified')))."</span></div>";
+				//}	
+				//if ($t_object->get('ca_objects.generation_supporting', array('excludeValues' => array('not_specified'))) != "") {
+				//	print "<div><span class='metaTitle'>Generation</span><span class='meta'>".$t_object->get('ca_objects.generation_supporting', array('convertCodesToDisplayText' => true, 'excludeValues' => array('not_specified')))."</span></div>";
+				//}					
 ?>				
-				{{{<unit><ifdef code="ca_objects.supporting_type"><div><span class='metaTitle'>Type</span><span class='meta'>^ca_objects.supporting_type</span></ifdef></div></unit>}}}
+				<!--{{{<unit><ifdef code="ca_objects.supporting_type"><div><span class='metaTitle'>Type</span><span class='meta'>^ca_objects.supporting_type</span></ifdef></div></unit>}}}-->
 				{{{<unit><ifdef code="ca_objects.idno"><div><span class='metaTitle'>Identifer</span><span class='meta'>^ca_objects.idno</span></div></ifdef></unit>}}}
 
 <?php
@@ -35,9 +35,66 @@
 ?>			
 			{{{<ifcount min="1" code="ca_occurrences.workDate.dates_value" ><div><span class='metaTitle'>Date:</span> <unit delimiter=", ">^ca_occurrences.workDate.dates_value <ifcount min="1" code="ca_occurrences.workDate.work_dates_types">(^ca_occurrences.workDate.work_dates_types)</ifdef></unit></div></ifcount>}}}
 			{{{<ifcount code="ca_occurrences.description" min="1"><span class='metaTitle'>Description</span><span class='meta'>^ca_occurrences.description</span></ifcount>}}}
-				{{{<ifcount code="ca_occurrences.location" min="1"><span class='metaTitle'>Location</span><span class='meta'>^ca_occurrences.location</span></ifcount>}}}
+			{{{<ifcount code="ca_occurrences.locationText" min="1"><span class='metaTitle'>Location</span><span class='meta'>^ca_occurrences.locationText</span></ifcount>}}}
 
-				{{{<ifcount min="1" code="ca_storage_locations.preferred_labels"><div><span class='metaTitle'>Storage Location</span><span class='meta'><unit>^ca_storage_locations.preferred_labels<br/></unit></span></div></ifcount>}}}
+
+			{{{<unit relativeTo="ca_occurrences"><ifcount code="ca_places" min="1" max="1"><span class='metaTitle'>Related place</H6></span></unit>}}}
+			{{{<unit relativeTo="ca_occurrences"><ifcount code="ca_places" min="2"><span class='metaTitle'>Related places</H6></span></unit>}}}
+			{{{<div class='meta'><unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit></div>}}}
+
+			{{{<unit relativeTo="ca_occurrences"><ifcount code="ca_entities" min="1" max="1"><span class='metaTitle'>Related person</H6></span></div>}}}
+			{{{<unit relativeTo="ca_occurrences"><ifcount code="ca_entities" min="2"><span class='metaTitle'>Related people</H6></span></div>}}}
+			{{{<div class='meta'><unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit></div>}}}
+
+			{{{<ifcount min="1" code="ca_occurrences.restrictions|ca_occurrences.rights|ca_occurrences.sniDepiction|ca_entities.preferred_labels"><hr><h5>Rights & Permissions</h5></ifcount>}}}
+			{{{<unit><ifdef code="ca_occurrences.restrictions"><div><span class='metaTitle'>Restrictions</span><span class='meta'>^ca_occurrences.restrictions</span></div></ifdef></unit>}}}
+			{{{<unit><ifdef code="ca_occurrences.rights"><div><span class='metaTitle'>Rights</span><span class='meta'>^ca_occurrences.rights</span></div></ifdef></unit>}}}
+
+<?php
+			if (is_array($va_occurrence_ids = $t_object->get('ca_occurrences.occurrence_id', array('returnAsArray' => true)))) {
+				$qr_occ = caMakeSearchResult('ca_occurrences', $va_occurrence_ids);
+				
+				while($qr_occ->nextHit()) {
+					if ($va_contributors = $qr_occ->get('ca_entities', array('excludeRelationshipTypes' => array('subject, interviewee'), 'returnAsArray' => true))) {
+						print "<div><span class='metaTitle'>Contributors</span><div class='meta'>";
+						foreach ($va_contributors as $cont_key => $va_contributor) {
+							print "<div>".caNavLink($this->request, $va_contributor['displayname']." (".$va_contributor['relationship_typename'].")", '' , 'Detail', 'entities', $va_contributor['entity_id'])."</div>";
+						}
+						print "</div></div>";
+					}	
+				}
+			}
+?>
+			{{{<ifcount code="ca_occurrences.workDate.dates_value|ca_occurrences.genre|ca_occurrences.productionTypes|ca_occurrences.mission.missionCritical|ca_occurrences.awards.award_event|ca_occurrences.distribution_status.distribution_date" min="1"><hr><h5>Program Info</h5></ifcount>}}}
+
+<?php
+			if (($vs_genre = $t_object->get('ca_occurrences.genre')) != "") {
+				print "<div><span class='metaTitle'>Genre</span><span class='meta'>{$vs_genre}</span></div>";
+			}	
+			if (($vs_prod_type = $t_object->get('ca_occurrences.productionTypes')) != "") {
+				print "<div><span class='metaTitle'>Production type</span><span class='meta'>{$vs_prod_type}</span></div>";
+			}
+			if (($vs_mission_crit = $t_object->get('ca_occurrences.mission.missionCritical')) == "Yes") {
+				print "<div><span class='metaTitle'>Mission critical</span><span class='meta'><div>Mission Critical: {$vs_mission_crit}</div>";
+				print "<div>Year: ".$t_object->get('ca_occurrences.mission.missionYear')." (".$t_object->get('ca_occurrences.mission.mission_dates_types').")</div>";
+				print "</span></div>";
+			}
+			
+			$va_awards = $t_object->get('ca_occurrences.awards', array('returnAsArray' => true));
+			if (sizeof($va_awards) > 0) {
+				print "<div><span class='metaTitle'>Awards</span><span class='meta'>";
+				foreach ($va_awards as $award => $va_award) {
+					print "<div>Award: ".$va_award['award_event']."</div>";
+					print "<div>Year: ".$va_award['award_year']."</div>";
+					print "<div>Type: ".$va_award['award_types']."</div>";
+					print "<div>Notes: ".$va_award['award_notes']."</div>";
+					print "<div style='height:10px;'></div>";
+				}
+				print "</span></div>";
+			}
+?>
+
+		<!--	{{{<ifcount min="1" code="ca_storage_locations.preferred_labels"><div><span class='metaTitle'>Storage Location</span><span class='meta'><unit>^ca_storage_locations.preferred_labels<br/></unit></span></div></ifcount>}}}-->
 <?php
 				if ($t_object->get('ca_objects.video_physical', array('excludeValues' => array('not_specified'))) != "") {
 					print "<div><span class='metaTitle'>Master Format</span><span class='meta'>".$t_object->get('ca_objects.video_physical', array('convertCodesToDisplayText' => true, 'excludeValues' => array('not_specified')))."</span></div>";
@@ -83,15 +140,7 @@
 				{{{<ifcount code="ca_objects.related" min="2"><span class="metaTitle">Related Objects</span></ifcount>}}}
 				{{{<div class='meta'><unit relativeTo="ca_objects" delimiter="<br/>"><l>^ca_objects.related.preferred_labels.displayname</l></unit></div>}}}					
 				
-				{{{<ifcount code="ca_entities" min="1" max="1"><span class='metaTitle'>Related person</H6></span>}}}
-				{{{<ifcount code="ca_entities" min="2"><span class='metaTitle'>Related people</H6></span>}}}
-				{{{<div class='meta'><unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit></div>}}}
-				
-				
-				{{{<ifcount code="ca_entities" min="1" max="1"><span class='metaTitle'>Related place</H6></span>}}}
-				{{{<ifcount code="ca_entities" min="2"><span class='metaTitle'>Related places</H6></span>}}}
-				{{{<div class='meta'><unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit></div>}}}
-				
+
 				
 				{{{<ifcount code="ca_objects.LcshNames" min="1"><H6>LC Terms</H6></ifcount>}}}
 				{{{<unit delimiter="<br/>">^ca_objects.LcshNames</unit>}}}
