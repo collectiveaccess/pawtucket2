6feb300d3edfd5f2d33373821b564df2f256396b
Seth <seth@collectiveaccess.org>
2015-03-22 22:48:55 -0400

    NOJIRA Add chartist

---
+++ assets/chartist/test/spec/spec-line-chart.js
@@ -0,0 +1,98 @@
+describe('Line chart tests', function() {
+  'use strict';
+
+  beforeEach(function() {
+
+  });
+
+  afterEach(function() {
+
+  });
+
+  describe('Meta data tests', function () {
+    it('should render meta data correctly with mixed value array', function(done) {
+      jasmine.getFixtures().set('<div class="ct-chart ct-golden-section"></div>');
+
+      var meta = {
+        test: 'Serialized Test'
+      };
+
+      var data = {
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+        series: [
+          [5, 2, 4, {
+            value: 2,
+            meta: meta
+          }, 0]
+        ]
+      };
+
+      var chart = new Chartist.Line('.ct-chart', data);
+
+      chart.on('created', function() {
+        expect(Chartist.deserialize($('.ct-point').eq(3).attr('ct:meta'))).toEqual(meta);
+        done();
+      });
+    });
+
+    it('should render meta data correctly with mixed value array and different normalized data length', function(done) {
+      jasmine.getFixtures().set('<div class="ct-chart ct-golden-section"></div>');
+
+      var meta = {
+        test: 'Serialized Test'
+      };
+
+      var data = {
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        series: [
+          [5, 2, 4, {
+            value: 2,
+            meta: meta
+          }, 0]
+        ]
+      };
+
+      var chart = new Chartist.Line('.ct-chart', data);
+
+      chart.on('created', function() {
+        expect(Chartist.deserialize($('.ct-point').eq(3).attr('ct:meta'))).toEqual(meta);
+        done();
+      });
+    });
+
+    it('should render meta data correctly with mixed value array and mixed series notation', function(done) {
+      jasmine.getFixtures().set('<div class="ct-chart ct-golden-section"></div>');
+
+      var seriesMeta = 9999,
+        valueMeta = {
+          test: 'Serialized Test'
+        };
+
+      var data = {
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        series: [
+          [5, 2, 4, {
+            value: 2,
+            meta: valueMeta
+          }, 0],
+          {
+            meta: seriesMeta,
+            data: [5, 2, {
+              value: 2,
+              meta: valueMeta
+            }, 0]
+          }
+        ]
+      };
+
+      var chart = new Chartist.Line('.ct-chart', data);
+
+      chart.on('created', function() {
+        expect(Chartist.deserialize($('.ct-series-a .ct-point').eq(3).attr('ct:meta'))).toEqual(valueMeta);
+        expect(Chartist.deserialize($('.ct-series-b')).attr('ct:meta')).toEqual(''+seriesMeta);
+        expect(Chartist.deserialize($('.ct-series-b .ct-point').eq(2).attr('ct:meta'))).toEqual(valueMeta);
+        done();
+      });
+    });
+  });
+});
