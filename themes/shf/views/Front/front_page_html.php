5c1acebb329245ebed65b04f0bd866cf2a82f3b2
Stefan <stefan@whirl-i-gig.com>
2015-06-05 11:09:47 -0400

    Add surfing theme

---
+++ themes/shf/views/Front/front_page_html.php
@@ -0,0 +1,48 @@
+<?php
+/** ---------------------------------------------------------------------
+ * themes/default/Front/front_page_html : Front page of site 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2013 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * @package CollectiveAccess
+ * @subpackage Core
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
+ *
+ * ----------------------------------------------------------------------
+ */
+		print $this->render("Front/featured_set_slideshow_html.php");
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-8"> 
+			<div>
+			<p><b>Digital Watermarking of our images - Public Notice</b></p>
+			As part of our commitment to protecting our image donors, the Surfing Heritage Foundation has begun using digital watermarking on ALL of our images, including those images seen on our website. This watermark is not visible to the eye, but is easily seen by many computer programs such at Photoshop and other image editing programs. In addition, we have also purchased a "watermark spider" that crawls the Internet specifically looking for any images that contain our SHF watermark. The Surfing Heritage Foundation is prepared to take the appropriate action should we find any illegal or unlicensed usage of images from our files.
+			</div>
+		</div><!--end col-sm-8-->
+		<div class="col-sm-4">
+<?php
+		print $this->render("Front/gallery_set_links_html.php");
+?>
+		</div> <!--end col-sm-4-->	
+	</div><!-- end row -->
+</div> <!--end container-->
