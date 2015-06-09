1ef8c2e0435436529644dc2a0c17bf30b00e0070
pinkfishies <sophie@whirl-i-gig.com>
2015-06-09 16:00:57 -0400

    morton theme

---
+++ themes/morton/views/Details/ca_collections_default_html.php
@@ -0,0 +1,94 @@
+<?php
+	$t_item = $this->getVar("item");
+	$va_comments = $this->getVar("comments");
+?>
+<div class="row">
+	<div class='col-xs-12 navTop'><!--- only shown at small screen size -->
+		{{{previousLink}}}{{{resultsLink}}}{{{nextLink}}}
+	</div><!-- end detailTop -->
+	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
+		<div class="detailNavBgLeft">
+			{{{previousLink}}}{{{resultsLink}}}
+		</div><!-- end detailNavBgLeft -->
+	</div><!-- end col -->
+	<div class='col-xs-12 col-sm-10 col-md-10 col-lg-10'>
+		<div class="container">
+			<div class="row">
+				<div class='col-md-12 col-lg-12'>
+					<H4>{{{^ca_collections.preferred_labels.name}}}</H4>
+					<H6>{{{^ca_collections.type_id}}}{{{<ifdef code="ca_collections.collection_number">, ^ca_collections.collection_number</ifdef>}}}</H6>
+				</div><!-- end col -->
+			</div><!-- end row -->
+			<div class="row">			
+				<div class='col-md-6 col-lg-6'>
+<?php
+					if ($vs_repository = $t_item->get('ca_collections.repository', array('template' => '^repositoryName ^repositoryLocation', 'delimiter' => '<br/>'))) {
+						print "<div class='unit'><h6>Repository/h6>".$vs_repository."</div>";
+					}
+					if ($vs_desc = $t_item->get('ca_collections.description.description_text', array('delimiter' => '<br/>'))) {
+						print "<div class='unit'><h6>Description</h6>".$vs_desc."</div>";
+					}	
+					if ($vs_date = $t_item->get('ca_collections.date', array('delimiter' => '<br/>', 'template' => '^date_value ^date_types <ifdef code="date_notes"><br/>^date_notes</ifdef>', 'convertCodesToDisplayText' => true))) {
+						print "<div class='unit'><h6>Date</h6>".$vs_date."</div>";
+					}
+					if ($vs_extent = $t_item->get('ca_collections.extent_text')) {
+						print "<div class='unit'><h6>Extent</h6>".$vs_extent."</div>";
+					}
+					if ($vs_creator = $t_item->get('ca_entities', array('delimiter' => ', ', 'returnAsLink' => true, 'template' => '^preferred_labels ^relationship_type'))) {
+						print "<div class='unit'><h6>Creator</h6>".$vs_creator."</div>";
+					}																				
+?>
+					<div id="detailTools">
+						<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><span class="glyphicon glyphicon-comment"></span>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
+						<div id='detailComments'>{{{itemComments}}}</div><!-- end itemComments -->
+						<div class="detailTool"><span class="glyphicon glyphicon-share-alt"></span>{{{shareLink}}}</div><!-- end detailTool -->
+					</div><!-- end detailTools -->
+					
+				</div><!-- end col -->
+				<div class='col-md-6 col-lg-6'>
+					{{{<ifcount code="ca_collections.related" min="1" max="1"><H6>Related collection</H6></ifcount>}}}
+					{{{<ifcount code="ca_collections.related" min="2"><H6>Related collections</H6></ifcount>}}}
+					{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.related.preferred_labels.name</l></unit>}}}
+					
+					{{{<ifcount code="ca_entities" min="1" max="1"><H6>Related person</H6></ifcount>}}}
+					{{{<ifcount code="ca_entities" min="2"><H6>Related people</H6></ifcount>}}}
+					{{{<unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit>}}}
+					
+					{{{<ifcount code="ca_occurrences" min="1" max="1"><H6>Related occurrence</H6></ifcount>}}}
+					{{{<ifcount code="ca_occurrences" min="2"><H6>Related occurrences</H6></ifcount>}}}
+					{{{<unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_occurrences.preferred_labels.name</l></unit>}}}
+					
+					{{{<ifcount code="ca_places" min="1" max="1"><H6>Related place</H6></ifcount>}}}
+					{{{<ifcount code="ca_places" min="2"><H6>Related places</H6></ifcount>}}}
+					{{{<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit>}}}					
+				</div><!-- end col -->
+			</div><!-- end row -->
+{{{<ifcount code="ca_objects" min="2">
+			<div class="row">
+				<div id="browseResultsContainer">
+					<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>
+				</div><!-- end browseResultsContainer -->
+			</div><!-- end row -->
+			<script type="text/javascript">
+				jQuery(document).ready(function() {
+					jQuery("#browseResultsContainer").load("<?php print caNavUrl($this->request, '', 'Search', 'objects', array('search' => 'collection_id:^ca_collections.collection_id'), array('dontURLEncodeParameters' => true)); ?>", function() {
+						jQuery('#browseResultsContainer').jscroll({
+							autoTrigger: true,
+							loadingHtml: '<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>',
+							padding: 20,
+							nextSelector: 'a.jscroll-next'
+						});
+					});
+					
+					
+				});
+			</script>
+</ifcount>}}}
+		</div><!-- end container -->
+	</div><!-- end col -->
+	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
+		<div class="detailNavBgRight">
+			{{{nextLink}}}
+		</div><!-- end detailNavBgLeft -->
+	</div><!-- end col -->
+</div><!-- end row -->
---
+++ themes/morton/views/Details/ca_objects_default_html.php
@@ -0,0 +1,98 @@
+<?php
+	$t_object = $this->getVar("item");
+	$va_comments = $this->getVar("comments");
+?>
+<div class="row">
+	<div class='col-xs-12 navTop'><!--- only shown at small screen size -->
+		{{{previousLink}}}{{{resultsLink}}}{{{nextLink}}}
+	</div><!-- end detailTop -->
+	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
+		<div class="detailNavBgLeft">
+			{{{previousLink}}}{{{resultsLink}}}
+		</div><!-- end detailNavBgLeft -->
+	</div><!-- end col -->
+	<div class='col-xs-12 col-sm-10 col-md-10 col-lg-10'>
+		<div class="container"><div class="row">
+			<div class='col-sm-6 col-md-6 col-lg-5 col-lg-offset-1'>
+				{{{representationViewer}}}
+				
+				<?php print caObjectRepresentationThumbnails($this->request, $this->getVar("representation_id"), $t_object, array("returnAs" => "bsCols", "linkTo" => "carousel", "bsColClasses" => "smallpadding col-sm-3 col-md-3 col-xs-4")); ?>
+				<div id="detailTools">
+					<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><span class="glyphicon glyphicon-comment"></span>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
+					<div id='detailComments'>{{{itemComments}}}</div><!-- end itemComments -->
+					<div class="detailTool"><span class="glyphicon glyphicon-share-alt"></span>{{{shareLink}}}</div><!-- end detailTool -->
+				</div><!-- end detailTools -->
+			</div><!-- end col -->
+			
+			<div class='col-sm-6 col-md-6 col-lg-5'>
+				<H4>{{{ca_objects.preferred_labels.name}}}</H4>
+				<H6>{{{^ca_objects.type_id}}}{{{<ifdef code='ca_objects.pbcoreMediaTypes'>, ^ca_objects.pbcoreMediaTypes</ifdef>}}}</H6>
+				<HR>
+<?php
+					if ($vs_description = $t_object->get('ca_objects.description.description_text')){
+						print "<div class='unit'><h6>Description</h6>".$vs_description."</div>";
+					}
+					if ($vs_extent = $t_object->get('ca_objects.extent_text')) {
+						print "<div class='unit'><h6>Extent</h6>".$vs_extent."</div>";
+					}
+					if ($vs_date = $t_object->get('ca_objects.date', array('delimiter' => '<br/>', 'template' => '^ca_objects.date.date_value ^ca_objects.date.date_types <ifdef code="date_notes"><br/>^ca_objects.date.date_notes</ifdef>', 'convertCodesToDisplayText' => true))) {
+						print "<div class='unit'><h6>Date</h6>".$vs_date."</div>";
+					}
+					if ($vs_language = $t_object->get('ca_objects.language', array('convertCodesToDisplayText' => true))) {
+						print "<div class='unit'><h6>Language</h6>".$vs_language."</div>";
+					}
+					if ($vs_format = $t_object->get('ca_objects.format', array('delimiter' => '<br/>'))) {
+						print "<div class='unit'><h6>Format</h6>".$vs_format."</div>";
+					}																			
+?>								
+				<hr></hr>
+					<div class="row">
+						<div class="col-sm-6">		
+							{{{<ifcount code="ca_entities" min="1" max="1"><H6>Related person</H6></ifcount>}}}
+							{{{<ifcount code="ca_entities" min="2"><H6>Related people</H6></ifcount>}}}
+							{{{<unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit>}}}
+							
+							{{{<ifcount code="ca_collections" min="1" max="1"><H6>Related collection</H6></ifcount>}}}
+							{{{<ifcount code="ca_collections" min="2"><H6>Related collections</H6></ifcount>}}}
+							{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.preferred_labels</l></unit>}}}
+														
+							{{{<ifcount code="ca_places" min="1" max="1"><H6>Related place</H6></ifcount>}}}
+							{{{<ifcount code="ca_places" min="2"><H6>Related places</H6></ifcount>}}}
+							{{{<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit>}}}
+							
+							{{{<ifcount code="ca_list_items" min="1" max="1"><H6>Related Term</H6></ifcount>}}}
+							{{{<ifcount code="ca_list_items" min="2"><H6>Related Terms</H6></ifcount>}}}
+							{{{<unit relativeTo="ca_list_items" delimiter="<br/>">^ca_list_items.preferred_labels.name_plural</unit>}}}
+							
+							{{{<ifcount code="ca_objects.LcshNames" min="1"><H6>LC Terms</H6></ifcount>}}}
+							{{{<unit delimiter="<br/>"><l>^ca_objects.LcshNames</l></unit>}}}
+						</div><!-- end col -->				
+						<div class="col-sm-6 colBorderLeft">
+							{{{map}}}
+<?php
+							if ($va_spatial = $t_object->get('ca_objects.coverageSpatial', array('delimiter' => '<br/>'))) {
+								print "<div class='unit'><h6>Spatial Coverage</h6>".$va_spatial."</div>";
+							}
+?>							
+						</div>
+					</div><!-- end row -->
+			</div><!-- end col -->
+		</div><!-- end row --></div><!-- end container -->
+	</div><!-- end col -->
+	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
+		<div class="detailNavBgRight">
+			{{{nextLink}}}
+		</div><!-- end detailNavBgLeft -->
+	</div><!-- end col -->
+</div><!-- end row -->
+
+
+
+<script type='text/javascript'>
+	jQuery(document).ready(function() {
+		$('.trimText').readmore({
+		  speed: 75,
+		  maxHeight: 120
+		});
+	});
+</script>
