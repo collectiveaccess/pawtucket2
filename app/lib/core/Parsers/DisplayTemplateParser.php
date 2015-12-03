d33f89c2657bebf92ea69e25774cf8c0121091ea
Seth <seth@collectiveaccess.org>
2015-12-03 16:36:28 -0500

    Add aggregateUnique support

--- app/lib/core/Parsers/DisplayTemplateParser.php
+++ app/lib/core/Parsers/DisplayTemplateParser.php
@@ -197,12 +197,26 @@ class DisplayTemplateParser {
 			if ($pa_options['relativeToContainer']) {
 				$va_vals = DisplayTemplateParser::_getValues($qr_res, $va_template['tags'], $pa_options);
 				foreach($va_vals as $vn_index => $va_val_list) {
-					$va_proc_templates[] = DisplayTemplateParser::_processChildren($qr_res, $va_template['tree']->children, $va_val_list, array_merge($pa_options, ['index' => $vn_index]));
+					$va_proc_templates[] = DisplayTemplateParser::_processChildren($qr_res, $va_template['tree']->children, $va_val_list, array_merge($pa_options, ['index' => $vn_index, 'returnAsArray' => $pa_options['aggregateUnique']]));
 				}
 			} else {
-				$va_proc_templates[] = DisplayTemplateParser::_processChildren($qr_res, $va_template['tree']->children, DisplayTemplateParser::_getValues($qr_res, $va_template['tags'], $pa_options), $pa_options);
+				$va_proc_templates[] = DisplayTemplateParser::_processChildren($qr_res, $va_template['tree']->children, DisplayTemplateParser::_getValues($qr_res, $va_template['tags'], $pa_options), array_merge($pa_options, ['returnAsArray' => $pa_options['aggregateUnique']]));
 			}
 		}
+		
+		if ($pa_options['aggregateUnique']) {
+			$va_acc = [];
+			foreach($va_proc_templates as $va_val_list) {
+				if(is_array($va_val_list)) { 
+					$va_acc = array_merge($va_acc, $va_val_list); 
+				} else {
+					$va_acc[] = $va_val_list;
+				}
+			}
+			$va_proc_templates = array_unique($va_acc);
+		}
+		
+		if (!$pb_include_blanks && !$pb_include_blanks_for_prefetch) { $va_proc_templates = array_filter($va_proc_templates, 'strlen'); }
 		
 		// Transform links
 		$va_proc_templates = caCreateLinksFromText(
@@ -210,8 +224,6 @@ class DisplayTemplateParser {
 			null, caGetOption('linkTarget', $pa_options, null),
 			array_merge(['addRelParameter' => true, 'requireLinkTags' => true], $pa_options)
 		);
-		
-		if (!$pb_include_blanks && !$pb_include_blanks_for_prefetch) { $va_proc_templates = array_filter($va_proc_templates, 'strlen'); }
 		
 		if (!$pb_return_as_array) {
 			return join($ps_delimiter, $va_proc_templates);
@@ -442,6 +454,7 @@ class DisplayTemplateParser {
 					// <unit> attributes
 					$vs_unit_delimiter = $o_node->delimiter ? (string)$o_node->delimiter : $ps_delimiter;
 					$vb_unique = $o_node->unique ? (bool)$o_node->unique : false;
+					$vb_aggregate_unique = $o_node->aggregateUnique ? (bool)$o_node->aggregateUnique : false;
 					$vs_unit_skip_if_expression = (string)$o_node->skipIfExpression;
 					
 					$vn_start = (int)$o_node->start;
@@ -525,7 +538,10 @@ class DisplayTemplateParser {
 									'isUnit' => true,
 									'unitStart' => $vn_start,
 									'unitLength' => $vn_length,
-									'relativeToContainer' => $vs_relative_to_container
+									'relativeToContainer' => $vs_relative_to_container,
+									'includeBlankValuesInTopLevelForPrefetch' => false,
+									'unique' => $vb_unique,
+									'aggregateUnique' => $vb_aggregate_unique
 								]
 							)
 						);
@@ -534,7 +550,7 @@ class DisplayTemplateParser {
 						if (($vn_start > 0) || !is_null($vn_length)) { 
 							$vn_last_unit_omit_count = sizeof($va_tmpl_val) - ($vn_length - $vn_start);
 						}
-					
+						if (caGetOption('returnAsArray', $pa_options, false)) { return $va_tmpl_val; }
 						$vs_acc .= join($vs_unit_delimiter, $va_tmpl_val);
 						if ($pb_is_case) { break(2); }
 					} else { 
@@ -583,7 +599,10 @@ class DisplayTemplateParser {
 									'restrictToTypes' => $va_get_options['restrictToTypes'],
 									'isUnit' => true,
 									'unitStart' => $vn_start,
-									'unitLength' => $vn_length
+									'unitLength' => $vn_length,
+									'includeBlankValuesInTopLevelForPrefetch' => false,
+									'unique' => $vb_unique,
+									'aggregateUnique' => $vb_aggregate_unique
 								]
 							)
 						);	
@@ -594,6 +613,7 @@ class DisplayTemplateParser {
 							$vn_last_unit_omit_count = $vn_num_vals -  ($vn_length - $vn_start);
 						}
 						
+						if (caGetOption('returnAsArray', $pa_options, false)) { return $va_tmpl_val; }
 						$vs_acc .= join($vs_unit_delimiter, $va_tmpl_val);
 						if ($pb_is_case) { break(2); }
 					}
