ed74f947190911700742273ffcb7ce247c032a4c
Seth <seth@collectiveaccess.org>
2015-12-14 19:48:15 -0500

    Prevent warnings

--- app/lib/core/Plugins/SearchEngine/SqlSearch.php
+++ app/lib/core/Plugins/SearchEngine/SqlSearch.php
@@ -884,8 +884,10 @@ class WLPlugSearchEngineSqlSearch extends BaseSearchPlugin implements IWLPlugSea
 										$vb_do_stemming = $this->opb_do_stemming;
 										if (mb_substr($vs_term, -1) == '|') {
 											$vs_term = mb_substr($vs_term, 0, mb_strlen($vs_term) - 1);
+											$vs_raw_term = mb_substr($vs_raw_term, 0, mb_strlen($vs_raw_term) - 1);
 											$vb_do_stemming = false;
 										} elseif (mb_substr($vs_raw_term, -1) == '|') {
+											$vs_raw_term = mb_substr($vs_raw_term, 0, mb_strlen($vs_raw_term) - 1);
 											$vb_do_stemming = false;
 										}
 										if ($vb_do_stemming) {
@@ -1300,8 +1302,7 @@ class WLPlugSearchEngineSqlSearch extends BaseSearchPlugin implements IWLPlugSea
 						if (sizeof($va_ft_terms)) {
 							if (($t_table) && (strlen($vs_fld_num) > 1)) {
 								$o_search = new SearchEngine();
-								$va_field_info = $o_search->getFieldOptions($pn_subject_tablenum, $vs_table_num, $vs_field);
-								
+								if (!is_array($va_field_info = $o_search->getFieldOptions($pn_subject_tablenum, $vs_table_num, $vs_field))) { $va_field_info = array(); }
 								$va_sql_where[] = "((swi.field_table_num = ".intval($vs_table_num).") AND (swi.field_num = '{$vs_fld_num}') AND (sw.word IN (".((in_array('DONT_TOKENIZE', $va_field_info) || in_array('INDEX_AS_IDNO', $va_field_info)) ? join(',', $va_raw_terms_escaped) : join(',', $va_ft_terms)).")))";
 							} else {
 								if (sizeof($va_ft_terms) == 1) {
