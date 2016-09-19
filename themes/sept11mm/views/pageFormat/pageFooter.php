0ca167ef255f60f939a233818fab511c7613071e
Maria Passarotti <maria@whirl-i-gig.com>
2016-09-19 11:12:26 -0400

    clicky code

--- themes/sept11mm/views/pageFormat/pageFooter.php
+++ themes/sept11mm/views/pageFormat/pageFooter.php
@@ -100,6 +100,17 @@
   ga('send', 'pageview');
  
 </script>
+<script type="text/javascript">
+	var clicky_site_ids = clicky_site_ids || [];
+	clicky_site_ids.push(100990350);
+	(function() {
+	  var s = document.createElement('script');
+	  s.type = 'text/javascript';
+	  s.async = true;
+	  s.src = '//static.getclicky.com/js';
+	  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+	})();
+</script>
 <?php
 	//
 	// Output HTML for debug bar
