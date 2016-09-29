37b6263070e0ae2f5071cb4d6a309b12a85bdd98
sophie <sophie@whirl-i-gig.com>
2016-09-29 15:51:35 -0400

    pre release default theme changes

--- themes/default/views/Search/ca_objects_advanced_search_objects_html.php
+++ themes/default/views/Search/ca_objects_advanced_search_objects_html.php
@@ -12,35 +12,35 @@
 	<div class='advancedContainer'>
 		<div class='row'>
 			<div class="advancedSearchField col-sm-12">
-				<div class='formLabel'>Title</div>
+				<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search across all fields in the database.">Keyword</span>
+				{{{_fulltext%width=200px&height=1}}}
+			</div>			
+		</div>		
+		<div class='row'>
+			<div class="advancedSearchField col-sm-12">
+				<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Limit your search to Object Titles only.">Title</span>
 				{{{ca_objects.preferred_labels.name%width=220px}}}
 			</div>
 		</div>
 		<div class='row'>
 			<div class="advancedSearchField col-sm-6">
-				<div class='formLabel'>Accession number</div>
+				<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search object identifiers.">Accession number</span>
 				{{{ca_objects.idno%width=210px}}}
 			</div>
 			<div class="advancedSearchField col-sm-6">
-				<div class='formLabel'>Type</div>
+				<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Limit your search to object types.">Type</span>
 				{{{ca_objects.type_id%height=30px}}}
 			</div>
 		</div>
 		<div class='row'>
 			<div class="advancedSearchField col-sm-12">
-				<div class='formLabel'>Keyword</div>
-				{{{_fulltext%width=200px&height=1}}}
-			</div>			
-		</div>
-		<div class='row'>
-			<div class="advancedSearchField col-sm-12">
-				<div class='formLabel'>Date range <i>(e.g. 1970-1979)</i></div>
+				<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search records of a particular date or date range.">Date range <i>(e.g. 1970-1979)</i></span>
 				{{{ca_objects.dates.dates_value%width=200px&height=40px&useDatePicker=0}}}
 			</div>
 		</div>
 		<div class='row'>
 			<div class="advancedSearchField col-sm-12">
-				<div class='formLabel'>Collection </div>
+				<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search records within a particular collection.">Collection </span>
 				{{{ca_collections.preferred_labels%restrictToTypes=collection%width=200px&height=40px}}}
 			</div>
 		</div>
@@ -60,3 +60,10 @@
 		</div><!-- end col -->
 	</div><!-- end row -->
 </div><!-- end container -->
+
+<script>
+	jQuery(document).ready(function() {
+		$('.advancedSearchField .formLabel').popover(); 
+	});
+	
+</script>
