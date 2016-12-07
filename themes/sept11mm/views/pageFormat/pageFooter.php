0afa618fda803299aac5eda098a5a5132d82f36c
Maria Passarotti <maria@whirl-i-gig.com>
2016-12-07 15:52:04 -0500

    clicky code

--- themes/sept11mm/views/pageFormat/pageFooter.php
+++ themes/sept11mm/views/pageFormat/pageFooter.php
@@ -100,17 +100,18 @@
   ga('send', 'pageview');
  
 </script>
-<!--<script type="text/javascript">
-	var clicky_site_ids = clicky_site_ids || [];
-	clicky_site_ids.push(100990350);
-	(function() {
-	  var s = document.createElement('script');
-	  s.type = 'text/javascript';
-	  s.async = true;
-	  s.src = '//static.getclicky.com/js';
-	  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
-	})();
-</script>-->
+<script type="text/javascript">
+var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(100990350);
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100990350ns.gif" /></p></noscript>
 <?php
 	//
 	// Output HTML for debug bar
