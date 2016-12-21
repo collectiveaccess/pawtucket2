d0f153682e3530b2dad9ad2b6ce422b5bf9aadf2
Maria <maria@whirl-i-gig.com>
2016-12-21 12:06:53 -0500

    bam theme collections hier updated

--- themes/bam2015/views/Details/ca_collections_default_html_bu_old_hier.php
+++ themes/bam2015/views/Details/ca_collections_default_html_bu_old_hier.php
@@ -11,15 +11,6 @@
  	}else{
  		$vn_collection_id = array_shift($t_item->get('ca_collections.hierarchy.collection_id', array("returnWithStructure" => true)));
  	}
- 	
- 	
- 	# --- get collections configuration
-	$o_collections_config = caGetCollectionsConfig();
-	$vb_show_hierarchy_viewer = true;
-	if($o_collections_config->get("do_not_display_collection_browser")){
-		$vb_show_hierarchy_viewer = false;	
-	}
-
 ?>
 <div class="row">
 	<div class='col-xs-12 navTop'><!--- only shown at small screen size -->
@@ -104,31 +95,54 @@
 							});
 						</script>
 						<div class='clearfix'></div>					
-<?php
-					}
-?>					
 					
-					<div class="row unit">
-						<div class='col-sm-12'>
-		<?php
-					if ($vb_show_hierarchy_viewer) {	
-		?>
-						<div id="collectionHierarchy"><?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?></div>
+					
+						<div class='unit row' style='margin-bottom:0px;'>
+							<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+								<hr class='divide' style='margin-bottom:0px; margin-top:3px;'></hr>
+							</div>
+							<div class='col-xs-12 col-sm-4 col-md-4 col-lg-4'>
+								<div class='findingAidContainer'><div class='label collection'>Collection Contents </div>
+<?php
+						foreach ($va_collection_children as $col_key => $vn_collection_id) {
+							$t_collection_series = new ca_collections($vn_collection_id);
+							$vs_collection_label = $t_collection_series->get('ca_collections.preferred_labels');
+							if (($t_collection_series->get('ca_collections.type_id', array('convertCodesToDisplayText' => true))) == "Box") {
+								$vs_icon = "<i class='fa fa-archive'></i>&nbsp;";
+							} else {
+								$vs_icon = null;
+							}
+							print "<div style='margin-left:0px;margin-top:5px;'><a href='#' class='openCollection openCollection".$vn_collection_id."'>".$vs_icon.$vs_collection_label."</a></div>";	
+?>													
 						<script>
 							$(document).ready(function(){
-								$('#collectionHierarchy').load("<?php print caNavUrl($this->request, '', 'Collections', 'collectionHierarchy', array('collection_id' => $t_item->get('collection_id'))); ?>"); 
+								$('.openCollection<?php print $vn_collection_id;?>').click(function(){
+									$('#collectionLoad').html("<?php print caGetThemeGraphic($this->request, 'indicator.gif');?> Loading");
+									$('#collectionLoad').load("<?php print caNavUrl($this->request, '', 'About', 'collectionsubview', array('collection_id' => $vn_collection_id)); ?>");
+									$('.openCollection').removeClass('active');
+									$('.openCollection<?php print $vn_collection_id;?>').addClass('active');
+									return false;
+								}); 
 							})
-						</script>
-		<?php				
-					}									
-		?>				
-						</div><!-- end col -->
-					</div><!-- end row -->
-			
+						</script>						
+<?php								
+						}
+?>
+								</div><!-- end findingAidContainer -->
+							</div><!-- end col -->
+							<div id='collectionLoad' class='col-xs-12 col-sm-8 col-md-8 col-lg-8'>
+								<i class='fa fa-arrow-left'></i> Click a Collection container to the left to see its contents.
+							</div>
+							<div class='col-sm-12 col-md-12 col-lg-12'><hr class='divide' style='margin-top:0px;'></hr></div>
+						</div><!-- end row unit -->
+<?php						
+					}										
+?>				
 				</div><!-- end col -->
 
 			</div><!-- end row -->
 {{{<ifcount code="ca_objects" min="1">
+			<hr class="divide" style="margin-bottom:0px;"/>
 			<div class="row">
 				<div class="col-sm-12 col-md-12 col-lg-12">
 					<div id="browseResultsContainer">
