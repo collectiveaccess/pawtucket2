1804be2d74f60326d3ce767c72607942a25726eb
sophie <sophie@whirl-i-gig.com>
2017-03-02 14:18:05 -0500

    pnf changes

--- themes/pnf/views/Browse/browse_results_images_html.php
+++ themes/pnf/views/Browse/browse_results_images_html.php
@@ -90,7 +90,12 @@
 			while($qr_res->nextHit() && ($vn_c < $vn_hits_per_block)) {
 				$vn_id 					= $qr_res->get("{$vs_table}.{$vs_pk}");
 				$vs_idno_detail_link 	= caDetailLink($this->request, $qr_res->get("{$vs_table}.idno"), '', $vs_table, $vn_id);
-				$vs_label_detail_link 	= caDetailLink($this->request, $qr_res->get("{$vs_table}.preferred_labels.name"), '', $vs_table, $vn_id);
+				$vs_uniform = null;
+				if ($vs_uniform = $qr_res->get('ca_objects.240_Uniform')) {
+					$vs_label_detail_link 	= caDetailLink($this->request, $vs_uniform, '', $vs_table, $vn_id);
+				} else {
+					$vs_label_detail_link 	= caDetailLink($this->request, $qr_res->get("{$vs_table}.preferred_labels"), '', $vs_table, $vn_id);
+				}				
 				$vs_thumbnail = "";
 				$vs_type_placeholder = "";
 				$vs_typecode = "";
