d4a60390bd071d69b20603e2d9082e9c5ddba08d
sophie <sophie@whirl-i-gig.com>
2018-02-27 13:44:44 -0500

    stormking theme

--- themes/stormking/views/Details/ca_objects_artwork_html.php
+++ themes/stormking/views/Details/ca_objects_artwork_html.php
@@ -45,7 +45,7 @@
 		<div class="row">
 			<div class="col-sm-12 objectInfo">
 <?php
-				if ($va_artist = $t_object->get('ca_entities.preferred_labels', array('checkAccess' => $va_access_values, 'returnAsLink' => true, 'restrictToRelationshipTypes' => array('artist')))) {
+/*				if ($va_artist = $t_object->get('ca_entities.preferred_labels', array('checkAccess' => $va_access_values, 'returnAsLink' => true, 'restrictToRelationshipTypes' => array('artist')))) {
 					print $va_artist."<br/>";
 				}
 				$vs_record_title = $t_object->get('ca_objects.preferred_labels.name');
@@ -56,7 +56,17 @@
 				}
 				if ($vs_date = $t_object->get('ca_objects.display_date')) {
 					print ", ".$vs_date;
-				}
+				} */
+				$t_list = new ca_lists();
+				$vn_view_on_status_id = $t_list->getItemIDFromList("view_status", "on_view");
+				$vn_view_off_status_id = $t_list->getItemIDFromList("view_status", "off_view");
+				
+				$vs_view_status_id = $t_object->get('ca_objects.view_status');
+				if ($vs_view_status_id == $vn_view_on_status_id) {
+					print "<div style='color:green;'>On view</div>";
+				} elseif ($vs_view_status_id  == $vn_view_off_status_id) {
+					print "<div style='color:red;'>Off view</div>";
+				}		
 ?>
 			</div>		
 		</div>
@@ -90,26 +100,21 @@
 #				}	
 				if ($va_credit = $t_object->get('ca_objects.credit_line')) {
 					print "<div class='tombstone'>".$va_credit."</div>";
-				}	
-				if ($va_extended = $t_object->getWithTemplate('
-				    <unit delimiter="<br/>">
-				        <ifdef code="ca_objects.plaque.plaque_text"><h6>Plaque Text</h6>^ca_objects.plaque.plaque_text</ifdef>
-				        <ifdef code="ca_objects.plaque.plaque_date"><h6>Text Date</h6>^ca_objects.plaque.plaque_date</ifdef>
-				        <ifdef code="ca_objects.plaque.plaque_purpose"><h6>Text Purpose</h6>^ca_objects.plaque.plaque_purpose</ifdef>
-				        <ifdef code="ca_objects.plaque.plaque_exh"><h6>Related Exhibition</h6>^ca_objects.plaque.plaque_exh</ifdef>
-				    </unit>')) {
-					print "<div class='unit'>".$va_extended."</div>";
-				}	
-				if ($vs_view_status = $t_object->get('ca_objects.view_status', array('convertCodesToDisplayText' => true))) {
-					print "<div class='unit'><h6>View Status</h6>".$vs_view_status."</div>";
-				}									
+				}		
+								
 ?>	
 				</div></div></div>
 			</div><!-- end col -->
 			<div class="col-sm-6">
 				<div class="container"><div class="row"><div class="col-sm-12">	
 <?php
+				if ($va_extended = $t_object->getWithTemplate('<unit delimiter="<br/>"><ifdef code="ca_objects.plaque.plaque_text">^ca_objects.plaque.plaque_text</ifdef><ifdef code="ca_objects.plaque.plaque_exh"><h6>Related Exhibition</h6>^ca_objects.plaque.plaque_exh</ifdef></unit>')) {
+					print "<div class='unit'>".$va_extended."</div>";
+				}
 				if ($va_related_ex_ids = $t_object->get('ca_occurrences.occurrence_id', array('returnAsArray' => true, 'checkAccess' => $va_access_values, 'restrictToTypes' => array('exhibition', 'public_program')))) {
+					if ($va_extended) {
+						print "<hr/>";
+					}
 					print '<div class="unit"><h6>Related Exhibitions & Programs</h6>';
 					foreach ($va_related_ex_ids as $va_id => $va_related_ex_id) {
 						$t_rel_ex = new ca_occurrences($va_related_ex_id);
@@ -120,6 +125,28 @@
 					}
 					print "</div>";
 				}
+				$vs_related_art = null;
+				if ($va_related_artworks = $t_object->get('ca_objects.related.object_id', array('returnAsArray' => true, 'checkAccess' => $va_access_values, 'restrictToTypes' => array('loaned_artwork', 'sk_artwork')))) {
+					if ($va_related_ex_ids | $va_extended) {
+						print "<hr>";
+					}
+					print "<div class='col-sm-6'>";
+					print '<div class="col-sm-12">	
+								<hr>
+								<h6 class="header">Related Artworks</h6>
+							</div>';
+					foreach ($va_related_artworks as $va_id => $va_related_artwork_id) {
+						$t_rel_obj = new ca_objects($va_related_artwork_id);
+						print "<div class='col-sm-6'>";
+						print "<div class='relatedArtwork'>";
+						print "<div class='relImg'>".caDetailLink($this->request, $t_rel_obj->get('ca_object_representations.media.iconlarge'), '', 'ca_objects', $t_rel_obj->get('ca_objects.object_id'))."</div>";
+						print "<p>".caDetailLink($this->request, $t_rel_obj->get('ca_objects.preferred_labels'), '', 'ca_objects', $t_rel_obj->get('ca_objects.object_id'))."</p>";
+						print "</div>";
+						print "</div>";
+					}
+					print "</div>";				
+				}
+							
 				if ($va_related_or_history = $t_object->get('ca_objects.related.object_id', array('returnAsArray' => true, 'checkAccess' => $va_access_values, 'restrictToTypes' => array('oral_history')))) {
 					print '<div class="unit"><h6>Related Oral Histories</h6>';
 					foreach ($va_related_or_history as $va_id => $va_related_or_history_id) {
@@ -138,24 +165,7 @@
 <?php
 			print '<div class="row">';
 					
-			$vs_related_art = null;
-			if ($va_related_artworks = $t_object->get('ca_objects.related.object_id', array('returnAsArray' => true, 'checkAccess' => $va_access_values, 'restrictToTypes' => array('loaned_artwork', 'sk_artwork')))) {
-				print "<div class='col-sm-6'>";
-				print '<div class="col-sm-12">	
-							<hr>
-							<h6 class="header">Related Artworks</h6>
-						</div>';
-				foreach ($va_related_artworks as $va_id => $va_related_artwork_id) {
-					$t_rel_obj = new ca_objects($va_related_artwork_id);
-					print "<div class='col-sm-6'>";
-					print "<div class='relatedArtwork'>";
-					print "<div class='relImg'>".caDetailLink($this->request, $t_rel_obj->get('ca_object_representations.media.iconlarge'), '', 'ca_objects', $t_rel_obj->get('ca_objects.object_id'))."</div>";
-					print "<p>".caDetailLink($this->request, $t_rel_obj->get('ca_objects.preferred_labels'), '', 'ca_objects', $t_rel_obj->get('ca_objects.object_id'))."</p>";
-					print "</div>";
-					print "</div>";
-				}
-				print "</div>";				
-			}
+
 ?>			
 				<div class="col-sm-6">
 <?php 	
