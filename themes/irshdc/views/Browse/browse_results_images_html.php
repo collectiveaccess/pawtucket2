505e1d5b8d6a84e96d86ad1925774f017821271a
Maria Passarotti <maria@whirl-i-gig.com>
2018-03-20 13:51:17 -0400

    irshdc

--- themes/irshdc/views/Browse/browse_results_images_html.php
+++ themes/irshdc/views/Browse/browse_results_images_html.php
@@ -86,7 +86,11 @@
 					$va_ids[] = $qr_res->get($vs_pk);
 					$vn_c++;
 				}
-				$va_images = caGetDisplayImagesForAuthorityItems($vs_table, $va_ids, array('version' => 'small', 'relationshipTypes' => caGetOption('selectMediaUsingRelationshipTypes', $va_options, null), 'objectTypes' => caGetOption('selectMediaUsingTypes', $va_options, null), 'checkAccess' => $va_access_values));
+				$va_tmp_options = array('version' => 'small', 'relationshipTypes' => caGetOption('selectMediaUsingRelationshipTypes', $va_options, null), 'objectTypes' => caGetOption('selectMediaUsingTypes', $va_options, null), 'checkAccess' => $va_access_values);
+				if($vs_table == 'ca_entities'){
+					$va_tmp_options["useRelatedObjectRepresentations"] = true;
+				}
+				$va_images = caGetDisplayImagesForAuthorityItems($vs_table, $va_ids, $va_tmp_options);
 			
 				$vn_c = 0;	
 				$qr_res->seek($vn_start);
