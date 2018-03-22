3b6bf31ef9391fab17548f984dcbcd96727b17c8
sophie <sophie@whirl-i-gig.com>
2018-03-22 16:26:43 -0400

    sk

--- themes/stormking/views/Details/ca_collections_default_html.php
+++ themes/stormking/views/Details/ca_collections_default_html.php
@@ -135,7 +135,7 @@
 			</div><!-- end row -->
 			<script type="text/javascript">
 				jQuery(document).ready(function() {
-					jQuery("#browseResultsContainer").load("<?php print caNavUrl($this->request, '', 'Search', 'archival', array('search' => 'collection_id:^ca_collections.collection_id'), array('dontURLEncodeParameters' => true)); ?>", function() {
+					jQuery("#browseResultsContainer").load("<?php print caNavUrl($this->request, '', 'Search', 'archival', array('search' => 'collection_id:^ca_collections.collection_id', 'sort' => 'ca_objects.idno'), array('dontURLEncodeParameters' => true)); ?>", function() {
 						jQuery('#browseResultsContainer').jscroll({
 							autoTrigger: true,
 							loadingHtml: '<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>',
