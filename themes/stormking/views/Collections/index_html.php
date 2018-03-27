c09f180c83cc175a13269c9a687d5840d07012aa
sophie <sophie@whirl-i-gig.com>
2018-03-27 10:05:07 -0400

    sk

--- themes/stormking/views/Collections/index_html.php
+++ themes/stormking/views/Collections/index_html.php
@@ -4,10 +4,10 @@
 	$va_collections = array();
 	if($qr_collections && $qr_collections->numHits()) {
 		while($qr_collections->nextHit()) {
-			$va_collections[$qr_collections->get('ca_collections.collection_id')] = array("rank" => $qr_collections->get('ca_collections.collection_rank'), "id" => $qr_collections->get("ca_collections.collection_id"), "media" => $qr_collections->get("ca_object_representations.media.widepreview", array('primaryOnly' => true)), "title" => $qr_collections->get("ca_collections.preferred_labels")); 
-			if (($o_collections_config->get("description_template")) && ($vs_scope = $qr_collections->getWithTemplate($o_collections_config->get("description_template")))) {
-				$va_collections[$qr_collections->get('ca_collections.collection_rank')]["description"] = $vs_scope;
-			}
+			$va_collections[$qr_collections->get('ca_collections.collection_id')] = array("rank" => $qr_collections->get('ca_collections.collection_rank'), "id" => $qr_collections->get("ca_collections.collection_id"), "media" => $qr_collections->get("ca_object_representations.media.widepreview", array('primaryOnly' => true)), "title" => $qr_collections->get("ca_collections.preferred_labels"), "abstract" => $qr_collections->get("ca_collections.abstract")); 
+			#if (($o_collections_config->get("description_template")) && ($vs_scope = $qr_collections->getWithTemplate($o_collections_config->get("description_template")))) {
+			#	$va_collections[$qr_collections->get('ca_collections.collection_rank')]["description"] = $vs_scope;
+			#}
 		}
 	}
 	ksort($va_collections);
@@ -28,11 +28,10 @@
 		foreach($va_collections as $va_collection) {
 			if ( $vn_i == 0) { print "<div class='row'>"; } 
 			print "<div class='col-sm-5'><div class='collectionTile'>";
-			print $va_collection["rank"];
 			print "<div class='colImage'>".caDetailLink($this->request, $va_collection["media"], "", "ca_collections",  $va_collection["id"])."</div>";
 			print "<div class='title'>".caDetailLink($this->request, $va_collection["title"], "", "ca_collections",  $va_collection["id"])."</div>";	
-			if ($va_collection["description"]) {
-				print "<div class='collectionDetail'>".$va_collection["description"]."</div>";
+			if ($va_collection["abstract"]) {
+				print "<div class='collectionDetail'>".$va_collection["abstract"]."</div>";
 			}
 			print "</div></div><div class='col-sm-1'></div>";
 			$vn_i++;
