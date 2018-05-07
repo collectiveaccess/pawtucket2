3db8750da16fc5f7e9d021992a2160a88bf2f95a
Seth <seth@collectiveaccess.org>
2018-05-07 13:18:56 -0400

    PAWX-138 Add "roles" and "access" setting to facet config

--- app/helpers/searchHelpers.php
+++ app/helpers/searchHelpers.php
@@ -34,6 +34,7 @@
 	 *
 	 */
 	require_once(__CA_LIB_DIR__.'/core/Zend/Search/Lucene.php');
+	require_once(__CA_LIB_DIR__.'/core/Search/SearchEngine.php');
 	require_once(__CA_LIB_DIR__.'/core/Search/Common/Parsers/LuceneSyntaxParser.php');
 	require_once(__CA_MODELS_DIR__.'/ca_lists.php');
 
