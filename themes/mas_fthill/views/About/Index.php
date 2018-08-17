a4aafcc2a2611fe911e2a86681d057e594d421d3
sophie <sophie@whirl-i-gig.com>
2018-08-17 15:08:00 -0400

    fthill theme changes

--- themes/mas_fthill/views/About/Index.php
+++ themes/mas_fthill/views/About/Index.php
@@ -9,12 +9,8 @@
 	</div>
 	<div class="row">
 		<div class="col-sm-8">			
-			<b>Introductory to the Museum</b>
-			<p>The F. T. Hill collection is a history of the Riverhurst area. The collection includes ferry information, a rope bed, pioneer tools, sports information-pictures, trophies and uniforms. There are also First Nation artifacts which includes a large arrowhead collection along with First Nation hammers. There is a display of the musical instruments used by the Riverhurst marching band and a working cylinder phonograph. The Museum also has the second largest antique gun collection and all the newspapers published by F.T. Hill from 1914 to 1962 when the newspaper stopped printing.  There is a display of models of what Riverhurst looked like in the early 1920’s along with information on the stores that were present.</p>
-			<p></p>
-			<p>Homesteaders came to Riverhurst in 1905 and 1906 and started to break up the land. In doing so they found an amazing amount of discarded weapons, tools, trader’s articles and Indian artifacts such as stone hammers, knives, spear points, scrapers, etc.…</p>
-			<p>In 1914 the railway came in and Riverhurst was born. The name Riverhurst came from Riverside and Boldenhurst.  Mr. F.T. Hill was one of the first businessmen and he was a printer who started a weekly paper-The Weekly Courier. Hearing about the many things found, he said:” Bring them in and we will display them”.  This started the Museum which originally was in the printing office (ruminants are behind the RM building on Main Street). This collection started in 1914 when Wilbur Nichol brought in a stone hammer. In 1964 the Village Council purchased the collection to preserve it for all time. In 1967 the building on Main Street by the Cenotaph was obtained and in 2002 the collection was moved to the current building which was the old school.</p>
-			<p>The gallery the F. T. Hill Museum would like to feature would be the arrowhead collection</p>
+			{{{about_text}}}
+			<br/><br/>
 		</div>
 		<div class="col-sm-3 col-sm-offset-1">
 				<address>
