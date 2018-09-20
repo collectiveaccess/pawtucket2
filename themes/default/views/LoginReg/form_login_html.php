3e9d9c0516733bd1fa930dfe1ff2be345c398d23
mwbenowitz <mwbenowitz@gmail.com>
2018-09-20 12:32:30 -0400

    NOJIRA Login form autocomplete disable

--- themes/default/views/LoginReg/form_login_html.php
+++ themes/default/views/LoginReg/form_login_html.php
@@ -51,7 +51,7 @@
 				<div class="form-group">
 					<label for="password" class="col-sm-<?php print $vn_label_col; ?> control-label"><?php print _t("Password"); ?></label>
 					<div class="col-sm-7">
-						<input type="password" name="password" class="form-control" id="password" />
+						<input type="password" name="password" class="form-control" id="password" autocomplete="off"/>
 					</div><!-- end col-sm-7 -->
 				</div><!-- end form-group -->
 				<div class="form-group">
