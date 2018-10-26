c2db08c7d87e79ad0729385c3b4f1c5f08a252e4
Seth <seth@collectiveaccess.org>
2018-10-26 08:51:01 -0400

    NOJIRA disable browser autocomplete in header search and login fields

--- themes/default/views/LoginReg/form_login_html.php
+++ themes/default/views/LoginReg/form_login_html.php
@@ -45,13 +45,13 @@
 				<div class="form-group">
 					<label for="username" class="col-sm-<?php print $vn_label_col; ?> control-label"><?php print _t("Username"); ?></label>
 					<div class="col-sm-7">
-						<input type="text" class="form-control" id="username" name="username">
+						<input type="text" class="form-control" id="username" name="username" autocomplete="off" />
 					</div><!-- end col-sm-7 -->
 				</div><!-- end form-group -->
 				<div class="form-group">
 					<label for="password" class="col-sm-<?php print $vn_label_col; ?> control-label"><?php print _t("Password"); ?></label>
 					<div class="col-sm-7">
-						<input type="password" name="password" class="form-control" id="password" />
+						<input type="password" name="password" class="form-control" id="password" autocomplete="off" />
 					</div><!-- end col-sm-7 -->
 				</div><!-- end form-group -->
 				<div class="form-group">
