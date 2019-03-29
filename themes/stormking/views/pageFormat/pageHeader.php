c85bbbfee18af45a6dee1d6afb5fc653838c0e2b
Maria Passarotti <maria@whirl-i-gig.com>
2019-03-29 02:47:43 -0400

    NOJIRA stormking

--- themes/stormking/views/pageFormat/pageHeader.php
+++ themes/stormking/views/pageFormat/pageHeader.php
@@ -54,11 +54,11 @@
 	<head>
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125575750-1"></script>
 	<script>
-//	  window.dataLayer = window.dataLayer || [];
-//	  function gtag(){dataLayer.push(arguments);}
-//	  gtag('js', new Date());
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
  
-//	  gtag('config', 'UA-125575750-1');
+	  gtag('config', 'UA-125575750-1');
 	</script>	
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
