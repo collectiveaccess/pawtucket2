6c109bb6c0598edd248c921b9a159053853027e5
Seth <seth@collectiveaccess.org>
2019-03-29 00:50:42 -0400

    PAWX-167 Add group(s) to users using group code(s)

--- themes/default/views/LoginReg/form_profile_html.php
+++ themes/default/views/LoginReg/form_profile_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2013-2017 Whirl-i-Gig
+ * Copyright 2013-2019 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -37,9 +37,8 @@
 <script type="text/javascript">
 	// initialize CA Utils
 	caUI.initUtils();
-
 </script>
-<div class="row"><div class="col-sm-4"><H1<?php print (!$this->request->isAjax()) ? ' class="text-right"' : ''; ?>><?php print _t("Profile"); ?></H1></div></div>
+<div class="row"><div class="col-sm-4"><H1<?php print (!$this->request->isAjax()) ? ' class="text-right"' : ''; ?>><?php print _t("User information"); ?></H1></div></div>
 
 <?php
 	if($va_errors["general"]){
@@ -78,12 +77,37 @@
 			<label for='password2' class='col-sm-4 control-label'><?php print _t('Re-Type password'); ?></label>
 			<div class="col-sm-7"><input type="password" name="password2" size="40" class="form-control" /></div><!-- end col-sm-7 -->
 		</div><!-- end form-group -->
+		<input type="hidden" name="sum" value="<?php print $vn_sum; ?>">
+
+	<div class="row"><div class="col-sm-4"><H1<?php print (!$this->request->isAjax()) ? ' class="text-right"' : ''; ?>><?php print _t("Groups"); ?></H1></div></div>
+		<div class="form-group<?php print (($va_errors["group_code"]) ? " has-error" : ""); ?>">
+			<label for='group_code' class='col-sm-4 control-label'><?php print _t('Join group'); ?></label>
+			<div class="col-sm-7"><p class="help-block"><?php print _t("If you have been provided with a group code enter it here to join the group."); ?></p><input type="text" name="group_code" size="40" class="form-control"  autocomplete="off" /></div><!-- end col-sm-7 -->
+		</div><!-- end form-group -->
+<?php
+	if (is_array($groups = $t_user->getUserGroups()) && (sizeof($groups) > 0)) {
+?>
 		<div class="form-group">
-			<div class="col-sm-offset-4 col-sm-7">
-				<button type="submit" class="btn btn-default">Save</button>
+			<label class='col-sm-4 control-label'><?php print _t('Group memberships'); ?></label>
+			<div class="col-sm-7">
+				
+				<ul style="list-style: none; padding: 3px;">
+<?php
+					foreach($groups as $group) {
+						print "<li><label>{$group['name']}</label> <blockquote class='help-block'>{$group['description']}</blockquote></li>";
+					}
+?>
+				</ul>
 			</div><!-- end col-sm-7 -->
 		</div><!-- end form-group -->
-		<input type="hidden" name="sum" value="<?php print $vn_sum; ?>">
+<?php
+	}
+?>
+		<div class="form-group">
+			<div class="col-sm-offset-4 col-sm-7">
+				<button type="submit" class="btn btn-default"><?php print _t('Save'); ?></button>
+			</div><!-- end col-sm-7 -->
+		</div><!-- end form-group -->
 	</form>
 <?php
 	if($this->request->isAjax()){
