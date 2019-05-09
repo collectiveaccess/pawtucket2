5245b4d2b8e3640a0837672cc82c8dbf8be6a9df
Maria Passarotti <maria@whirl-i-gig.com>
2019-05-09 07:36:35 -0400

    NOJIRA cooper

--- themes/cooper/views/pageFormat/pageHeader.php
+++ themes/cooper/views/pageFormat/pageHeader.php
@@ -53,6 +53,7 @@
 ?><!DOCTYPE html>
 <html lang="en">
 	<head>
+	<meta name="robots" content="noindex">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
 	<?php print MetaTagManager::getHTML(); ?>
