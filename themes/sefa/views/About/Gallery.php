5bd97e3e177334035bf12f93256fb81491071da8
Maria Passarotti <maria@whirl-i-gig.com>
2019-06-13 13:46:07 -0400

    NOJIRA sefa

--- themes/sefa/views/About/Gallery.php
+++ themes/sefa/views/About/Gallery.php
@@ -8,11 +8,14 @@
 			The Gallery focuses on contemporary art by emerging and mid-career artists, who work in a range of media, from paint to photography to sculpture and print. Solo and group exhibitions showcase abstract and figurative work from a diverse body of artists from the US, Asia, Latin America and Europe. Gallery artists are dynamic, active professionals, who produce strong bodies of work that constantly shift and evolve.
 		</p>
 		<p>
-			In addition to the exhibition program, the Gallery regularly hosts <a href="http://www.vicamillersalons.com/" target="_blank">literary and poetry salons</a>, panel discussions and our special series, “Collector Talks. The Gallery has participated in art fairs throughout the US, in Miami, Houston, San Francisco and New York, and abroad in Toronto. In the past few years the Gallery has developed a strong online presence marketing and selling art through platforms such as <a href="https://artsy.net/susan-eley-fine-art" target="_blank">artsy.net</a> and <a href="https://www.1stdibs.com/dealers/susan-eley-fine-art/?utm_source=susaneleyfineart.com&utm_medium=referral&utm_campaign=dealer" target="_blank">1stdibs.com</a>.
+			In addition to the exhibition program, the Gallery regularly hosts collector talks, political fundraisers, <a href="http://www.vicamillersalons.com/" target="_blank">literary and poetry salons</a> and panel discussions on a variety of cultural and political topics. The Gallery has participated in art fairs in Miami, Houston, San Francisco, New York City and Saratoga Springs, and abroad in Toronto. SEFA has a strong online presence through platforms such as <a href="https://artsy.net/susan-eley-fine-art" target="_blank">artsy.net</a> and <a href="https://www.1stdibs.com/dealers/susan-eley-fine-art/?utm_source=susaneleyfineart.com&utm_medium=referral&utm_campaign=dealer" target="_blank">1stdibs.com</a>.
 		</p>
+		<br/><br/>
+		<H5>Director Bio</H5>
 		<p>
-			Eley has a BA in Art History from Brown University and an MA in Visual Arts Administration from NYU.
+			Before establishing the Gallery, Eley was an editor and writer for national and regional publications featuring articles on fine art, dance and travel. (<a href="http://www.huffingtonpost.com/susan-eley" target="_blank">Author archive</a>) Eley worked in public relations and education at the Morgan Library & Museum, NY, the Mayor's Art Commission of the City of New York and interned at the Peggy Guggenheim Collection in Venice, Italy. She is also a former professional ballet dancer with the Feld Ballet, NY. Eley has a BA in Art History from Brown University and an MA in Visual Arts Administration from NYU.
 		</p>
+		
 		<div class="row" style="margin-top:50px;">
 			<div class="col-xs-6 col-md-3 col-md-offset-2 fullWidthImg">
 				<a href="http://womenartdealers.org/" target="_blank"><?php print caGetThemeGraphic($this->request, 'awad-logo.jpg'); ?></a>
