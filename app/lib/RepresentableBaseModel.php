560f2b467cee7c081f72f4a0f72d9baf482fed04
Seth <seth@collectiveaccess.org>
2019-07-31 12:54:23 -0400

    NOJIRA oops

--- app/lib/RepresentableBaseModel.php
+++ app/lib/RepresentableBaseModel.php
@@ -1083,7 +1083,7 @@
 		
 			$va_media = array();
 			while($qr_res->nextRow()) {
-				$va_media_tags[
+				$va_media_tags = [
 					'representation_id' => $qr_res->get('ca_object_representations.representation_id');
 					'access' => $qr_res->get('ca_object_representations.access')
 				];
