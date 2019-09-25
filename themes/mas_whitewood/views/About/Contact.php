ea34508394f41796ce55a02bc2c3adc802c3280b
Seth <seth@collectiveaccess.org>
2019-09-25 10:16:17 -0400

    Update Whitewood

--- themes/mas_whitewood/views/About/Contact.php
+++ themes/mas_whitewood/views/About/Contact.php
@@ -9,15 +9,17 @@
 	</div>
 	<div class="row">
 		<div class="col-sm-8">
-			<address>Location:<br>
-			107 Administration Pl<br>
-			University of Saskatchewan Campus<br/>
-			Saskatoon, SK S7N 5A2<br/>
-			(306) 966-4571</address>
+			<address><h3>Location</h3>
+			Whitewood Tourism and Heritage Association<br/>
+			P.O. Box 515<br/>
+			904 North Railway Avenue<br/>
+			Whitewood, SK  S0G 5C0
+			</address>
 		
-			<address>Contact:<br/> Blair Barbeau<br>			<span class="info">Phone</span> — (306) 966 4571<br><a href="mailto:blair.barbeau@usask.ca">blair.barbeau@usask.ca</a></address>
+			<address><h3>Contact</h3> 			
+			<a href="mailto:whitewoodmuseum@hotmail.com">whitewoodmuseum@hotmail.com</a>
+			</address>
 		</div>
 		<div class="col-sm-3 col-sm-offset-1">
-			<?php print "<div class='logo'>".caGetThemeGraphic($this->request, 'logo 2013.jpg')."</div>"; ?>
 		</div>
 	</div>
