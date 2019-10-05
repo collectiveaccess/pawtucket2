2368f06a8df94c82503f1d8aae3f1558127665b6
Seth <seth@collectiveaccess.org>
2019-10-05 19:27:08 -0400

    Partial refactor

--- themes/default/js/browse.js
+++ themes/default/js/browse.js
@@ -1,4 +1,6 @@
+/*jshint esversion: 6 */
 'use strict';
+import React from "react"
 const axios = require('axios');
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -64,5 +66,99 @@ function fetchFacetValues(url, callback) {
 		});
 }
 
+/**
+ *
+ * @param criteria
+ * @returns {string}
+ * @private
+ */
+function getCriteriaString(criteria) {
+	let acc = [];
+	for(let k in criteria) {
+		if(criteria[k]) {
+			acc.push(k + ':' + (Object.keys(criteria[k]).join('|')));
+		}
+	}
+	return acc.join(';');
+}
 
-export { initialState, fetchResults, fetchFacetValues };
+/**
+ *
+ */
+function initBrowseContainer(instance, props) {
+	let that = instance;
+	that.state = initialState();
+
+	that.loadResults = function(callback) {
+		let that = this;
+		let offset = (that.state.start + that.state.itemsPerPage);
+		let criteriaString = getCriteriaString(that.state.criteria);
+		fetchResults(that.props.baseUrl + '/' + that.props.endpoint + '/s/' +
+			offset + (that.state.key ? '/key/' + that.state.key : '') + (criteriaString ? '/facets/' +
+				criteriaString : ''), function(newState) {
+			callback(newState);
+		});
+	};
+
+	/**
+	 *
+	 * @param e
+	 */
+	that.loadMoreResults = function(e) {
+		if(that.loadMoreRef && that.loadMoreRef.current) {
+			that.loadMoreText = that.loadMoreRef.current.innerHTML;
+			that.loadMoreRef.current.innerHTML = 'LOADING';
+		}
+
+		that.loadResults(function(newState) {
+			let state = that.state;
+			state.resultList.push(...newState.resultList);
+			state.start += state.itemsPerPage;
+			that.setState(state);
+			that.loadMoreRef.current.innerHTML = that.loadMoreText;
+		});
+		e.preventDefault();
+	};
+
+	/**
+	 *
+	 * @param criteria
+	 */
+	that.reloadResults = function(criteria, clearCriteria=false) {
+		let that = this;
+		let state = that.state;
+
+		if (clearCriteria) {
+			state.criteria = {};
+		}
+
+		for(let k in criteria) {
+			state.criteria[k] = criteria[k];
+		}
+		state.key = null;
+		state.start = 0;
+		that.setState(state);
+		that.loadResults(function(newState) {
+			that.setState(newState);
+		});
+	};
+
+
+	that.loadResults = that.loadResults.bind(that);
+	that.loadMoreResults = that.loadMoreResults.bind(that);
+	that.reloadResults = that.reloadResults.bind(that);
+
+	if(props.initialCriteria) {
+		that.state.criteria = props.initialCriteria;
+	}
+
+	that.loadResults(function(newState) {
+		that.setState(newState);
+	});
+
+
+	that.loadMoreRef = React.createRef();
+}
+
+
+export { initialState, fetchResults, fetchFacetValues, getCriteriaString, initBrowseContainer };
