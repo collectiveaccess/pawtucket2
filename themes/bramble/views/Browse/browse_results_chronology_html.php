8a9c084e24cff4dee9a389959d6c1a201487d8b9
Maria Passarotti <maria@whirl-i-gig.com>
2019-10-28 10:17:31 -0400

    NOJIRA bramble

--- themes/bramble/views/Browse/browse_results_chronology_html.php
+++ themes/bramble/views/Browse/browse_results_chronology_html.php
@@ -64,16 +64,6 @@
 	
 	$va_add_to_set_link_info = caGetAddToSetInfo($this->request);
 	
-		$vn_col_span = 4;
-		$vn_col_span_sm = 4;
-		$vn_col_span_xs = 12;
-		$vb_refine = false;
-		if(is_array($va_facets) && sizeof($va_facets)){
-			$vb_refine = true;
-			$vn_col_span = 6;
-			$vn_col_span_sm = 6;
-			$vn_col_span_xs = 12;
-		}
 		if ($vn_start < $qr_res->numHits()) {
 			$vn_c = 0;
 			$vn_results_output = 0;
@@ -92,6 +82,7 @@
 			}
 			
 			$t_list_item = new ca_list_items();
+			$vn_row = 0;
 			while($qr_res->nextHit()) {
 				if($vn_c == $vn_hits_per_block){
 					if($vb_row_id_loaded){
@@ -100,7 +91,7 @@
 						$vn_c = 0;
 					}
 				}
-				$vn_id 					= $qr_res->get("{$vs_table}.{$vs_pk}");
+				$vn_id 	= $qr_res->get("{$vs_table}.{$vs_pk}");
 				if($vn_id == $vn_row_id){
 					$vb_row_id_loaded = true;
 				}
@@ -110,64 +101,17 @@
 				if(($o_config->get("cache_timeout") > 0) && ExternalCache::contains($vs_cache_key,'browse_result')){
 					print ExternalCache::fetch($vs_cache_key, 'browse_result');
 				}else{
-				
 					$vs_label = trim($qr_res->get("ca_objects.genus")." ".$qr_res->get("ca_objects.species"));
 					$vs_variety = $qr_res->get("ca_objects.variety");
-					$vs_common_name = $qr_res->get("ca_objects.preferred_labels.name");
-					$vs_caption = $qr_res->getWithTemplate("<ifdef code='ca_objects.height'>Height: ^ca_objects.height </ifdef><ifdef code='ca_objects.width'>Width: ^ca_objects.width</ifdef><ifdef code='ca_objects.height|ca_objects.width'><br/></ifdef><ifdef code='light_needs'>Light Needs: ^ca_objects.light_needs%delimiter=,_ <br/></ifdef><ifdef code='water_use'>Water Use: ^ca_objects.water_use%delimiter=,_ <br/></ifdef><ifdef code='ca_objects.soil_type_best|ca_objects.soil_type_tolerates'>Soil Type: <ifdef code='ca_objects.soil_type_best'><unit relativeTo='ca_objects' delimiter=', '>^ca_objects.soil_type_best</unit> (best)</ifdef><ifdef code='ca_objects.soil_type_best|ca_objects.soil_type_tolerates'>; </ifdef><ifdef code='ca_objects.soil_type_tolerates'><unit relativeTo='ca_objects' delimiter=', '>^ca_objects.soil_type_tolerates</unit> (tolerates);</ifdef><br/></ifdef><ifdef code='ca_objects.native_state'>Native to State: <unit relativeTo='ca_objects' delimiter=', '>^ca_objects.native_state</unit><br/></ifdef><ifdef code='ca_objects.native_country'>Native to Country: <unit relativeTo='ca_objects' delimiter=', '>^ca_objects.native_country</unit><br/></ifdef><ifdef code='ca_objects.nativar'>Nativar (Native cultivar): <unit relativeTo='ca_objects' delimiter=', '>^ca_objects.nativar</unit></ifdef>");
-					$vs_label_detail_link 	= caDetailLink($this->request, "<div class='bResultListItemSimpleTitle'><i>".$vs_label.(($vs_variety) ? " ".$vs_variety." " : "")."</i> - ".$vs_common_name."</div>".$vs_caption, '', $vs_table, $vn_id);
-					$vs_thumbnail = "";
-					$vs_type_placeholder = "";
-					$vs_typecode = "";
-					$vs_image = ($vs_table === 'ca_objects') ? $qr_res->getMediaTag("ca_object_representations.media", 'small', array("checkAccess" => $va_access_values)) : $va_images[$vn_id];
-				
-					if(!$vs_image){
-						if ($vs_table == 'ca_objects') {
-							$t_list_item->load($qr_res->get("type_id"));
-							$vs_typecode = $t_list_item->get("idno");
-							if($vs_type_placeholder = caGetPlaceholder($vs_typecode, "placeholder_media_icon")){
-								$vs_image = "<div class='bResultItemImgPlaceholder'>".$vs_type_placeholder."</div>";
-							}else{
-								$vs_image = $vs_default_placeholder_tag;
-							}
-						}else{
-							$vs_image = $vs_default_placeholder_tag;
-						}
-					}
-					$vs_rep_detail_link 	= caDetailLink($this->request, $vs_image, '', $vs_table, $vn_id);	
-				
-					$vs_add_to_set_link = "";
-					if(($vs_table == 'ca_objects') && is_array($va_add_to_set_link_info) && sizeof($va_add_to_set_link_info)){
-						$vs_add_to_set_link = "<a href='#' onclick='caMediaPanel.showPanel(\"".caNavUrl($this->request, '', $va_add_to_set_link_info["controller"], 'addItemForm', array($vs_pk => $vn_id))."\"); return false;' title='".$va_add_to_set_link_info["link_text"]."'>".$va_add_to_set_link_info["icon"]."</a>";
-					}
-				
-					$vs_expanded_info = $qr_res->getWithTemplate($vs_extended_info_template);
-
-					$vs_result_output = "
-		<div class='container'><div class='row'>
-			<div class='col-xs-12'>
-				<div class='bResultListItemSimple' id='row{$vn_id}'>
-					<div class='row'>
-						<div class='col-sm-12 col-md-3 text-center'><div class='bResultListItemImg'>{$vs_rep_detail_link}</div><div class='text-left'>{$vs_add_to_set_link}</div></div>
-						<div class='col-sm-12 col-md-9'>
-							<div class='bSetsSelectMultiple'><input type='checkbox' name='object_ids[]' value='{$vn_id}'></div>
-							<div class='bResultListItemText'>
-								{$vs_label_detail_link}
-							</div><!-- end bResultListItemText -->
-						</div>
-					</div>					
-				</div><!-- end bResultListItemSimple -->
-			</div><!-- end col -->
-		</div></div><!-- end row cotainer-->";
+					$vs_label_detail_link 	= caDetailLink($this->request, "<span class='bResultListItemSimpleTitle'><i>".$vs_label.(($vs_variety) ? " ".$vs_variety." " : "")."</i></span>", '', $vs_table, $vn_id);
+					$vs_color_chart = caColorChart($qr_res);
+					$vs_result_output = "<div class='container'><div class='row'><div class='col-sm-3 btccResultLabel'>".$vs_label_detail_link."</div><div class='col-sm-9'>".$vs_color_chart."</div></div></div>";
 					ExternalCache::save($vs_cache_key, $vs_result_output, 'browse_result', $o_config->get("cache_timeout"));
 					print $vs_result_output;
 				}				
 				$vn_c++;
 				$vn_results_output++;
-
-			}
-			if($vn_row > 0){
-				print "</div></div><!-- end row cotainer-->";
+				$vn_row++;
 			}
 			
 			print "<div style='clear:both'></div>".caNavLink($this->request, _t('Next %1', $vn_hits_per_block), 'jscroll-next', '*', '*', '*', array('s' => $vn_start + $vn_results_output, 'key' => $vs_browse_key, 'view' => $vs_current_view, 'sort' => $vs_current_sort, '_advanced' => $this->getVar('is_advanced') ? 1  : 0));
