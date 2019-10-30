a6b3d077d61dcbb825ae3e2b1b713b3b8a2da11a
Seth <seth@collectiveaccess.org>
2019-10-30 18:33:30 -0400

    Rename var to avoid conflict

--- app/models/ca_ip_bans.php
+++ app/models/ca_ip_bans.php
@@ -164,7 +164,7 @@ class ca_ip_bans extends BaseModel {
 	/**
 	 * Ban config
 	 */
-    static $config = null;
+    static $ban_config = null;
 	
 	# ------------------------------------------------------
 	# --- Constructor
@@ -226,8 +226,8 @@ class ca_ip_bans extends BaseModel {
 	 *
 	 */
 	static public function isWhitelisted($options=null) {
-		if(!self::$config) { self::$config = Configuration::load(__CA_CONF_DIR__.'/ban_hammer.conf'); }
-		if (!is_array($whitelist = self::$config->get('ip_whitelist')) || !sizeof($whitelist)) { return false; }
+		if(!self::$ban_config) { self::$ban_config = Configuration::load(__CA_CONF_DIR__.'/ban_hammer.conf'); }
+		if (!is_array($whitelist = self::$ban_config->get('ip_whitelist')) || !sizeof($whitelist)) { return false; }
 		
 		$request_ip = RequestHTTP::ip();
 		$request_ip_long = ip2long($request_ip);
