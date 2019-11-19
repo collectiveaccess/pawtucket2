98889a614cf01f4394ab1ccc70c316b2ef5e3799
Maria Passarotti <maria@whirl-i-gig.com>
2019-11-19 12:29:26 -0500

    noguchi

--- themes/noguchi/views/Details/ca_occurrences_exhibition_html.php
+++ themes/noguchi/views/Details/ca_occurrences_exhibition_html.php
@@ -121,7 +121,6 @@
 		switch(strToLower($vs_status)){
 			case "published":
 			case "research suspended":
-			default:
 ?>
             <div class="wrap-max-content text-align-center">
 
@@ -257,6 +256,7 @@
 			break;
 			# ----------------------------------------------------
 			case "research pending":
+			default:
 ?>
             <div class="wrap-max-content text-align-center">
 
