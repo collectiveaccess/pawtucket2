d9e48dad814215e43a3308310c360bbb62047097
Seth <seth@whirl-i-gig.com>
2020-01-17 10:42:26 -0500

    Update Pillow theme

--- app/plugins/FindingAid/themes/pillow/views/index_html.php
+++ app/plugins/FindingAid/themes/pillow/views/index_html.php
@@ -21,48 +21,11 @@
 	if ($qr_top_level_collections) {
 		while($qr_top_level_collections->nextHit()) { 
 			$vn_top_level_collection_id = $qr_top_level_collections->get('ca_collections.collection_id');
-			//print $qr_top_level_collections->get('ca_collections.preferred_labels.name')."<br>\n";
-		
-			$va_hierarchy = $t_collection->hierarchyWithTemplate($ps_template, array('collection_id' => $vn_top_level_collection_id, 'sort' => 'ca_collections.preferred_labels'));
-			foreach($va_hierarchy as $vn_i => $va_hierarchy_item) {
-				print "<div class='collHeader' style='margin-left: ".($va_hierarchy_item['level'] * 35)."px'>";
-				if (($va_hierarchy_item['level']) == 0 && ($qr_top_level_collections->get('ca_collections.children.collection_id'))) {
-					print "<a href='#'><i class='fa fa-angle-double-down finding-aid down{$vn_top_level_collection_id}'></i></a>";
-				} elseif ($va_hierarchy_item['level'] == 0) {
-					print "<span style='width:30px; display:inline-block;float:left;height:10px;'></span>";
-				} else {
-					$va_opacity = "style='opacity: .".(90 - ($va_hierarchy_item['level'] * 20))."' ";
-					print "<i class='fa fa-angle-right finding-aid' {$va_opacity}></i>";
-				}
-				print "{$va_hierarchy_item['display']}</div>\n";
-				if ($va_hierarchy_item['level'] == 0) {
-				
-					print "<div class='collBlock".$vn_top_level_collection_id."' ".$vs_hierarchy_style.">";
-?>				
-					<script>
-						$(function() {
-						  $('.down<?php print $vn_top_level_collection_id;?>').click(function() {
-							  if ($('.collBlock<?php print $vn_top_level_collection_id;?>').css('display') == 'none') {
-							  	 $('.down<?php print $vn_top_level_collection_id;?>').css('-webkit-transform', 'rotate(0deg)');
-							     $('.collBlock<?php print $vn_top_level_collection_id;?>').fadeIn("300"); 
-							  } else {
-							  	$('.down<?php print $vn_top_level_collection_id;?>').css('-webkit-transform', 'rotate(180deg)');
-							    $('.collBlock<?php print $vn_top_level_collection_id;?>').fadeOut("300");
-							  }
-							  return false;
-						  });
-						})
-					</script>
-<?php	
-				}	
-				$v_i++;			
-			}
-		print "</div>";
+			print "<div class='collHeader' style='margin-left: 0px'>";
+			print $qr_top_level_collections->getWithTemplate($ps_template, array('collection_id' => $vn_top_level_collection_id, 'sort' => 'ca_collections.preferred_labels'));
+			print "</div>\n";
 		}
 	} else {
 		print _t('No collections available');
 	}
 ?>
-	</div>
-</div>	
-<div class="col-sm-1"></div>	
