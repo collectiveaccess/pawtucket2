0facb7e0817c8891d61b75482e162591e48ed0e4
Maria <maria@whirl-i-gig.com>
2020-03-18 08:26:19 -0400

    comedias

--- themes/pnf/views/Listing/ca_occurrences_listing_subview_html.php
+++ themes/pnf/views/Listing/ca_occurrences_listing_subview_html.php
@@ -73,7 +73,7 @@
 			$vs_first_letter = ucfirst(substr($qr_list->get('ca_occurrences.author'), 0, 1));
 			$va_letter_array[$vs_first_letter] = $vs_first_letter;
 			$vn_id = $qr_list->get('ca_occurrences.occurrence_id');
-			$vs_sort = str_replace(array("á", "é", "í", "è", "ó", "ô", "ú", "ü", "ñ"), array("a", "e", "i", "e", "o", "o", "u", "u", "n"), $qr_list->get('ca_occurrences.author')." ".$qr_list->get('ca_occurrences.preferred_labels'));
+			$vs_sort = strip_tags(str_replace(array("À", "Á", "á", "à", "â", "ã", "Ç", "ç", "È", "É", "Ê", "è", "ê", "é", "Ì", "Í", "Î", "ì", "í", "î", "è", "Ò", "Ó", "ò", "ó", "ô", "õ", "Ü", "ù", "ú", "ü", "Ñ", "ñ"), array("A", "A", "a", "a", "a", "a", "C", "c", "E", "E", "E", "e", "e", "e", "I", "I", "I", "i", "i", "i", "e", "O", "O", "o", "o", "o", "o", "U", "u", "u", "u", "N", "n"), $qr_list->get('ca_occurrences.author')." ".$qr_list->get('ca_occurrences.preferred_labels')));
 			$va_links_array[$vs_first_letter][$vs_sort] = "<div class='listLink listEntry'><span class='listAuthor'>".$qr_list->get('ca_occurrences.author')."&nbsp;</span><span class='listTitle'>".$qr_list->getWithTemplate('<l>^ca_occurrences.preferred_labels</l>')."</span><span class='listPub'>&nbsp;".$qr_list->get('ca_occurrences.publication_info')."</span></div>\n";	
 		}
 		foreach ($va_links_array as $va_first_letter => $va_links) {
