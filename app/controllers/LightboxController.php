5255f08b9775476e98c0e9520299030863ba4128
maria <maria@whirl-i-gig.com>
2020-04-09 08:45:52 -0400

    NOJIRA lightbox and detail media download naming

--- app/controllers/LightboxController.php
+++ app/controllers/LightboxController.php
@@ -578,7 +578,7 @@
 					$t_instance = Datamodel::getInstanceByTableName($vs_subject_table);
 					if(!$qr_res){
 						$qr_res = $vs_subject_table::createResultSet($record_ids);
-# TODO: all media or primary?						$qr_res->filterNonPrimaryRepresentations(false);
+# TODO: all media or primary?	Currenlty only primary					$qr_res->filterNonPrimaryRepresentations(false);
 					}
 					$va_paths = array();
 					while($qr_res->nextHit()) {
@@ -592,25 +592,70 @@
 						if(sizeof($va_original_paths)>0) {
 							$va_paths[$qr_res->get($t_instance->primaryKey())] = array(
 								'idno' => $qr_res->get($t_instance->getProperty('ID_NUMBERING_ID_FIELD')),
-								'paths' => $va_original_paths
+								# --- this path is only the primary rep path
+								'paths' => $va_original_paths,
+								'rep_id' => $qr_res->get("ca_object_representations.representation_id")
 							);
 						}
 					}
 					if (sizeof($va_paths) > 0){
 						$o_zip = new ZipStream();
-
+			
+						$vs_downloaded_file_naming = $this->request->config->get('downloaded_file_naming');
+							
 						foreach($va_paths as $vn_pk => $va_path_info) {
 							$vn_c = 1;
 							foreach($va_path_info['paths'] as $vs_path) {
 								if (!file_exists($vs_path)) { continue; }
-# TODO:make download media file name configurable
-								$vs_filename = $va_path_info['idno'] ? $va_path_info['idno'] : $vn_pk;
-								$vs_filename .= "_{$vn_c}";
-
-								if ($vs_ext = pathinfo($vs_path, PATHINFO_EXTENSION)) {
-									$vs_filename .= ".{$vs_ext}";
+								$vs_idno_proc = preg_replace('![^A-Za-z0-9_\-]+!', '_', $va_path_info['idno']);
+								if(!$vs_idno_proc){
+									$vs_idno_proc = $vn_pk;
 								}
-								$o_zip->addFile($vs_path, $vs_filename);
+								$vs_ext = pathinfo($vs_path, PATHINFO_EXTENSION);
+								switch($vs_downloaded_file_naming) {
+									case 'idno':
+										$vs_file_name = $vs_idno_proc.'_'.$vn_c.'.'.$vs_ext;
+										break;
+									case 'idno_and_version':
+										$vs_file_name = $vs_idno_proc.'_'.$vs_media_version.'_'.$vn_c.'.'.$vs_ext;
+										break;
+									case 'idno_and_rep_id_and_version':
+										$vs_file_name = $vs_idno_proc.'_representation_'.$va_path_info['rep_id'].'_'.$vs_media_version.'.'.$vs_ext;
+										break;
+									case 'original_name':
+									default:
+										$t_rep = new ca_object_representations($va_path_info['rep_id']);
+										$va_info = $t_rep->getMediaInfo('media');
+										$va_rep_info = $t_rep->getMediaInfo('media', $vs_media_version);
+										$vals = ['idno' => $vs_idno_proc];
+										foreach(array_merge($va_rep_info, $va_info) as $k => $v) {
+											if (is_array($v)) { continue; }
+											if (strtolower($k) == 'original_filename') { $v = pathinfo($v, PATHINFO_FILENAME); }
+											$vals[strtolower($k)] = preg_replace('![^A-Za-z0-9_\-]+!', '_', $v);
+										}
+
+										if (strpos($vs_downloaded_file_naming, "^") !== false) { // template
+											$vs_file_name = caProcessTemplate($vs_downloaded_file_naming, $vals);
+										} elseif ($va_info['ORIGINAL_FILENAME']) {
+											$va_tmp = explode('.', $va_info['ORIGINAL_FILENAME']);
+											if (sizeof($va_tmp) > 1) { 
+												if (strlen($vs_ext = array_pop($va_tmp)) < 3) {
+													$va_tmp[] = $vs_ext;
+												}
+											}
+											$vs_file_name = join('_', $va_tmp); 					
+										} else {
+											$vs_file_name = $vs_idno_proc.'_representation_'.$va_path_info['rep_id'].'_'.$vs_media_version;
+										}
+						
+										if (isset($va_file_names[$vs_file_name.'.'.$vs_ext])) {
+											$vs_file_name.= "_{$vn_c}";
+										}
+										$vs_file_name .= '.'.$vs_ext;
+										break;
+								}			
+								
+								$o_zip->addFile($vs_path, $vs_file_name);
 
 								$vn_c++;
 							}
