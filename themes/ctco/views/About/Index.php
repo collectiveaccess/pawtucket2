9b1af842c92d7d5756a7da8149cd8023f63a8174
CollectiveAccess <seth@collectiveaccess.org>
2020-09-22 12:28:31 -0400

    Update CTCO theme

--- themes/ctco/views/About/Index.php
+++ themes/ctco/views/About/Index.php
@@ -1,14 +1,32 @@
 <?php
 	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": About");
+	require_once(__CA_MODELS_DIR__."/ca_entities.php");
 ?>
 <div class="container"><div class="row"><div class="col-sm-12">
 
 <H1><?php print _t("About"); ?></H1>
 <div class="row">
 	<div class="col-sm-12">
-		<p>Connecticut Collections (CTCo) is a project of the Connecticut League of History Organizations and includes a growing number of participating organizations. A customization of <a href='http://www.collectiveaccess.org' target='_blank'>Collective Access software</a>, CTCo provides organizations with a tool to help them privately manage and publicly share their collections. In turn, CTCo helps organizations build internal capacity, and by providing public access, builds awareness of Connecticut history and heritage.</p>
-		<p>For more information, please visit <a href='http://clho.org/ctcollections/' target='_blank'>clho.org.</a></p>
+		{{{about_page}}}
+		<?php print "<div style='width:300px;'>".caGetThemeGraphic($this->request, 'cthumanities.jpg')."</div>"; ?>
 	</div>
 </div>
+<hr/ style='margin-top:30px;'>
+<div class="row" style='padding-bottom:40px;margin-top:0px;margin-bottom:40px; '>
+	<div class="col-sm-12"><h2 style='margin-bottom:25px;'>Participating Institutions</h2></div>
+<?php
+$qr_members = ca_entities::find(['type_id' => 'member'], ['returnAs' => 'searchResult']);
+if ($qr_members) {
+	while ($qr_members->nextHit()) {
+		print "<div class='col-sm-4 col-lg-3'>
+					<div class='frontTile'>";
+		print "<div class='tileImageWrapper'><div class='tileImage'>".caDetailLink($this->request, $qr_members->get('ca_object_representations.media.small'), '', 'ca_entities', $qr_members->get('ca_entities.entity_id'))."</div></div>";
+		print "<div class='titleCaption'>".$qr_members->get('ca_entities.preferred_labels', array('returnAsLink' => true))."</div>";
+		print "	</div>
+			</div>";
+	}
+}
+?>									
+</div><!-- end row -->	
 
 </div></div></div>
