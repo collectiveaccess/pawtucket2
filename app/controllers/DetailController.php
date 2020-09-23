700900b840cdf7c635a5abe647d9c7d5c1389b0c
CollectiveAccess <seth@collectiveaccess.org>
2020-09-23 10:46:17 -0400

    NOJIRA Don't look for type-specific view when no view is set

--- app/controllers/DetailController.php
+++ app/controllers/DetailController.php
@@ -477,15 +477,17 @@
  			// find view
  			//		first look for type-specific view
  			$vs_path = "Details/{$vs_table}_default_html.php";		// If no type specific view use the default
- 			if (is_array($va_type_codes = caMakeTypeList($vs_table, [$t_subject->getTypeCode()]))) {
- 			    $va_type_codes = array_merge($va_type_codes, caMakeTypeList($vs_table, $t_subject->getTypeInstance()->getHierarchyAncestors($t_subject->getTypeID(), ['idsOnly' => true]), ['dontIncludeSubtypesInTypeRestriction' => true]));
-                foreach(array_reverse($va_type_codes) as $vs_type_code) {   // reverse list to try more specific types first
-                    if ($this->viewExists("Details/{$vs_table}_{$vs_type_code}_html.php")) {
-                        $vs_path = "Details/{$vs_table}_{$vs_type_code}_html.php";
-                        break;
-                    }
-                }
-            }
+ 			if ($subject_type_code = $t_subject->getTypeCode()) {
+				if (is_array($va_type_codes = caMakeTypeList($vs_table, [$subject_type_code]))) {
+					$va_type_codes = array_merge($va_type_codes, caMakeTypeList($vs_table, $t_subject->getTypeInstance()->getHierarchyAncestors($t_subject->getTypeID(), ['idsOnly' => true]), ['dontIncludeSubtypesInTypeRestriction' => true]));
+					foreach(array_reverse($va_type_codes) as $vs_type_code) {   // reverse list to try more specific types first
+						if ($this->viewExists("Details/{$vs_table}_{$vs_type_code}_html.php")) {
+							$vs_path = "Details/{$vs_table}_{$vs_type_code}_html.php";
+							break;
+						}
+					}
+				}
+			}
             
  			// inquire link
  			//
