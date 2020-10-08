ab1d9825cbcb36a45960c19396d3d1572e3f9857
Maria <maria@whirl-i-gig.com>
2020-10-08 07:23:11 -0400

    NOJIRA irshdc

--- themes/irshdc/views/Browse/browse_results_html.php
+++ themes/irshdc/views/Browse/browse_results_html.php
@@ -114,7 +114,7 @@ if($vs_detailNav = $this->request->getParameter("detailNav", pString)){
 									<ul class="dropdown-menu" role="menu">
 <?php
 										foreach($va_facets[$vs_filter_facet]["content"] as $vn_item_id => $va_item){
-											print '<li><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', 'Search', 'objects', array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'facet' => $vs_filter_facet, 'id' => $va_item['id'], 'view' => $vs_current_view), array('dontURLEncodeParameters' => true)).'\'); return false;">'.$va_item["label"].'</a></li>';
+											print '<li><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', $this->request->getController(), $this->request->getAction(), array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'facet' => $vs_filter_facet, 'id' => $va_item['id'], 'view' => $vs_current_view), array('dontURLEncodeParameters' => true)).'\'); return false;">'.$va_item["label"].'</a></li>';
 										}
 ?>
 									</ul>
@@ -134,13 +134,13 @@ if($vs_detailNav = $this->request->getParameter("detailNav", pString)){
 								if ($vs_current_sort === $vs_sort) {
 									print "<li><a href='#' onClick='return false;'><em>{$vs_sort}</em></a></li>\n";
 								} else {
-									print '<li><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', 'Search', 'objects', array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'sort' => $vs_sort, 'view' => $vs_current_view), array('dontURLEncodeParameters' => true)).'\'); return false;">'.$vs_sort.'</a></li>';
+									print '<li><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', $this->request->getController(), $this->request->getAction(), array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'sort' => $vs_sort, 'view' => $vs_current_view), array('dontURLEncodeParameters' => true)).'\'); return false;">'.$vs_sort.'</a></li>';
 								}
 							}
 							print "<li class='divider'></li>\n";
 							print "<li class='dropdown-header'>"._t("Sort order:")."</li>\n";
-							print '<li><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', 'Search', 'objects', array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'sort' => $vs_current_sort, 'view' => $vs_current_view, 'direction' => 'asc'), array('dontURLEncodeParameters' => true)).'\'); return false;">'.(($vs_sort_dir == 'asc') ? '<em>' : '')._t("Ascending").(($vs_sort_dir == 'asc') ? '</em>' : '').'</a></li>';
-							print '<li><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', 'Search', 'objects', array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'sort' => $vs_current_sort, 'view' => $vs_current_view, 'direction' => 'desc'), array('dontURLEncodeParameters' => true)).'\'); return false;">'.(($vs_sort_dir == 'desc') ? '<em>' : '')._t("Descending").(($vs_sort_dir == 'desc') ? '</em>' : '').'</a></li>';
+							print '<li><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', $this->request->getController(), $this->request->getAction(), array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'sort' => $vs_current_sort, 'view' => $vs_current_view, 'direction' => 'asc'), array('dontURLEncodeParameters' => true)).'\'); return false;">'.(($vs_sort_dir == 'asc') ? '<em>' : '')._t("Ascending").(($vs_sort_dir == 'asc') ? '</em>' : '').'</a></li>';
+							print '<li><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', $this->request->getController(), $this->request->getAction(), array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'sort' => $vs_current_sort, 'view' => $vs_current_view, 'direction' => 'desc'), array('dontURLEncodeParameters' => true)).'\'); return false;">'.(($vs_sort_dir == 'desc') ? '<em>' : '')._t("Descending").(($vs_sort_dir == 'desc') ? '</em>' : '').'</a></li>';
 ?>
 						</ul>
 					</div>
@@ -156,7 +156,7 @@ if($vs_detailNav = $this->request->getParameter("detailNav", pString)){
 					# --- check if type criteria has been selected
 					foreach($va_criteria as $va_facet_criteria){
 						if (!in_array($va_facet_criteria['facet_name'], array("detail_entity", "detail_occurrence", "detail_place"))) {
-							print '<div class="btn-group"><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', 'Search', 'objects', array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'view' => $vs_current_view, 'sort' => $vs_current_sort, 'removeCriterion' => $va_facet_criteria['facet_name'], 'removeID' => urlencode($va_facet_criteria['id'])), array('dontURLEncodeParameters' => true)).'\'); return false;"><button class="btn btn-default">'.$va_facet_criteria["facet"].": ".str_replace("Texts ➜ ", "", $va_facet_criteria["value"]).' <span class="glyphicon glyphicon-remove-circle"></span></button></a></div>';
+							print '<div class="btn-group"><a href="#" onClick="loadDetailResults(\''.caNavUrl($this->request, '', $this->request->getController(), $this->request->getAction(), array('detailNav' => $vs_detailNav, 'key' => $vs_browse_key, 'view' => $vs_current_view, 'sort' => $vs_current_sort, 'removeCriterion' => $va_facet_criteria['facet_name'], 'removeID' => urlencode($va_facet_criteria['id'])), array('dontURLEncodeParameters' => true)).'\'); return false;"><button class="btn btn-default">'.$va_facet_criteria["facet"].": ".str_replace("Texts ➜ ", "", $va_facet_criteria["value"]).' <span class="glyphicon glyphicon-remove-circle"></span></button></a></div>';
 						}
 					}
 ?>
