4412a346f3fbbe59038cd906f38ff350686d8dce
Maria <maria@whirl-i-gig.com>
2020-10-09 21:04:30 -0400

    NOJIRA heartland

--- themes/heartland/views/Details/ca_entities_default_html.php
+++ themes/heartland/views/Details/ca_entities_default_html.php
@@ -18,20 +18,12 @@
 			<div class="row">
 				<div class='col-md-12 col-lg-12'>
 					<H1>{{{^ca_entities.preferred_labels.displayname}}}</H1>
-					<H2>{{{^ca_entities.type_id}}}</H2>
 				</div><!-- end col -->
 			</div><!-- end row -->
 			<div class="row">			
 				<div class='col-sm-12'>
 					{{{<ifdef code="ca_entities.nonpreferred_labels.displayname"><div class='unit'><label>Alternate Names</label>^ca_entities.nonpreferred_labels.displayname%delimiter=<br/> </div></ifdef>}}}
-					{{{<ifdef code="ca_entities.birthdate|ca_entities.deathdate"><div class="unit"><label>Lifespan</label>^ca_entities.birthdate-^ca_entities.deathdate</div></if>}}}
-					{{{<ifdef code="ca_entities.biography">
-							<label>Biography</label>
-							<span class="trimText">^ca_entities.biography</span>
-						</div>
-					</ifdef>}}}
-					{{{<ifcount code="ca_places" min="1"><div class="unit"><label>Related place<ifcount code="ca_places" min="2">s</ifcount></label><unit relativeTo="ca_entities_x_places" delimiter="<br/>">
-						<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit> (^relationship_typename)</unit></div></ifcount>}}}
+					
 				</div><!-- end col -->
 			</div><!-- end row -->
 			
