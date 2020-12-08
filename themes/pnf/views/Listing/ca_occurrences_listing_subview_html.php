1c47cbb3820f1cb7e8392de00ecf5389f5245f56
Maria <maria@whirl-i-gig.com>
2020-12-08 17:17:46 -0500

    NOJIRA comedias pnf

--- themes/pnf/views/Listing/ca_occurrences_listing_subview_html.php
+++ themes/pnf/views/Listing/ca_occurrences_listing_subview_html.php
@@ -134,7 +134,7 @@
 		if(!$qr_list) { continue; }
 		while($qr_list->nextHit()) {
 			$vn_id = $qr_list->get('ca_occurrences.occurrence_id');
-			$vs_sort = strToLower(strip_tags(str_replace(array(",", ".", "\"", "“", "”", "La ", "El ", "Los ", "Las ", "À", "Á", "á", "à", "â", "ã", "Ç", "ç", "È", "É", "Ê", "è", "ê", "é", "Ì", "Í", "Î", "ì", "í", "î", "è", "Ò", "Ó", "ò", "ó", "ô", "õ", "Ü", "ù", "ú", "ü", "Ñ", "ñ", "Š", "š"), array("", "", "", "", "", "", "", "", "", "A", "A", "a", "a", "a", "a", "C", "c", "E", "E", "E", "e", "e", "e", "I", "I", "I", "i", "i", "i", "e", "O", "O", "o", "o", "o", "o", "U", "u", "u", "u", "N", "n", "S", "s"), trim(strip_tags($qr_list->get('ca_occurrences.author')." ".$qr_list->get('ca_occurrences.preferred_labels'))))));
+			$vs_sort = strToLower(strip_tags(str_replace(array(",", ".", "\"", "“", "”", "La ", "El ", "Los ", "Las ", "Del ", "À", "Á", "á", "à", "â", "ã", "Ç", "ç", "È", "É", "Ê", "è", "ê", "é", "Ì", "Í", "Î", "ì", "í", "î", "è", "Ò", "Ó", "ò", "ó", "ô", "õ", "Ü", "ù", "ú", "ü", "Ñ", "ñ", "Š", "š"), array("", "", "", "", "", "", "", "", "", "", "A", "A", "a", "a", "a", "a", "C", "c", "E", "E", "E", "e", "e", "e", "I", "I", "I", "i", "i", "i", "e", "O", "O", "o", "o", "o", "o", "U", "u", "u", "u", "N", "n", "S", "s"), trim(strip_tags($qr_list->get('ca_occurrences.author')." ".$qr_list->get('ca_occurrences.preferred_labels'))))));
 			$vs_first_letter = ucfirst(substr($vs_sort, 0, 1));
 			$va_letter_array[$vs_first_letter] = $vs_first_letter;
 			$va_links_array[$vs_first_letter][$vs_sort] = "<div class='listLink listEntry listEntryIndentSecondLine'>".(($qr_list->get('ca_occurrences.author')) ? "<span class='listAuthor'>".$qr_list->get('ca_occurrences.author')."</span> " : "")."<span class='listTitle'>".$qr_list->getWithTemplate('<l>^ca_occurrences.preferred_labels</l>')."</span><span class='listPub'>&nbsp;".$qr_list->get('ca_occurrences.publication_info')."</span>".(($qr_list->get('ca_occurrences.internal_notes')) ? " ".$qr_list->get('ca_occurrences.internal_notes') : "")."</div>\n";	
@@ -145,6 +145,56 @@
 			ksort($va_links);
 			print "<p class='separator'><a name='".$vs_first_letter."'></a><br></p>";			
 			print "<h2 id='".$va_first_letter."' class='mw-headline'>".$va_first_letter."</h2>";
+			if($vs_action == "modern_editions"){
+				switch(strToLower($va_first_letter)){
+					# ----------------------
+					case "b":
+	?>
+						<div class="listingLetterIntro">
+							<p>
+								<b>Bances Candamo items contributed by Blanca Oteiza Pérez.</b>
+							</p>
+							<p>
+								En el teatro de Bances Candamo, que se representa entre 1685 y 1697, fecha de la primera y última representación documentadas de sus comedias, las comedias sueltas del XVIII tienen importancia capital, pues su teatro se publicó en dos volúmenes titulados <i>Poesías cómicas</i> (1722), la primera y hasta ahora única colección bastante completa de su teatro, que reúne treinta piezas dramáticas, contando el teatro breve, diecisiete en el tomo primero y trece en el segundo.
+							</p>
+							<p>
+								Los datos del primer tomo son: <i>Poesías cómicas</i>. Obras póstumas de D. Francisco Bances Candamo, año 1722, con privilegio en Madrid por Blas de Villanueva, impresor de libros en la calle Hortaleza, a costa de Joseph Antonio Pimentel, mercader de libros en la Puerta del Sol, véndese en su casa. Y del segundo tomo: <i>Poesías cómicas</i>. Obras póstumas de D. Francisco Bances Candamo, año 1722, con privilegio en Madrid, por Lorenzo Francisco Mojados, impresor de libros en la calle del Olivo Alta, a costa de Joseph Antonio Pimentel, mercader de libros en la Puerta del Sol, véndese en su casa.
+							</p>
+							<p>
+								La edición de <i>Poesías cómicas</i> se basa en textos contemporáneos sin precisar, que las ediciones críticas modernas van reconociendo, en especial los de prensas del XVIII, que lamentablemente en muchas ocasiones no presentan ningún dato de identificación: por ejemplo, en la transmisión del <i>Entremés El astrólogo tunante</i> se registra una suelta de Diego López de Haro (Impreso, en Sevilla, en la Imprenta Real de D. Diego López de Haro, en la calle de Génova); en la de la comedia <i>Quién es quien premia al amor</i>, se constata una suelta, sin año, impresa en Sevilla por Lucas Martín de Hermosilla 1685-1719], según consta en el colofón, y otra suelta, impresa sin año en Sevilla, por Francisco de Leefdael, en la casa del Correo Viejo, lugar en que aparecen sus impresiones a partir de 1717.
+							</p>
+						</div>
+	<?php
+					break;
+					case "c":
+	?>
+						<div class="listingLetterIntro"><p><b>Calderón items contributed by Don W. Cruickshank.</b></p></div>
+	<?php				
+					break;
+					# ----------------------
+					case "t":
+	?>
+						<div class="listingLetterIntro">
+							<p>
+								<b>Tirso items contributed by Blanca Oteiza Pérez.</b>
+							</p>
+							<p>
+								En el caso de Tirso, la transmisión textual de sus comedias a partir de los textos princeps y algunos manuscritos, tienen un eslabón fundamental en ediciones del XVIII, en concreto las de Teresa de Guzmán, que edita entre 1733-1736 (o 1737) 32 comedias de Tirso y el auto El colmenero divino en forma de sueltas que posteriormente se publican en 3 volúmenes. Coe  ha fechado 31 de estas comedias sueltas a partir de los anuncios de los periódicos madrileños, como La Gaceta, en los que se daba noticia de las ediciones de Guzmán no más tarde de diez a quince días de su publicación. Ver Ada M. Coe, <i>Catálogo bibliográfico y crítico de las comedias anunciadas en los periódicos de Madrid, desde 1661 hasta 1819</i>, Baltimore, The Johns Hopkins Press, 1935, y Alice H. Bushee, «The <i>Guzmán</i> edition of Tirso de Molina's Comedias», <i>Hispanic Review</i>, 5, 1937, pp. 25-39.
+							</p>
+						</div>
+	<?php				
+					break;
+					# ----------------------
+					case "v":
+	?>
+						<div class="listingLetterIntro">
+							<b>Luis Vélez de Guevara items contributed by C. George Peale.</b>
+						</div>
+	<?php				
+					break;
+					# ----------------------
+				}
+			}
 			foreach ($va_links as $vn_i => $va_link) {
 				print $va_link;
 			}
