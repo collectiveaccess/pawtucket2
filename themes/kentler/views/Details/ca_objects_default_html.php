b47bc2bfc054bd8b23b8d7c612364612dbb791b5
Maria <maria@whirl-i-gig.com>
2021-01-06 06:50:58 -0500

    NOJIRA kentler

--- themes/kentler/views/Details/ca_objects_default_html.php
+++ themes/kentler/views/Details/ca_objects_default_html.php
@@ -1,10 +1,16 @@
 <?php
-	# --- detail for audio/video records
-	
 	$t_object = $this->getVar("item");
 	$va_comments = $this->getVar("comments");
-	$va_access_values = caGetUserAccessValues($this->request);
-
+	$vb_removed = false;
+	if(strtolower($t_object->get("ca_objects.removed.removal_text", array("convertCodesToDisplayText" => true))) == "yes"){
+		$vb_removed = true;
+	}
+	if($t_object->get("ca_objects.is_deaccessioned")){
+		$vb_removed = true;
+	}
+	if($t_object->get("ca_entities.entity_id", array("restrictToRelationshipTypes" => array("sold")))){
+		$vb_removed = true;
+	}
 ?>
 <div class="row">
 	<div class='col-xs-12 navTop'><!--- only shown at small screen size -->
@@ -13,74 +19,66 @@
 	<div class='col-xs-12 col-sm-12'>
 		<div class="row">
 			<div class='col-sm-4'>
-<?php			
-				$va_object_ids = $t_object->get("ca_objects.related.object_id", array("returnAsArray" => true, "checkAccess" => $va_access_values, "sort" => "ca_entities.preferred_labels.surname"));
-				$q_artworks = caMakeSearchResult("ca_objects", $va_object_ids);
-				if($q_artworks->numHits()){
-					print "<br/><br/><br/><H6>Related Artwork</H6>";
-					while($q_artworks->nextHit()){
-						$vs_image = "";
-						$vs_image = $q_artworks->get('ca_object_representations.media.large', array("checkAccess" => $va_access_values));
-						$vb_no_rep = false;
-						if(!$vs_image){
-							$vs_image = "<div class='dontScale detailPlaceholder'>".caGetThemeGraphic($this->request, 'KentlerLogoWhiteBG.jpg')."</div>";
-							$vb_no_rep = true;
-						}
-						$vs_caption = "";
-						if($vs_artist = $q_artworks->get('ca_entities.preferred_labels.displayname', array("restrictToRelationshipTypes" => array("artist"), 'checkAccess' => $va_access_values))){
-							$vs_caption = $vs_artist.", ";
-						}
-						$vs_caption .= "<i>".$q_artworks->get("ca_objects.preferred_labels.name")."</i>, ";
-						$vs_medium = "";
-						if($q_artworks->get("medium_text")){
-							$vs_medium = $q_artworks->get("medium_text");
-						}else{
-							if($q_artworks->get("medium")){
-								$vs_medium .= $q_artworks->get("medium", array("delimiter" => ", ", "convertCodesToDisplayText" => true));
-							}
-						}
-						if($vs_medium){
-							$vs_caption .= $vs_medium.", ";
-						}					
-						if($q_artworks->get("ca_objects.dimensions")){
-							$vs_caption .= $q_artworks->get("ca_objects.dimensions.dimensions_height")." X ".$q_artworks->get("ca_objects.dimensions.dimensions_width").", ";
-						}
-						if($q_artworks->get("ca_objects.date")){
-							$vs_caption .= $q_artworks->get("ca_objects.date").".";
-						}
-						$vs_label_detail_link 	= caDetailLink($this->request, $vs_caption, '', 'ca_objects', $q_artworks->get("ca_objects.object_id"));
-						$tmp = array("image" => $vs_image, "label" => $vs_label_detail_link, "image_link" => ($vb_no_rep) ? $vs_image : "<a href='#' onclick='caMediaPanel.showPanel(\"".caNavUrl($this->request, '', 'Detail', 'GetMediaOverlay', array('context' => 'objects', 'id' => $q_artworks->get("ca_objects.object_id"), 'representation_id' => $q_artworks->get("ca_object_representations.representation_id", array("checkAccess" => $va_access_values)), 'overlay' => 1))."\"); return false;' >".$vs_image."</a>");
-					
-						print '<div class="fullWidthImg">'.$tmp["image_link"];
-						if($tmp["label"]){
-							print "<br/><small>".$tmp["label"]."</small>";
-						}
-						print "</div>";
-					}
-				}
-?>					
 				{{{representationViewer}}}
 				
 				<?php print caObjectRepresentationThumbnails($this->request, $this->getVar("representation_id"), $t_object, array("returnAs" => "bsCols", "linkTo" => "carousel", "bsColClasses" => "smallpadding col-sm-3 col-md-3 col-xs-4")); ?>
-			
+<?php
+				if($vb_removed && $this->getVar("representation_id")){
+					print "<div class='text-right'><small>No longer available</small></div>";
+				}
+?>
 			</div><!-- end col -->
 			
 			<div class='col-sm-6'>
-				<H1>{{{ca_objects.preferred_labels.name}}}</H1>
-				<H4>{{{<unit relativeTo="ca_entities" delimiter=", " restrictToRelationshipTypes="creator"><l>^ca_entities.preferred_labels.displayname</l></unit>}}}</H4>
+				<H1><span class="ltgrayText">Flatfile Artwork</span><br/>{{{ca_objects.preferred_labels.name}}}</H1>
+				<H4>{{{<unit relativeTo="ca_entities" delimiter=", " restrictToRelationshipTypes="artist"><l>^ca_entities.preferred_labels.displayname</l></unit>}}}</H4>
 				<HR/>
-
+<?php
+				$vs_medium = "";
+				if($t_object->get("medium_text")){
+					$vs_medium = $t_object->get("medium_text");
+				}else{
+					#if($t_object->get("category")){
+					#	$vs_medium = $t_object->get("category", array("delimiter" => ", ", "convertCodesToDisplayText" => true));
+					#}
+					#if($t_object->get("category") && $t_object->get("medium")){
+					#	$vs_medium .= " > ";
+					#}
+					if($t_object->get("medium")){
+						$vs_medium .= $t_object->get("medium", array("delimiter" => ", ", "convertCodesToDisplayText" => true));
+					}
+				}
+				if($vs_medium){
+					print "<H6>Medium</H6>".$vs_medium."<br/>";
+				}
+?>
 				{{{<ifdef code="ca_objects.date"><H6>Date</H6>^ca_objects.date<br/></ifdev>}}}
-				
-				{{{<ifdef code="ca_objects.description">^ca_objects.description<br/></ifdev>}}}
-				
-				{{{<ifcount code="ca_entities" min="1" max="1"><HR/><H6>Related person</H6></ifcount>
-					<ifcount code="ca_entities" min="2"><HR/><H6>Related people</H6></ifcount>
-					<unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit>}}}
-				
+				{{{<ifdef code="ca_objects.dimensions.dimensions_height|ca_objects.dimensions.dimensions_width"><H6>Dimensions</H6></ifdef><ifdef code="ca_objects.dimensions.dimensions_height">^ca_objects.dimensions.dimensions_height X </ifdef><ifdef code="ca_objects.dimensions.dimensions_width">^ca_objects.dimensions.dimensions_width<br/></ifdef>}}}
+<?php
+				if($vb_removed && !$this->getVar("representation_id")){
+					print "<br/>No longer available<br/>";
+				}
+?>	
+
+				{{{<ifdef code="ca_objects.artwork_tags"><HR/><H6>Tags</H6><unit delimiter=", ">^ca_objects.artwork_tags</unit><br/></ifdef>}}}
+
+<?php
+				$vs_collections = $t_object->get("ca_collections");
+				if(!(stripos($vs_collections, "Red Hook") === False)){
+					print "<H6>Part of the Red Hook Archives</H6>";
+				}
+?>
 				{{{<ifcount code="ca_occurrences" min="1" max="1"><HR/><H6>Related exhibition</H6></ifcount>}}}
 				{{{<ifcount code="ca_occurrences" min="2"><HR/><H6>Related exhibitions</H6></ifcount>}}}
 				{{{<unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_occurrences.preferred_labels.name</l></unit>}}}
+				
+				{{{<ifcount code="ca_objects.related" restrictToTypes="audio,video" min="1"><HR/><H6>Related Audio/Video</H6></ifcount>
+					<unit relativeTo="ca_objects.related" restrictToTypes="audio,video" delimiter="<br/>"><l>^ca_objects.preferred_labels.name</l></unit>}}}
+<?php
+				if(!$vb_removed && $t_object->get("price")){
+					#print "<HR/><H6>Price</H6>".$t_object->get("price")."<br/>";
+				}
+?>
 			</div><!-- end col -->
 			<div class='navLeftRight col-sm-2'>
 				<div class="detailNavBgRight">
