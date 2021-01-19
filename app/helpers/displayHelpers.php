5caf334aa84270b33eccab93370d2853c53f1982
CollectiveAccess <seth@collectiveaccess.org>
2021-01-19 09:15:41 -0500

    NOJIRA Fix incorrect calling pattern

--- app/helpers/displayHelpers.php
+++ app/helpers/displayHelpers.php
@@ -4162,7 +4162,7 @@ require_once(__CA_LIB_DIR__.'/Media/MediaInfoCoder.php');
 	 *
 	 */
 	function caGetMediaAnnotationList($po_data, $pa_options=null) {
-		$va_detail_config = caGetDetailConfig();
+		$va_detail_config = caGetDetailConfig()->get($po_data->tableName());
 		$ps_annotation_display_template 	= caGetOption('displayAnnotationTemplate', $pa_options, caGetOption('displayAnnotationTemplate', $va_detail_config['options'], '^ca_representation_annotations.preferred_labels.name'));
 		$ps_display_type		 			= caGetOption('display', $pa_options, false);
 
@@ -4209,7 +4209,7 @@ require_once(__CA_LIB_DIR__.'/Media/MediaInfoCoder.php');
 			throw new ApplicationException(_t('Invalid identifier %1', $ps_identifier));
 		}
 
-		$va_detail_config = caGetDetailConfig();
+		$va_detail_config = caGetDetailConfig()->get($pt_subject->tableName());
 
 		$ps_display_type 					= caGetOption('display', $pa_options, 'media_overlay');
 		$pb_inline 							= (bool)caGetOption('inline', $pa_options, false);
