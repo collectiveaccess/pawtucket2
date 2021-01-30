e712ee3c0ec6ed02ac2cf679f08d7fc0f0dbe0ac
Maria <maria@whirl-i-gig.com>
2021-01-29 19:24:33 -0500

    NOJIRA comedias pnf

--- themes/pnf/views/About/Statistics.php
+++ themes/pnf/views/About/Statistics.php
@@ -42,48 +42,48 @@ $qr_authors = $o_browse_authors->getResults();
 
 
 ?>
-<div id="statistics">
-	<H2>Statistical Table</H2>
-	<div class="statisticsIntro">The statistical information in this table is generated automatically by the CollectiveAccess database.  At the completion of the survey, we expect to see large data sets that may be analyzed computationally to reveal patterns, trends, and associations, and that we hope will lead to research, especially in Spanish 18th-century printing industry, in ways that were not possible previously.</div>
-	<div class="row">
-		<div class="col-sm-12 col-md-offset-1 col-md-10">
+<div class="row">
+	<div class="col-sm-12 col-md-offset-1 col-md-10">
+		<div id="statistics">
+			<H2>Statistical Table</H2>
+			<div class="statisticsIntro">{{{statistics_intro}}}</div>
 			<hr/>
 			<div class="row">
 				<div class="col-sm-3"><p><b>INSTITUTIONS</b><br/>in public view</p></div>
-				<div class="col-sm-3"><p>Running Tally<br/><?php print $q_institutions_public->numRows(); ?></p></div>
-				<div class="col-sm-6"><p>INSTITUTIONS list those libraries in public view that we have identified as having collections of sueltas. These appear in alphabetical order under INSTITUTION on the Navigation bar, also in Browse.</p></div>
+				<div class="col-sm-2"><p>Running Tally<br/><?php print $q_institutions_public->numRows(); ?></p></div>
+				<div class="col-sm-7"><p>{{{statistics_inst_public}}}</p></div>
 			</div>
 			<hr/>
 			<div class="row">
 				<div class="col-sm-3"><p><b>INSTITUTIONS</b><br/>in protected view</p></div>
-				<div class="col-sm-3"><p>Running Tally<br/><?php print $q_institutions_protected->numRows(); ?></p></p></div>
-				<div class="col-sm-6"><p>UNDER CONSTRUCTION / EN OBRAS: These are libraries in our pipeline in varying stages of completion. Some lack sufficient information or need images before they are ready to be uploaded.</p></div>
+				<div class="col-sm-2"><p>Running Tally<br/><?php print $q_institutions_protected->numRows(); ?></p></p></div>
+				<div class="col-sm-7"><p>{{{statistics_inst_protected}}}</p></div>
 			</div>
 			<hr/>
 			<div class="row">
 				<div class="col-sm-3"><p><b>NUMBER OF SUELTAS</b><br/>in each institution</p></div>
-				<div class="col-sm-3">
+				<div class="col-sm-2">
 					<p>Running Tally<br/><?php print $q_sueltas_public->numRows(); ?> </p>
 				</div>
-				<div class="col-sm-6"><p>The number of sueltas held by a library appears as soon as an Institution is selected. Numbers may diminish if we discover a post-1833 imprint or some other reason for eliminating an item.</p></div>
+				<div class="col-sm-7"><p>{{{statistics_number_sueltas}}}</p></div>
 			</div>
 			<hr/>
 			<div class="row">
-				<div class="col-sm-3"><p><b>AUTHORS</b><br/>including translators and adaptors</p></div>
-				<div class="col-sm-3"><p>Running Tally<br/><?php print $qr_authors->numHits(); ?></p></div>
-				<div class="col-sm-6"><p>Each name in the Author’s Authority File is listed with a single main entry. These main entries are based on the Library of Congress Name Authority File (NACO), Biblioteca Nacional de España Autoridades, and other national standards. They are listed as they appear in the Virtual Internet Authority File (VIAF.) Variant forms of the name will be listed under the main entry, separated by a semi-colon. The distinction between author and translator, or author and adaptor, can be fuzzy. This list also includes a few dozen French, Italian, English, and German writers whose works appear in translation.</p></div>
+				<div class="col-sm-3"><p><b>AUTHORS</b><br/>including translators<br/>and adaptors</p></div>
+				<div class="col-sm-2"><p>Running Tally<br/><?php print $qr_authors->numHits(); ?></p></div>
+				<div class="col-sm-7"><p>{{{statistics_authors}}}</p></div>
 			</div>
 			<hr/>
 			<div class="row">
-				<div class="col-sm-3"><p><b>PRINTERS</b><br/>including publishers and booksellers</p></div>
-				<div class="col-sm-3"><p>Running Tally<br/><?php print $qr_printers->numHits(); ?></p></div>
-				<div class="col-sm-6"><p>Each name in the Printers’ Authority File is listed with a single main entry. These main entries are based on the Library of Congress Name Authority File (NACO), Biblioteca Nacional de España Autoridades, and other national standards. They are listed as they appear in the Virtual Internet Authority File (VIAF.) Variant forms of the name will be listed under the main entry, separated by a semi-colon. In the period covered in this database, the role of printers, publishers, and booksellers often overlapped, and was not well defined.</p></div>
+				<div class="col-sm-3"><p><b>PRINTERS</b><br/>including publishers<br/>and booksellers</p></div>
+				<div class="col-sm-2"><p>Running Tally<br/><?php print $qr_printers->numHits(); ?></p></div>
+				<div class="col-sm-7"><p>{{{statistics_printers}}}</p></div>
 			</div>
 			<hr/>
 			<div class="row">
 				<div class="col-sm-3"><p><b>TITLES</b></p></div>
-				<div class="col-sm-3"><p>Running Tally<br/><?php print sizeof($va_titles); ?></p></div>
-				<div class="col-sm-6"><p>Titles can be tracked in TITLES MODERNIZED under the RESOURCES menu. These are not firm numbers because titles are not unique.  The same title had been used by two or more authors, for example, both Pérez de Montalbán and Calderón wrote plays entitled Los Hijos de la fortuna. The titles La cruz en la sepultura and La devoción de la cruz refer to the same play by Calderón. Or the simple alteration of Calderón’s Nunca lo peor es cierto for a less categorical No siempre lo peor es cierto. Some of these plays rely on comparison of first and last lines to ascertain proper identification. Over the time sueltas were printed, titles were also modified, truncated, misprinted, corrupted, or simply changed in any number of ways. </p></div>
+				<div class="col-sm-2"><p>Running Tally<br/><?php print sizeof($va_titles); ?></p></div>
+				<div class="col-sm-7"><p>{{{statistics_titles}}}</p></div>
 			</div>
 			<hr/>
 		</div>
