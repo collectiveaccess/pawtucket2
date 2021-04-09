dc6c6dc748edbb0b61939433a1759c56e9b6a22b
Maria <maria@whirl-i-gig.com>
2021-04-09 15:46:04 -0400

    NOJIRA lamama

--- themes/lamama/views/Details/ca_collections_default_html.php
+++ themes/lamama/views/Details/ca_collections_default_html.php
@@ -43,46 +43,13 @@
 					{{{<ifcount code="ca_entities" min="1"><div class="unit"><label>Related <ifcount code="ca_entities" min="1" max="1">person</ifcount><ifcount code="ca_entities" min="2">people</ifcount></label>
 						<span class="trimText"><unit relativeTo="ca_entities" delimiter=", "><l>^ca_entities.preferred_labels (^relationship_typename)</l></unit></span>
 					</div></ifcount>}}}
-					{{{<ifcount code="ca_occurrences" min="1" restrictToTypes="work"><div class="unit"><label>Related work<ifcount code="ca_occurrences" min="2" restrictToTypes="work">s</ifcount></label>
-						<span class="trimText"><unit relativeTo="ca_occurrences" delimiter=", " restrictToTypes="work"><l>^ca_occurrences.preferred_labels</l></unit></span>
-					</ifcount>}}}
 					{{{<ifcount code="ca_occurrences" min="1" restrictToTypes="production"><div class="unit"><label>Related production<ifcount code="ca_occurrences" min="2" restrictToTypes="production">s</ifcount></label>
 						<span class="trimText"><unit relativeTo="ca_occurrences" delimiter=", " restrictToTypes="production"><l>^ca_occurrences.preferred_labels</l></unit></span>
 					</div></ifcount>}}}
 					{{{<ifcount code="ca_occurrences" min="1" restrictToTypes="special_event"><div class="unit"><label>Related special event<ifcount code="ca_occurrences" min="2" restrictToTypes="special_event">s</ifcount></label>
 						<span class="trimText"><unit relativeTo="ca_occurrences" delimiter=", " restrictToTypes="special_event"><l>^ca_occurrences.preferred_labels</l></unit></span>
 					</div></ifcount>}}}
-					{{{<ifcount code="ca_occurrences" min="1" restrictToTypes="lamama_venues"><div class="unit"><label>Related venue<ifcount code="ca_occurrences" min="2" restrictToTypes="lamama_venues">s</ifcount></label>
-						<span class="trimText"><unit relativeTo="ca_occurrences" delimiter=", " restrictToTypes="lamama_venues"><l>^ca_occurrences.preferred_labels</l></unit></span>
-					</div></ifcount>}}}
-					{{{<ifcount code="ca_occurrences" min="1" restrictToTypes="off_site"><div class="unit"><label>Related offsite venue<ifcount code="ca_occurrences" min="2" restrictToTypes="off_site">s</ifcount></label>
-						<span class="trimText"><unit relativeTo="ca_occurrences" delimiter=", " restrictToTypes="off_site"><l>^ca_occurrences.preferred_labels</l></unit></span>
-					</div></ifcount>}}}
-					{{{<ifcount code="ca_occurrences" min="1" restrictToTypes="tours"><div class="unit"><label>Related tour<ifcount code="ca_occurrences" min="2" restrictToTypes="tours">s</ifcount></label>
-						<span class="trimText"><unit relativeTo="ca_occurrences" delimiter=", " restrictToTypes="tours"><l>^ca_occurrences.preferred_labels</l></unit></span>
-					</div></ifcount>}}}
-<?php
-					$va_all_subjects = array();
-				
-					foreach(array("lcshNames", "lcshTopical", "lcshGeo") as $vs_field){
-						$va_lc = $t_item->get("ca_collections.".$vs_field, array("returnAsArray" => true));
-						$va_lc_names_processed = array();
-						if(is_array($va_lc) && sizeof($va_lc)){
-							foreach($va_lc as $vs_lc_terms){
-								if($vs_lc_terms){
-									$vs_lc_term = "";
-									if($vs_lc_terms && (strpos($vs_lc_terms, " [") !== false)){
-										$vs_lc_term = mb_substr($vs_lc_terms, 0, strpos($vs_lc_terms, " ["));
-									}
-									$va_all_subjects[] = caNavLink($this->request, $vs_lc_term, "", "", "Search", "objects", array("search" => "ca_objects.".$vs_field.": ".$vs_lc_term));
-								}
-							}
-						}
-					}
-					if(is_array($va_all_subjects) && sizeof($va_all_subjects)){
-						print "<div class='unit'><label>Subjects</label>".join(", ", $va_all_subjects)."</div>";
-					}
-?>
+
 				</div><!-- end col -->
 			</div><!-- end row -->
 {{{<ifcount code="ca_objects" min="1">
