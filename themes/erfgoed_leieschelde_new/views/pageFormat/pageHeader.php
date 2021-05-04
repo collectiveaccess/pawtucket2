0c2c9619b5cc36ab7ed0567dee293a607d9b2b8c
Maria <maria@whirl-i-gig.com>
2021-05-04 18:05:23 -0400

    NOJIRA erfgoed_leieschelde

---
+++ themes/erfgoed_leieschelde_new/views/pageFormat/pageHeader.php
@@ -0,0 +1,223 @@
+<?php
+/* ----------------------------------------------------------------------
+ * views/pageFormat/pageHeader.php : 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2014-2017 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * ----------------------------------------------------------------------
+ */
+	$va_lightboxDisplayName = caGetLightboxDisplayName();
+	$vs_lightbox_sectionHeading = ucFirst($va_lightboxDisplayName["section_heading"]);
+	$va_classroomDisplayName = caGetClassroomDisplayName();
+	$vs_classroom_sectionHeading = ucFirst($va_classroomDisplayName["section_heading"]);
+	
+	# Collect the user links: they are output twice, once for toggle menu and once for nav
+	$va_user_links = array();
+	if($this->request->isLoggedIn()){
+		$va_user_links[] = '<li role="presentation" class="dropdown-header">'.trim($this->request->user->get("fname")." ".$this->request->user->get("lname")).', '.$this->request->user->get("email").'</li>';
+		$va_user_links[] = '<li class="divider nav-divider"></li>';
+		if(caDisplayLightbox($this->request)){
+			$va_user_links[] = "<li>".caNavLink($this->request, $vs_lightbox_sectionHeading, '', '', 'Lightbox', 'Index', array())."</li>";
+		}
+		if(caDisplayClassroom($this->request)){
+			$va_user_links[] = "<li>".caNavLink($this->request, $vs_classroom_sectionHeading, '', '', 'Classroom', 'Index', array())."</li>";
+		}
+		$va_user_links[] = "<li>".caNavLink($this->request, _t('User Profile'), '', '', 'LoginReg', 'profileForm', array())."</li>";
+		$va_user_links[] = "<li>".caNavLink($this->request, _t('Logout'), '', '', 'LoginReg', 'Logout', array())."</li>";
+	} else {	
+		if (!$this->request->config->get(['dontAllowRegistrationAndLogin', 'dont_allow_registration_and_login']) || $this->request->config->get('pawtucket_requires_login')) { $va_user_links[] = "<li><a href='#' onclick='caMediaPanel.showPanel(\"".caNavUrl($this->request, '', 'LoginReg', 'LoginForm', array())."\"); return false;' >"._t("Login")."</a></li>"; }
+		if (!$this->request->config->get(['dontAllowRegistrationAndLogin', 'dont_allow_registration_and_login']) && !$this->request->config->get('dontAllowRegistration')) { $va_user_links[] = "<li><a href='#' onclick='caMediaPanel.showPanel(\"".caNavUrl($this->request, '', 'LoginReg', 'RegisterForm', array())."\"); return false;' >"._t("Register")."</a></li>"; }
+		$va_user_links[] = "<li><a href='#' onclick='caMediaPanel.showPanel(\"".caNavUrl($this->request, '', 'About', 'userTools', array())."\"); return false;' >"._t("Hoe werkt dit?")."</a></li>";
+	}
+	$vb_has_user_links = (sizeof($va_user_links) > 0);
+	$va_access_values = caGetUserAccessValues($this->request);
+
+?><!DOCTYPE html>
+<html lang="en" <?php print ((strtoLower($this->request->getController()) == "front")) ? "class='frontContainer initial'" : ""; ?>>
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
+	
+	<meta property="og:url" content="<?php print $this->request->config->get("site_host").caNavUrl($this->request, "*", "*", "*"); ?>" />
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="<?php print htmlspecialchars((MetaTagManager::getWindowTitle()) ? MetaTagManager::getWindowTitle() : $this->request->config->get("app_display_name")); ?>" />
+	<meta property="og:title" content="<?php print $this->request->config->get("app_display_name"); ?>" />
+<?php
+	# --- what should the image to share be?
+	# --- default to logo --- use image from detail page if on object page
+	$vs_og_image = $this->request->config->get("site_host").caGetThemeGraphicUrl($this->request, 'LEIE_SCHELDE_Logo.jpg'); # --- replace this with logos for institutions
+	if((strToLower($this->request->getController()) == "detail") && (strToLower($this->request->getAction()) == "objects")){
+		$ps_id = str_replace("~", "/", urldecode($this->request->getActionExtra()));
+		$vs_use_alt_identifier_in_urls = caUseAltIdentifierInUrls("ca_objects");
+		$t_subject = new ca_objects();
+		if ((($vb_use_identifiers_in_urls = caUseIdentifiersInUrls()) || ($vs_use_alt_identifier_in_urls)) && (substr($ps_id, 0, 3) == "id:")) {
+			$va_tmp = explode(":", $ps_id);
+			$ps_id = (int)$va_tmp[1];
+			$vb_use_identifiers_in_urls = $vs_use_alt_identifier_in_urls = false;
+		}
+
+		if($vs_use_alt_identifier_in_urls && $t_subject->hasElement($vs_use_alt_identifier_in_urls)) {
+			$va_load_params = [$vs_use_alt_identifier_in_urls => $ps_id];
+		} elseif ($vb_use_identifiers_in_urls && $t_subject->getProperty('ID_NUMBERING_ID_FIELD')) {
+			$va_load_params = [$t_subject->getProperty('ID_NUMBERING_ID_FIELD') => $ps_id];
+		} else {
+			$va_load_params = [$t_subject->primaryKey() => (int)$ps_id];
+		}
+		
+		if (!($t_subject = call_user_func_array($t_subject->tableName().'::find', array($va_load_params, ['returnAs' => 'firstModelInstance'])))) {
+			// invalid id - throw error
+			throw new ApplicationException("Invalid id");
+		}else{
+			if($vs_rep = $t_subject->get("ca_object_representations.media.medium.url", array("checkAccess" => $va_access_values))){
+				$vs_og_image = $vs_rep;
+			}
+			
+		}
+	}
+?>
+	<meta property="og:image" content="<?php print $vs_og_image; ?>" />
+
+	
+	<?php print MetaTagManager::getHTML(); ?>
+    <link rel="stylesheet" type="text/css" href="<?php print $this->request->getAssetsUrlPath(); ?>/mirador/css/mirador-combined.css">
+	<?php print AssetLoadManager::getLoadHTML($this->request); ?>
+
+	<title><?php print (MetaTagManager::getWindowTitle()) ? MetaTagManager::getWindowTitle() : $this->request->config->get("app_display_name"); ?></title>
+	
+	<script type="text/javascript">
+		jQuery(document).ready(function() {
+    		jQuery('#browse-menu').on('click mouseover mouseout mousemove mouseenter',function(e) { e.stopPropagation(); });
+    	});
+	</script>
+<?php
+	if(Debug::isEnabled()) {		
+		//
+		// Pull in JS and CSS for debug bar
+		// 
+		$o_debugbar_renderer = Debug::$bar->getJavascriptRenderer();
+		$o_debugbar_renderer->setBaseUrl(__CA_URL_ROOT__.$o_debugbar_renderer->getBaseUrl());
+		print $o_debugbar_renderer->renderHead();
+	}
+?>
+ <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;1,400&display=swap" rel="stylesheet"> 
+<?php
+	$vs_body_class = "";
+	switch(strtoLower($this->request->getController())){
+		case "contact":
+			$vs_body_class = "bgGray";
+		break;
+		# -------------------------------------------
+		case "search":
+			if(strtoLower($this->request->getAction()) == "advanced"){
+				$vs_body_class = "bgGray";
+			}
+		break;
+		# -------------------------------------------
+		case "front":
+			$vs_body_class = "frontContainer";
+		break;
+		# -------------------------------------------
+	}
+?>
+<body class='<?php print $vs_body_class; ?>'>
+	<div id="skipNavigation"><a href="#main">Skip to main content</a></div>
+	<nav class="navbar navbar-default yamm" role="navigation">
+		<div class="container menuBar">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+<?php
+	if ($vb_has_user_links) {
+?>
+				<button type="button" class="navbar-toggle navbar-toggle-user" data-toggle="collapse" data-target="#user-navbar-toggle">
+					<span class="sr-only">User Options</span>
+					<span class="glyphicon glyphicon-user"></span>
+				</button>
+<?php
+	}
+?>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-main-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+<?php
+				print caNavLink($this->request, caGetThemeGraphic($this->request, 'leiescheldeLogo.png'), "navbar-brand", "", "","");
+?>
+			</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+			<!-- bs-user-navbar-collapse is the user menu that shows up in the toggle menu - hidden at larger size -->
+<?php
+	if ($vb_has_user_links) {
+?>
+			<div class="collapse navbar-collapse" id="user-navbar-toggle" aria-label="<?php print _t("Mobile User Navigation"); ?>">
+				<ul class="nav navbar-nav">
+					<?php print join("\n", $va_user_links); ?>
+				</ul>
+			</div>
+<?php
+	}
+?>
+			<div class="collapse navbar-collapse" id="bs-main-navbar-collapse-1">
+<?php
+	if ($vb_has_user_links) {
+?>
+				<ul class="nav navbar-nav navbar-right" id="user-navbar" role="list" aria-label="<?php print _t("User Navigation"); ?>">
+					<li class="dropdown" style="position:relative;">
+						<a href="#" class="dropdown-toggle icon" data-toggle="dropdown"><span class="glyphicon glyphicon-user" aria-label="<?php print _t("User options"); ?>"></span></a>
+						<ul class="dropdown-menu" role="list"><?php print join("\n", $va_user_links); ?></ul>
+					</li>
+				</ul>
+<?php
+	}
+?>
+				<form class="navbar-form navbar-right" role="search" action="<?php print caNavUrl($this->request, '', 'MultiSearch', 'Index'); ?>" aria-label="<?php print _t("Search"); ?>">
+					<div class="formOutline">
+						<div class="form-group">
+							<input type="text" class="form-control" id="headerSearchInput" placeholder="Zoeken" name="search" aria-label="<?php print _t("Search text"); ?>" >
+						</div>
+						<button type="submit" class="btn-search" id="headerSearchButton"><span class="glyphicon glyphicon-search" aria-label="<?php print _t("Submit"); ?>"></span></button>
+					</div>
+					<div class="headerAdvancedSearch"><?php print caNavLink($this->request, "uitgebreid zoeken", "", "", "Search", "advanced/objects"); ?></div>
+				</form>
+				<script type="text/javascript">
+					$(document).ready(function(){
+						$('#headerSearchButton').prop('disabled',true);
+						$('#headerSearchInput').keyup(function(){
+							$('#headerSearchButton').prop('disabled', this.value == "" ? true : false);     
+						})
+					});
+				</script>
+				<ul class="nav navbar-nav navbar-right menuItems" role="list" aria-label="<?php print _t("Primary Navigation"); ?>">
+					<li <?php print ($this->request->getController() == "Gallery") ? 'class="active"' : ''; ?>><?php print caNavLink($this->request, "Expo's", "", "", "Gallery", "Index"); ?></li>
+					<li <?php print ($this->request->getController() == "Collections") ? 'class="active"' : ''; ?>><?php print caNavLink($this->request, "Collecties", "", "", "Collections", "index"); ?></li>					
+					<?php print $this->render("pageFormat/browseMenu.php"); ?>	
+					<li <?php print ($this->request->getController() == "Overons") ? 'class="active"' : ''; ?>><?php print caNavLink($this->request, "Over ons", "", "", "Overons", ""); ?></li>
+					<li <?php print ($this->request->getController() == "Contact") ? 'class="active"' : ''; ?>><?php print caNavLink($this->request, "Contact", "", "", "Contact", "form"); ?></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- end container -->
+	</nav>
+	<div role="main" id="main"><div id="pageArea" <?php print caGetPageCSSClasses(); ?>><div class="container"><div class="row"><div class="col-xs-12">
+		
