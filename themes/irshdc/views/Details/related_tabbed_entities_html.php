5eb59c1f014b5ed655b47ac9194617ef6fb95642
Maria <maria@whirl-i-gig.com>
2021-06-01 18:22:58 -0400

    fixed relationship typename display of related entities on entity detail

--- themes/irshdc/views/Details/related_tabbed_entities_html.php
+++ themes/irshdc/views/Details/related_tabbed_entities_html.php
@@ -5,8 +5,37 @@
 					# --- get all related authority records for tabbed presentation
 					$vs_rel_communities = $vs_rel_places = $vs_rel_entities = $vs_rel_events = $vs_rel_exhibitions;
 					$vs_rel_places = $t_item->getWithTemplate('<ifcount code="ca_places.related" min="1"><div class="row relTab" id="relPlaces"><unit relativeTo="ca_places" delimiter=" "><div class="col-sm-12 col-md-3"><div class="placePlaceholder"><span>^ca_places.preferred_labels.name (^relationship_typename)</span></div></div></unit></div></ifcount>', array("checkAccess" => $va_access_values));
-					$vs_rel_entities = $t_item->getWithTemplate('<ifcount code="ca_entities.related" excludeTypes="school,community" min="1"><div class="row relTab" id="relEntities"><unit relativeTo="ca_entities_x_entities" excludeTypes="school,community" delimiter=" "><div class="col-sm-12 col-md-3"><l><span><unit relativeTo="ca_entities.related">^ca_entities.preferred_labels.displayname</unit> (^relationship_typename<ifdef code="relationshipDate">, ^relationshipDate</ifdef>)</l></div></unit></div></ifcount>', array("checkAccess" => $va_access_values));
-					$vs_rel_communities = $t_item->getWithTemplate('<ifcount code="ca_entities.related" restrictToTypes="community" min="1"><div class="row relTab" id="relCommunities"><unit relativeTo="ca_entities_x_entities" restrictToTypes="community" delimiter=" "><div class="col-sm-12 col-md-3"><l><span><unit relativeTo="ca_entities.related">^ca_entities.preferred_labels.displayname</unit> (^relationship_typename<ifdef code="relationshipDate">, ^relationshipDate</ifdef>)</l></div></unit></div></ifcount>', array("checkAccess" => $va_access_values));
+					#$vs_rel_entities = $t_item->getWithTemplate('<ifcount code="ca_entities.related" excludeTypes="school,community" min="1"><div class="row relTab" id="relEntities"><unit relativeTo="ca_entities_x_entities" excludeTypes="school,community" delimiter=" "><div class="col-sm-12 col-md-3"><l><span><unit relativeTo="ca_entities.related">^ca_entities.preferred_labels.displayname</unit> (^relationship_typename<ifdef code="relationshipDate">, ^relationshipDate</ifdef>)</span></l></div></unit></div></ifcount>', array("checkAccess" => $va_access_values));
+					$vs_rel_entities = "";
+					$va_rel_entities = $t_item->get("ca_entities.related", array("returnWithStructure" => true, "checkAccess" => $va_access_values, "excludeTypes" => array("school", "community")));
+					if(is_array($va_rel_entities) && sizeof($va_rel_entities)){
+						$vs_rel_entities .= '<div class="row relTab" id="relEntities">';
+						foreach($va_rel_entities as $va_rel_entity){
+							$t_relation = new ca_entities_x_entities($va_rel_entity["relation_id"]);
+							$vs_rel_date = "";
+							if($t_relation->get("relationshipDate")){
+								$vs_rel_date = ", ".$t_relation->get("relationshipDate");
+							}
+							$vs_rel_entities .= '<div class="col-sm-12 col-md-3">'.caDetailLink($this->request, '<span>'.$va_rel_entity['displayname'].' ('.$va_rel_entity["relationship_typename"].$vs_rel_date.')</span>', '', 'ca_entities', $va_rel_entity["entity_id"]).'</div>';
+						}
+						$vs_rel_entities .= '</div>';
+					}
+					#$vs_rel_communities = $t_item->getWithTemplate('<ifcount code="ca_entities.related" restrictToTypes="community" min="1"><div class="row relTab" id="relCommunities"><unit relativeTo="ca_entities_x_entities" restrictToTypes="community" delimiter=" "><div class="col-sm-12 col-md-3"><l><span><unit relativeTo="ca_entities.related">^ca_entities.preferred_labels.displayname</unit> (^relationship_typename<ifdef code="relationshipDate">, ^relationshipDate</ifdef>)</span></l></div></unit></div></ifcount>', array("checkAccess" => $va_access_values));
+					$vs_rel_communities = "";
+					$va_rel_communities = $t_item->get("ca_entities.related", array("returnWithStructure" => true, "checkAccess" => $va_access_values, "restrictToTypes" => array("community")));
+					if(is_array($va_rel_communities) && sizeof($va_rel_communities)){
+						$vs_rel_communities .= '<div class="row relTab" id="relCommunities">';
+						foreach($va_rel_communities as $va_rel_community){
+							$t_relation = new ca_entities_x_entities($va_rel_community["relation_id"]);
+							$vs_rel_date = "";
+							if($t_relation->get("relationshipDate")){
+								$vs_rel_date = ", ".$t_relation->get("relationshipDate");
+							}
+							$vs_rel_communities .= '<div class="col-sm-12 col-md-3">'.caDetailLink($this->request, '<span>'.$va_rel_community['displayname'].' ('.$va_rel_community["relationship_typename"].$vs_rel_date.')</span>', '', 'ca_entities', $va_rel_community["entity_id"]).'</div>';
+						}
+						$vs_rel_communities .= '</div>';
+					}
+					
 					$vs_rel_events = $t_item->getWithTemplate('<ifcount code="ca_occurrences.related" restrictToTypes="institutional" min="1"><div class="row relTab" id="relEvents"><unit relativeTo="ca_occurrences" restrictToTypes="institutional" delimiter=" "><div class="col-sm-12 col-md-3"><l><span>^ca_occurrences.preferred_labels.name (^relationship_typename)</span></l></div></unit></div></ifcount>', array("checkAccess" => $va_access_values));
 					$vs_rel_exhibitions = $t_item->getWithTemplate('<ifcount code="ca_occurrences.related" restrictToTypes="exhibitions" min="1"><div class="row relTab" id="relExhibitions"><unit relativeTo="ca_occurrences" restrictToTypes="exhibitions" delimiter=" "><div class="col-sm-12 col-md-3"><l><span>^ca_occurrences.preferred_labels.name (^relationship_typename)</span></l></div></unit></div></ifcount>', array("checkAccess" => $va_access_values));
 					# --- rel_object is just for outputting label properly
