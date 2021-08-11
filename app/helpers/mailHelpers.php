27f0b78869dd22bd624da80d7d1f90f5d707e408
Seth <seth@collectiveaccess.org>
2021-08-11 12:49:45 -0400

    Remove Zend refs

--- app/helpers/mailHelpers.php
+++ app/helpers/mailHelpers.php
@@ -37,9 +37,6 @@
  	require_once(__CA_LIB_DIR__.'/Configuration.php');
  	require_once(__CA_LIB_DIR__.'/View.php');
  	require_once(__CA_LIB_DIR__.'/Logging/Eventlog.php');
- 	require_once(__CA_LIB_DIR__.'/Zend/Mail.php');
- 	require_once(__CA_LIB_DIR__.'/Zend/Mail/Transport/Smtp.php');
-	require_once(__CA_LIB_DIR__.'/Zend/Mail/Transport/Sendmail.php');
  	
  	# ------------------------------------------------------------------------------------------------
  	/**
