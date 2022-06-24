c42147b07df7a41a07a1c1de5a8f6e8d87c029e5
CollectiveAccess <seth@collectiveaccess.org>
2022-06-24 16:30:04 -0400

    Fix typo

--- themes/default/views/Details/ca_entities_default_html.php
+++ themes/default/views/Details/ca_entities_default_html.php
@@ -50,7 +50,6 @@
 					{{{<ifcount code="ca_objects" min="2"><H6>Related objects</H6></ifcount>}}}
 					{{{<unit relativeTo="ca_objects"><l>^ca_objects.preferred_labels.name</l> (^relationship_typename)</unit>}}}
 
-
 					{{{<ifcount code="ca_collections" min="1" max="1"><H6>Related collection</H6></ifcount>}}}
 					{{{<ifcount code="ca_collections" min="2"><H6>Related collections</H6></ifcount>}}}
 					{{{<unit relativeTo="ca_collections"><l>^ca_collections.preferred_labels.name</l> (^relationship_typename)</unit>}}}
