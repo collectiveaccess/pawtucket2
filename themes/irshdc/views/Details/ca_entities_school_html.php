7422bd5ec6091253c33c690be63c39a29c5b33a5
Maria <maria@whirl-i-gig.com>
2022-08-30 17:53:14 -0400

    icons on school links

--- themes/irshdc/views/Details/ca_entities_school_html.php
+++ themes/irshdc/views/Details/ca_entities_school_html.php
@@ -177,10 +177,10 @@ if($vs_mode == "map"){
 						print '<div class="detailTool"><span class="glyphicon glyphicon-share-alt"></span>'.$this->getVar("shareLink").'</div><!-- end detailTool -->';
 					}
 					if($vs_url = $t_item->get("ca_entities.moreRecordsUrl")){
-						print "<div class='detailTool'><a href='".$vs_url."'>Find More Records</a></div>";
+						print "<div class='detailTool'><a href='".$vs_url."'><i class='fa fa-archive' aria-hidden='true'></i>Find More Records</a></div>";
 					}
 					if($vs_url = $t_item->get("ca_entities.resourcesUrl")){
-						print "<div class='detailTool'><a href='".$vs_url."'>Resources</a></div>";
+						print "<div class='detailTool'><a href='".$vs_url."'><i class='fa fa-list-ul' aria-hidden='true'></i>Resources</a></div>";
 					}
 					print "<div class='detailTool'><span class='glyphicon glyphicon-envelope'></span>".caNavLink($this->request, "Ask a Question", "", "", "Contact", "Form", array("contactType" => "askArchivist", "table" => "ca_entities", "row_id" => $t_item->get("entity_id")))."</div>";
 					print '</div><!-- end detailTools -->';			
