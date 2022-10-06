7132d1cc34286963df057abc63d34046978c5e8f
Maria <maria@whirl-i-gig.com>
2022-10-06 18:44:09 -0400

    kentler

--- themes/kentler/views/LoginReg/form_login_html.php
+++ themes/kentler/views/LoginReg/form_login_html.php
@@ -80,7 +80,7 @@
 <script>
 // Set the date we're counting down to
 var countDownDate = new Date("Oct 15, 2022 09:00:00").getTime();
-var countDownDate = new Date("Oct 6, 2022 18:36:00").getTime();
+//var countDownDate = new Date("Oct 6, 2022 18:36:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
