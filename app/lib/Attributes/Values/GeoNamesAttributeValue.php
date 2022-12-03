8be9ea8b68ad5eeb4a481db5d38a5020ba4c0bde
CollectiveAccess <seth@collectiveaccess.org>
2022-12-03 11:47:02 -0500

    Reformat

--- app/lib/Attributes/Values/GeoNamesAttributeValue.php
+++ app/lib/Attributes/Values/GeoNamesAttributeValue.php
@@ -33,19 +33,19 @@
   /**
   *
   */
-  define("__CA_ATTRIBUTE_VALUE_GEONAMES__", 14);
+define("__CA_ATTRIBUTE_VALUE_GEONAMES__", 14);
   
- require_once(__CA_LIB_DIR__.'/Configuration.php');
- require_once(__CA_LIB_DIR__.'/Attributes/Values/IAttributeValue.php');
- require_once(__CA_LIB_DIR__.'/Attributes/Values/AttributeValue.php');
- require_once(__CA_LIB_DIR__.'/Configuration.php');
- require_once(__CA_LIB_DIR__.'/BaseModel.php');	// we use the BaseModel field type (FT_*) and display type (DT_*) constants
+require_once(__CA_LIB_DIR__.'/Configuration.php');
+require_once(__CA_LIB_DIR__.'/Attributes/Values/IAttributeValue.php');
+require_once(__CA_LIB_DIR__.'/Attributes/Values/AttributeValue.php');
+require_once(__CA_LIB_DIR__.'/Configuration.php');
+require_once(__CA_LIB_DIR__.'/BaseModel.php');	// we use the BaseModel field type (FT_*) and display type (DT_*) constants
 
- require_once(__CA_APP_DIR__.'/helpers/gisHelpers.php');
+require_once(__CA_APP_DIR__.'/helpers/gisHelpers.php');
 
- global $_ca_attribute_settings;
+global $_ca_attribute_settings;
  
- $_ca_attribute_settings['GeoNamesAttributeValue'] = array(		// global
+$_ca_attribute_settings['GeoNamesAttributeValue'] = array(		// global
 	'fieldWidth' => array(
 		'formatType' => FT_NUMBER,
 		'displayType' => DT_FIELD,
@@ -541,6 +541,15 @@ class GeoNamesAttributeValue extends AttributeValue implements IAttributeValue {
 	 */
 	public function sortField() {
 		return 'value_sortable';
+	}
+	# ------------------------------------------------------------------
+	/**
+	 * Returns name of field in ca_attribute_values to use for query operations
+	 *
+	 * @return string Name of sort field
+	 */
+	public function queryFields() : ?array {
+		return ['value_longtext1', 'value_longtext2'];
 	}
 	# ------------------------------------------------------------------
 	/**
