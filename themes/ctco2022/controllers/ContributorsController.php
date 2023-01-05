0cf3f47738347f5e57a5e1ce1202a701da9d9af0
Maria <maria@whirl-i-gig.com>
2023-01-05 21:06:22 +0000

    ctco qa

--- themes/ctco2022/controllers/ContributorsController.php
+++ themes/ctco2022/controllers/ContributorsController.php
@@ -30,6 +30,8 @@
  	require_once(__CA_LIB_DIR__.'/Search/EntitySearch.php');
  	
  	class ContributorsController extends BasePawtucketController {
+ 		private $opo_result_context = null;
+ 		
  		# -------------------------------------------------------
  		public function __construct(&$po_request, &$po_response, $pa_view_paths=null) {
  			parent::__construct($po_request, $po_response, $pa_view_paths);
@@ -65,7 +67,10 @@
  		 		$o_search->addResultFilter("ca_entities.access", "IN", join(',', $this->opa_access_values));
 			}
 			$qr_res = $o_search->search("ca_entities.type_id:".$this->opn_contributor_id, array("sort" => "ca_entity_labels.name_sort"));
- 				
+ 			
+ 			$this->opo_result_context = new ResultContext($this->request, "ca_entities", "contributors");
+			$this->opo_result_context->setAsLastFind();
+			
  			$o_map = new GeographicMap('100%', $this->config->get("contributor_map_height"), 'map');
 			$va_map_stats = $o_map->mapFrom($qr_res, $this->config->get("contributor_map_georeference_field"), array("labelTemplate" => $this->config->get("contributor_map_label_template"), "contentTemplate" => $this->config->get("contributor_map_content_template"), "request" => $this->request, "checkAccess" => $this->opa_access_values));
 			$this->view->setVar("map", $o_map->render('HTML', array('delimiter' => "<br/>")));
@@ -73,8 +78,28 @@
 			$this->view->setVar("timeline_set_id", $vn_timeline_set_id);
 			
 			$qr_res->seek(0);
+			
+			$this->opo_result_context->setResultList($qr_res->getPrimaryKeyValues(200));
+			$this->opo_result_context->saveContext();
+			
 			$this->view->setVar("contributor_results", $qr_res);
 
  			$this->render("Contributors/index_html.php");
+ 		}
+ 		# -------------------------------------------------------
+		/** 
+		 * Generate the URL for the "back to results" link from a browse result item
+		 * as an array of path components.
+		 */
+ 		public static function getReturnToResultsUrl($po_request) {
+ 			$va_ret = array(
+ 				'module_path' => '',
+ 				'controller' => 'Contributors',
+ 				'action' => $po_request->getAction(),
+ 				'params' => array(
+ 					
+ 				)
+ 			);
+			return $va_ret;
  		}
  	}
