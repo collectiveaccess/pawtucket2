0b531eaf7e1dccc8fb650cd905d4d318e626e1b5
Maria <maria@whirl-i-gig.com>
2023-01-06 17:25:26 -0500

    appalshop

--- themes/appalshop/views/Gallery/detail_html.php
+++ themes/appalshop/views/Gallery/detail_html.php
@@ -5,30 +5,46 @@
 	$ps_label = $this->getVar("label");
 	$ps_description = $this->getVar("description");
 	$pn_set_item_id = $this->getVar("set_item_id");
+	MetaTagManager::addMetaProperty("og:description", $ps_label);
+	MetaTagManager::addMetaProperty("description", $ps_label);
+	$set_item = array_shift(array_shift($t_set->getFirstItemsFromSets(array($pn_set_id), array("version" => "page", "checkAccess" => caGetUserAccessValues($this->request))))); 			
+	if($vs_rep = $set_item["representation_url"]){
+		MetaTagManager::addMetaProperty("og:image", $vs_rep);
+	}
 ?>
 	<div class="row">
 		<div class="col-sm-12">
-			<div class="galleryBackButton"><?php print caNavLink($this->request, "<i class='fa fa-angle-double-left'></i><div class='small'>Back</div>", "", "", "Gallery", "Index"); ?></div>
-			<H1><?php print $this->getVar("label")."</H1>"; ?>
+			<H1>Special Project: <?php print $this->getVar("label")."</H1>"; ?>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-sm-8"><div id="galleryDetailImageArea">
+		</div><!-- end galleryDetailImageArea --></div><!--end col-sm-8-->
+		<div class="col-sm-4" id="galleryDetailObjectInfo"> </div>
+	</div><!-- end row -->
+<div class="galleryDetailBottom"></div>
+
+
+	<div class="row">
 <?php
 	if($ps_description){
 ?>
-	<div class="row">
 		<div class="col-sm-12 setDescription">
 			<?php print "<p>".$ps_description."</p>"; ?>
 		</div><!-- end col -->
 	</div>
+	<div class="row">
 <?php
 	}
-?>
-	<div class="row">
-		<div id="galleryDetailImageGrid" class="col-sm-12">
-			<div class="row">		
+?>	
+		<div id="galleryDetailImageGrid" class="col-sm-10 col-sm-offset-1">		
 <?php
 		$vn_i = 0;
+		$vn_c = 0;
 		foreach($pa_set_items as $pa_set_item){
+			if($vn_c == 0){
+				print "<div class='row'>";
+			}
 			if(!$vn_first_item_id){
 				$vn_first_item_id = $pa_set_item["item_id"];
 			}
@@ -47,15 +63,25 @@
 				$vs_rep = $pa_set_item["representation_tag_".$vs_icon];
 			}
 			if($pa_set_item["representation_tag_".$vs_icon]){
+			    $t_set_obj = new ca_objects($t_set_item->get('row_id'));
 				$vn_i++;
-				print "<div class='smallpadding col-xs-3 col-sm-2 col-md-1".(($vn_i > 24) ? " galleryIconHidden" : "")."'>";
-				print "<a href='#' id='galleryIcon".$pa_set_item["item_id"]."' onclick='jQuery(\"#galleryDetailImageArea\").load(\"".caNavUrl($this->request, '', 'Gallery', 'getSetItemRep', array('item_id' => $pa_set_item["item_id"], 'set_id' => $pn_set_id))."\"); jQuery(\"#galleryDetailObjectInfo\").load(\"".caNavUrl($this->request, '', 'Gallery', 'getSetItemInfo', array('item_id' => $pa_set_item["item_id"], 'set_id' => $pn_set_id))."\"); galleryHighlightThumbnail(\"galleryIcon".$pa_set_item["item_id"]."\"); return false;'>".$vs_rep."</a>";
+				$vn_c++;
+				$vs_tmp = strip_tags($t_set_obj->get('ca_objects.preferred_labels'));
+				if(strlen($vs_tmp) > 48){
+					$vs_tmp = mb_substr($vs_tmp, 0, 45)."...";
+				}
+				print "<div class='smallpadding col-xs-6 col-sm-4 col-md-2".(($vn_i > 24) ? " galleryIconHidden" : "")."'>";
+				print "<a href='#' id='galleryIcon".$pa_set_item["item_id"]."' onclick='jQuery(\"#galleryDetailImageArea\").load(\"".caNavUrl($this->request, '', 'Gallery', 'getSetItemRep', array('item_id' => $pa_set_item["item_id"], 'set_id' => $pn_set_id))."\"); jQuery(\"#galleryDetailObjectInfo\").load(\"".caNavUrl($this->request, '', 'Gallery', 'getSetItemInfo', array('item_id' => $pa_set_item["item_id"], 'set_id' => $pn_set_id))."\"); galleryHighlightThumbnail(\"galleryIcon".$pa_set_item["item_id"]."\");'>".$vs_rep."</a>";
+				print "<span title='".$t_set_obj->get('ca_objects.preferred_labels')."'>".$vs_tmp."</span>";
 				print "</div>\n";
-				
+				if($vn_c == 6){
+					print "</div><!-- end row -->";
+					$vn_c = 0;
+				}
 				if($vn_i == 24){
-					print "<div class='col-sm-3' id='moreLink'>
-								<a href='#' onclick='$(\".galleryIconHidden\").removeClass(\"galleryIconHidden\"); $(\"#moreLink\").hide(); return false;'>".(sizeof($pa_set_items) - 24)." "._t("more")."</a>
-							</div>";
+					print "<div class='row'><div class='col-sm-12 text-center' id='moreLink'>
+								<a href='#' onclick='$(\".galleryIconHidden\").removeClass(\"galleryIconHidden\"); $(\"#moreLink\").hide(); return false;' class='btn btn-default'>".(sizeof($pa_set_items) - 24)." "._t("more")."</a>
+							</div></div>";
 				}
 			}
 		}
@@ -63,15 +89,6 @@
 			</div><!-- end row -->
 		</div><!-- end col -->
 	</div><!-- end row -->
-<div class="galleryDetailBottom"></div>
-
-	<div class="row">
-		<div class="col-sm-8"><div id="galleryDetailImageArea">
-		</div><!-- end galleryDetailImageArea --></div><!--end col-sm-8-->
-		<div class="col-sm-4" id="galleryDetailObjectInfo"> </div>
-	</div><!-- end row -->
-
-
 <script type='text/javascript'>
 		jQuery(document).ready(function() {		
 			jQuery("#galleryDetailImageArea").load("<?php print caNavUrl($this->request, '', 'Gallery', 'getSetItemRep', array('item_id' => ($pn_set_item_id) ? $pn_set_item_id : $vn_first_item_id, 'set_id' => $pn_set_id)); ?>");
