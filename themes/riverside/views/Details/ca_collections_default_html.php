8a9ba30ba4082bb92dffff5f60b4600ca8e2783a
Maria <maria@whirl-i-gig.com>
2023-02-16 09:32:45 -0500

    Riverside sort order of objects on collection page

--- themes/riverside/views/Details/ca_collections_default_html.php
+++ themes/riverside/views/Details/ca_collections_default_html.php
@@ -119,7 +119,7 @@
 			</div><!-- end row -->
 			<script type="text/javascript">
 				jQuery(document).ready(function() {
-					jQuery("#browseResultsContainer").load("<?php print caNavUrl($this->request, '', 'Search', 'objects', array('search' => 'collection_id:^ca_collections.collection_id'), array('dontURLEncodeParameters' => true)); ?>", function() {
+					jQuery("#browseResultsContainer").load("<?php print caNavUrl($this->request, '', 'Search', 'objects', array('search' => 'collection_id:^ca_collections.collection_id', 'sort' => 'Collection'), array('dontURLEncodeParameters' => true)); ?>", function() {
 						jQuery('#browseResultsContainer').jscroll({
 							autoTrigger: true,
 							loadingHtml: '<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>',
