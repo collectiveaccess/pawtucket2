d907570cfe00153206c477759ab2d903bab68f61
Maria <maria@whirl-i-gig.com>
2023-04-28 15:33:22 -0400

    mel tracking code

--- themes/mel/views/pageFormat/pageHeader.php
+++ themes/mel/views/pageFormat/pageHeader.php
@@ -54,6 +54,15 @@
 ?><!DOCTYPE html>
 <html lang="en">
 	<head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-TBLJZ6MF7Z"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-TBLJZ6MF7Z');
+	</script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
 	<?php print MetaTagManager::getHTML(); ?>
