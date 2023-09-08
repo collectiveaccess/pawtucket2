6234391d24f058f8fcc56682aff2f61c688fa53f
Seth <seth@whirl-i-gig.com>
2023-09-08 20:29:32 +0000

    Update theme

--- themes/hff/views/system/configuration_error_install_html.php
+++ themes/hff/views/system/configuration_error_install_html.php
@@ -1,13 +1,13 @@
 <?php
 /** ---------------------------------------------------------------------
- * themes/default/views/system/configuration_error_html.php : 
+ * themes/default/views/system/configuration_error_intstall_html.php : 
  * ----------------------------------------------------------------------
  * CollectiveAccess
  * Open-source collections management software
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2011-2022 Whirl-i-Gig
+ * Copyright 2011 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -32,39 +32,21 @@
 		$va_tmp = explode("/", str_replace("\\", "/", $_SERVER['SCRIPT_NAME']));
 		array_pop($va_tmp);
 		$vs_path = join("/", $va_tmp);
-		
-		if (!is_array($opa_error_messages)) {
-			$opa_error_messages = self::$opa_error_messages;
-		}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<title>CollectiveAccess configuration error display</title>
-	<link href="<?= $vs_path; ?>/themes/default/assets/pawtucket/css/error.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-	<div id='box'>
-	<div id="logo"><img src="<?= $vs_path ?>/themes/default/assets/pawtucket/graphics/logos/logo.svg"  width="350" height="35" alt="CollectiveAccess"/></div><!-- end logo -->
-	<div id="content">
-		<?= "<div class='error'>An error in your system configuration has been detected</div>
-			General installation instructions can be found
-			<a href='https://manual.collectiveaccess.org' target='_blank'>here</a>.
-			For more specific hints on the existing issues please have a look at the messages below."; ?>
-		<br/><br/>
+<?php print _t("<div class='error'>An error in your system configuration has been detected</div>
+	    General installation instructions can be found
+	    <a href='http://wiki.collectiveaccess.org/index.php?title=Installation_(Pawtucket)' target='_blank'>here</a>.
+	    For more specific hints on the existing issues please have a look at the messages below."); ?>
+	<br/><br/>
 <?php
-foreach ($opa_error_messages as $vs_message):
+foreach (self::$opa_error_messages as $vs_message):
 ?>
 		<div class="permissionError">
-			<img src='<?= $vs_path; ?>/themes/default/assets/pawtucket/graphics/glyphicons_078_warning_sign.png' class="permissionErrorIcon"/>
-			<?= $vs_message; ?>
+			<img src='<?php print $vs_path; ?>/graphics/vorsicht.gif' class="permissionErrorIcon"/>
+			<?php print $vs_message; ?>
 			<div style='clear:both; height:1px;'><!-- empty --></div>
 		</div>
 		<br/>
 <?php
 endforeach;
 ?>
-	
-</div><!-- end content --></div><!-- end box -->
-</body>
-</html>
