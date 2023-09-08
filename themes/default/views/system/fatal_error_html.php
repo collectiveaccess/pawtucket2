070e5bbb8a78b2703a26ef10db9c08d8da486c3f
Seth <seth@whirl-i-gig.com>
2023-09-08 20:30:17 +0000

    Fix error displayx

--- themes/default/views/system/fatal_error_html.php
+++ themes/default/views/system/fatal_error_html.php
@@ -26,47 +26,59 @@
  * ----------------------------------------------------------------------
  */
  
-$tmp = explode("/", str_replace("\\", "/", $_SERVER['SCRIPT_NAME']));
-array_pop($tmp);
-$path = join("/", $tmp);
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>CollectiveAccess error</title>
-	<link href="<?= $path; ?>/themes/default/assets/pawtucket/css/error.css" rel="stylesheet" type="text/css" />
+	<link href="<?= caGetThemeUrlPath(); ?>/assets/pawtucket/css/error.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 	<div id='errorDetails'>
-		<div id="logo"><img src="<?= $path ?>/themes/default/assets/pawtucket/graphics/logos/logo.svg" width="350" height="35" alt="CollectiveAccess"/></div><!-- end logo -->
+		<div id="logo"><?= caGetDefaultLogo(); ?></div><!-- end logo -->
 		<div id="content">
-			<div class='error'>Something went wrong</div>	
-			
+			<div class='error'><?= _t("Something went wrong"); ?></div>
+<?php if((defined('__CA_ENABLE_DEBUG_OUTPUT__') && __CA_ENABLE_DEBUG_OUTPUT__) || (defined('__CA_STACKTRACE_ON_EXCEPTION__') && __CA_STACKTRACE_ON_EXCEPTION__)) { ?>
 			<div id="errorLocation" class="errorPanel">
-				<img src='<?= $path; ?>/themes/default/assets/pawtucket/graphics/glyphicons_078_warning_sign.png' class="permissionErrorIcon"/>
-				<div class="errorDescription"><span class="errorMessage"><?= $ps_errstr; ?></span> in <?= $ps_errfile; ?> line <?= $pn_errline; ?>:</div>
+				<?= caNavIcon(__CA_NAV_ICON_ALERT__ , 2, array('class' => 'permissionErrorIcon')); ?>
+				<div class="errorDescription"><span class="errorMessage"><?= $errstr; ?></span> in <?= $errfile; ?> line <?= $errline; ?>:</div>
 			</div>
 			<div id="stacktace">
-					<ol class="tracelist">
+				<ol class="tracelist">
 <?php
-						foreach($pa_errcontext as $i => $trace) {
-							print "<li>".(($i == 0) ? "In " : "At ").$trace['class'].$trace['type'].$trace['function']."(".join(', ', $pa_errcontext_args[$i]).") in <a class='tracelistEntry' title='".$trace['file']."' ondblclick='var f=this.innerHTML;this.innerHTML=this.title;this.title=f;'>".pathinfo($trace['file'], PATHINFO_FILENAME)."</a> line ".$trace['line']."</li>\n";
-						}
+					foreach($errcontext as $vn_i => $va_trace) {
+						print "<li>".(($vn_i == 0) ? "In " : "At ").$va_trace['class'].$va_trace['type'].$va_trace['function']."(".join(', ', $pa_errcontext_args[$vn_i] ?? []).") in <a class='tracelistEntry' title='".$va_trace['file']."' ondblclick='var f=this.innerHTML;this.innerHTML=this.title;this.title=f;'>".pathinfo($va_trace['file'], PATHINFO_FILENAME)."</a> line ".$va_trace['line']."</li>\n";
+					}
 ?>
-					</ol>
-	
+				</ol>
+<?php
+		if(is_array($request_params) && (sizeof($request_params) > 0)) {
+?>
 			<div id="requestParameters" class="errorPanel">
-				<img src='<?= $path; ?>/themes/default/assets/pawtucket/graphics/glyphicons_195_circle_info.png' class="permissionErrorIcon"/>
+				<?= caNavIcon(__CA_NAV_ICON_INFO__ , 2, array('class' => 'permissionErrorIcon')); ?>
 				<div class="errorDescription">
 					<span class="errorMessage"></span>Request parameters:</span>
 					<ol class="paramList">
 <?php
-					foreach($pa_request_params as $k => $v) {
-						print "<li>{$k} =&gt; {$v}</li>";
-					}
+						foreach($request_params as $k => $v) {
+							print "<li>{$k} =&gt; {$v}</li>";
+						}
 ?>
-					</ol>
+						</ol>
+					</div>
+				</div>
+<?php
+		}
+} else {
+?>
+			<div id="errorLocation" class="errorPanel">
+				<?= caNavIcon(__CA_NAV_ICON_ALERT__ , 2, array('class' => 'permissionErrorIcon')); ?>
+				<div class="errorDescription">
+				<?= _t("There was an uncaught fatal error. Please contact your system administrator and check the CollectiveAccess log files."); ?>
 				</div>
 			</div>
+<?php
+}
+?>
 		</div><!-- end content -->
 	</div><!-- end box -->
 </body>
