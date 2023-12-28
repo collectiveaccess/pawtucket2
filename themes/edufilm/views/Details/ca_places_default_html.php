d38da310f866ed40b5cb95d95d710cce821b55cc
CollectiveAccess <seth@collectiveaccess.org>
2023-12-28 11:03:34 -0500

    Theme update

--- themes/edufilm/views/Details/ca_places_default_html.php
+++ themes/edufilm/views/Details/ca_places_default_html.php
@@ -75,6 +75,7 @@
 					<div id="detailAnnotations"></div>
 					<?php print caObjectRepresentationThumbnails($this->request, $this->getVar("representation_id"), $t_object, array("returnAs" => "bsCols", "linkTo" => "carousel", "bsColClasses" => "smallpadding col-sm-3 col-md-3 col-xs-4", "primaryOnly" => $this->getVar('representationViewerPrimaryOnly') ? 1 : 0)); ?>
 					
+					<div class="map"></div>
 					{{{map}}}
 				</div><!-- end col -->
 				<div class='col-md-6 col-lg-6'>
