582528af37591af690c31f20e368568a497c350f
Selina <selinamangaroo@gmail.com>
2024-01-19 22:01:02 +0000

    redo register form

--- themes/default/views/LoginReg/form_register_html.php
+++ themes/default/views/LoginReg/form_register_html.php
@@ -36,126 +36,64 @@
 			$co_security = 'equation_sum';
 		}
 	}
-	if($this->request->isAjax()){
+	$vn_num1 = rand(1,10);
+	$vn_num2 = rand(1,10);
+	$vn_sum = $vn_num1 + $vn_num2;
+	if($this->request->isAjax()){}
 ?>
-<div id="caFormOverlay">
-<?php
-	}
-?>
+
 <script type="text/javascript">
 	// initialize CA Utils
 	caUI.initUtils();
 </script>
-	<form id="RegForm" action="<?php print caNavUrl($this->request, "", "LoginReg", "register"); ?>" class="form-horizontal" method="POST">
-	    <input type="hidden" name="csrfToken" value="<?php print caGenerateCSRFToken($this->request); ?>"/>
-<?php
-	if($this->request->isAjax()){
-?>
-		<div class="row">
-			<div class="col-sm-12">
-				<div class="pull-right pointer" onclick="caMediaPanel.hidePanel(); return false;"><span class="glyphicon glyphicon-remove-circle"></span></div>
-				<H1><?php print _t("Register"); ?></H1>
+
+<div class="row">
+	<div class="col">
+
+		<h1><?= _t("Register"); ?></h1>
+
+		<form class="row g-3 my-2" action="<?php print caNavUrl($this->request, "", "LoginReg", "register"); ?>" method="POST">
+			<div class="col-md-4">
+				<label for="inputFirstName" class="form-label"><?= _t("First Name"); ?></label>
+				<input type="text" class="form-control" id="inputFirstName" aria-label="enter name" placeholder="Enter first name" required>
 			</div>
-		</div>
-<?php
-	}else{
-?>
-	    <div class="row"><div class="col-sm-4"><H1><?php print _t("Register"); ?></H1></div></div>
-<?php
-	}
-	if($va_errors["register"]){
-		print "<div class='alert alert-danger'>".$va_errors["register"]."</div>";
-	}
-		foreach(array("fname", "lname", "email") as $vs_field){
-			if($va_errors[$vs_field]){
-				print "<div class='alert alert-danger'>".$va_errors[$vs_field]."</div>";
-			}	
-			print $t_user->htmlFormElement($vs_field,"<div class='form-group".(($va_errors[$vs_field]) ? " has-error" : "")."'><label for='".$vs_field."' class='col-sm-4 control-label'>^LABEL</label><div class='col-sm-7'>^ELEMENT</div><!-- end col-sm-7 --></div><!-- end form-group -->\n", array("classname" => "form-control"));
-		}
-		$va_profile_settings = $this->getVar("profile_settings");
-		if(is_array($va_profile_settings) and sizeof($va_profile_settings)){
-			foreach($va_profile_settings as $vs_field => $va_profile_element){
-				if($va_errors[$vs_field]){
-					print "<div class='alert alert-danger'>".$va_errors[$vs_field]."</div>";
-				}
-				
-				print "<div class='form-group".(($va_errors[$vs_field]) ? " has-error" : "")."'>"; 
-				print $va_profile_element["bs_formatted_element"];
-				print "</div><!-- end form-group -->";
-			}
-		}
-		if($co_security == 'captcha'){
-?>
-			<script type="text/javascript">
-				var gCaptchaRender = function(){
-					grecaptcha.render('regCaptcha', {'sitekey': '<?php print __CA_GOOGLE_RECAPTCHA_KEY__; ?>'});
-				};
-			</script>
-			<script src='https://www.google.com/recaptcha/api.js?onload=gCaptchaRender&render=explicit' async defer></script>
-			<div class='form-group<?php print (($va_errors["recaptcha"]) ? " has-error" : ""); ?>'>
-        		<div id="regCaptcha" class="col-sm-8 col-sm-offset-4"></div>
-        	</div>
-<?php		
-		} else {
-			if($va_errors["security"]){
-				print "<div class='alert alert-danger'>".$va_errors["security"]."</div>";
-			}
-			$vn_num1 = rand(1,10);
-			$vn_num2 = rand(1,10);
-			$vn_sum = $vn_num1 + $vn_num2;
-?>
-			<div class='form-group<?php print (($va_errors["security"]) ? " has-error" : ""); ?>'>
-				<label for='security' class='col-sm-4 control-label'><?php print _t("Security Question"); ?></label>
-				<div class='col-sm-7'>
-					<div class='col-sm-5'>
-						<p class="form-control-static"><?php print $vn_num1; ?> + <?php print $vn_num2; ?> = </p>
-					</div>
-					<div class='col-sm-5'>
-						<input name="security" value="" id="security" type="text" class="form-control" />
-					</div>
-				</div><!-- end col-sm-7 -->
-			</div><!-- end form-group -->
-<?php
-		}
-		if($va_errors["password"]){
-			print "<div class='alert alert-danger'>".$va_errors["password"]."</div>";
-		}
-		print $t_user->htmlFormElement("password", "<div class='form-group".(($va_errors["password"]) ? " has-error" : "")."'><label for='password' class='col-sm-4 control-label'>^LABEL</label><div class='col-sm-7'>^ELEMENT</div><!-- end col-sm-7 --></div><!-- end form-group -->\n", array("classname" => "form-control"));
-?>
-		<div class="form-group<?php print (($va_errors["password"]) ? " has-error" : ""); ?>">
-			<label for='password2' class='col-sm-4 control-label'><?php print _t('Re-Type password'); ?></label>
-			<div class="col-sm-7"><input type="password" name="password2" id="password2" size="40" class="form-control"  autocomplete="off" /></div><!-- end col-sm-7 -->
-		</div><!-- end form-group -->
-		
-<?php	
-		if($va_errors["group_code"]){
-			print "<div class='alert alert-danger'>".$va_errors["group_code"]."</div>";
-		}
-		print "<div class='form-group".(($va_errors["group_code"]) ? " has-error" : "")."'><label for='registrationGroupCode' class='col-sm-4 control-label'>"._t("Group code (optional)")."</label><div class='col-sm-7'>".caHTMLTextInput("group_code", ['class' => 'form-control', 'id' => 'registrationGroupCode'], [])."</div></div>\n";
-?>
-		<div class="form-group">
-			<div class="col-sm-offset-4 col-sm-7">
-				<button type="submit" class="btn btn-default"><?php print _t('Register'); ?></button>
-			</div><!-- end col-sm-7 -->
-		</div><!-- end form-group -->
-		<input type="hidden" name="sum" value="<?php print $vn_sum; ?>">
-	</form>
-<?php
-	if($this->request->isAjax()){
-?>
-</div><!-- end caFormOverlay -->
-<script type='text/javascript'>
-	jQuery(document).ready(function() {
-		jQuery('#RegForm').on('submit', function(e){		
-			jQuery('#caMediaPanelContentArea').load(
-				'<?php print caNavUrl($this->request, '', 'LoginReg', 'register', null); ?>',
-				jQuery('#RegForm').serializeObject()
-			);
-			e.preventDefault();
-			return false;
-		});
-	});
-</script>
-<?php
-	}
-?>
+
+			<div class="col-md-4">
+				<label for="inputLastName" class="form-label"><?= _t("Last Name"); ?></label>
+				<input type="text" class="form-control" id="inputLastName" aria-label="enter name" placeholder="Enter last name" required>
+			</div>
+
+			<div class="col-md-4">
+				<label for="inputEmail" class="form-label"><?= _t("Email"); ?></label>
+				<input type="email" class="form-control" id="inputEmail" aria-label="enter email" placeholder="Enter email" required>
+			</div>
+
+			<div class="col-md-4">
+				<label for="inputPassword" class="form-label"><?= _t("Password"); ?></label>
+				<input type="password" class="form-control" id="inputPassword" required>
+			</div>
+
+			<div class="col-md-4">
+				<label for="inputPassword2" class="form-label"><?= _t("Re-Type Password"); ?></label>
+				<input type="password" class="form-control" id="inputPassword2" required>
+			</div>
+
+			<div class="col-md-4">
+				<label for="inputCode" class="form-label"><?= _t("Group Code (optional)"); ?></label>
+				<input type="text" class="form-control" id="inputCode">
+			</div>
+
+			<div class="col-md-4">
+				<label for="securityQuestion" class="form-label"><?= _t("Security Question"); ?></label>
+				<div id="securityQuestionBlock" class="form-text mb-2"><?= $vn_num1; ?> + <?= $vn_num2; ?> = </div>
+				<input type="text" class="form-control" id="securityQuestion" aria-label="enter answer" aria-describedby="securityQuestionBlock" required>
+			</div>
+
+			<div class="col-12">
+				<button type="submit" class="btn btn-dark"><?= _t("Register"); ?></button>
+			</div>
+		</form>
+
+	</div>
+</div>
+
