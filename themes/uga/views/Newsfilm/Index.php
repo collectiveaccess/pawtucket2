99b15501f92b66e44c9dbf2bc350e2a27f64729f
Michael Allen <mballen@uga.edu>
2024-02-07 11:37:25 -0500

    Resolve "Updates to News Film (Will be News Contents)"

--- themes/uga/views/Newsfilm/Index.php
+++ themes/uga/views/Newsfilm/Index.php
@@ -1,17 +1,18 @@
 <?php
 	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": Newsfilm");
 ?>
-<H1><?php print _t("Newsfilm"); ?></H1>
+<H1><?php print _t("News Content"); ?></H1>
 <div class="row">
 	<div class="col-sm-8">
-	<p>Newsfilm was used in broadcast for television newscasts from 1948 until the 1970s. Television from this time period is very different from the television
-	news we know and count on today. During the early history of newsfilm local stations relied heavily on their cameramen to capture local news stories -
-	there were no feeds between stations because the technology to do so was not yet in place. Cameramen would go out and capture the stories on 100 foot reels,
-	return to the studio and process this reversal film stock (film stock that passed through the camera and was broadcast ready after a one-step development process),
-	the film would then be viewed and edited, and put on a film chain (device that included one or more film projectors aligned to TV pick-up camera ) ready for the
-	news broadcast. These 16mm reels were filmed in black & white and then color with sound and without sound in the era before videotape was widely used.
-	Unlike videotape or digital capture which can be easily erased for re-use, film can only be used one time. Film is also the most long-lived of the moving image formats and
-	therefore newsfilm from collections like these are unique filled with one-of-kind images and interviews.</p>
+	<p>We have several types of news content in the collection.
+
+Newsfilm was used in broadcast for television newscasts from 1948 until the 1970s. Television from this time period is very different from the television news we know and count on today. During the early history of newsfilm local stations relied heavily on their cameramen to capture local news stories - there were no feeds between stations because the technology to do so was not yet in place. Cameramen would go out and capture the stories on 100 foot reels, return to the studio and process this reversal film stock (film stock that passed through the camera and was broadcast ready after a one-step development process), the film would then be viewed and edited, and put on a film chain (device that included one or more film projectors aligned to TV pick-up camera ) ready for the news broadcast. These 16mm reels were filmed in black & white and then color with sound and without sound in the era before videotape was widely used. Unlike videotape or digital capture which can be easily erased for re-use, film can only be used one time. Film is also the most long-lived of the moving image formats and therefore newsfilm from collections like these are unique filled with one-of-kind images and interviews.
+
+In 2023, BMA merged the Newsfilm Database into our overall holdings to allow for integrated searching across all collections. If you are interested in searching only a specific newsfilm collection in BMA, use the Advanced Search option on the <a href="https://bmac.libs.uga.edu/index.php/Search/advanced/objects">Search Collections</a> page. Add your search terms to the top field(s) and specify which news collection to limit results to in the Collection field. You may also perform a simple search and then filter the results by collection. Archivists are available to help with research.
+
+News videotape starts in the 1970s and is used into the 2000s. This format allowed for more ease of use because of the portability of video cassettes with the benefit for the station of being able to re-use tapes over and over again.
+
+In our collection we have raw news content in the newsfilm and videotape and in some cases recorded airchecks of news programs especially in the WSB Videotape Collection and the Peabody Awards Collection. The Peabody Awards Collection has news content from all over the United States.</p>
 
 	<h3>Newsfilm Collections</h3>
 	<p>
