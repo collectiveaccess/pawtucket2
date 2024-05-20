752be862c4f08447f50ecb7e24a6bcb973a54ed3
CollectiveAccess <seth@collectiveaccess.org>
2024-05-20 10:28:42 -0400

    Remove Metabolic theme

--- themes/metabolic/node_modules/@rjsf/bootstrap-4/dist/DateTimeWidget/DateTimeWidget.d.ts
+++
@@ -1,3 +0,0 @@
-import { TextWidgetProps } from "../TextWidget";
-declare const DateTimeWidget: (props: TextWidgetProps) => JSX.Element;
-export default DateTimeWidget;
--- themes/metabolic/views/Chronology/index_html.php
+++
@@ -1,9 +0,0 @@
-<div id="chronology"></div>
-
-<script type="text/javascript">	
-    pawtucketUIApps['Chronology'] = {
-    	baseUrl: "<?php print __CA_URL_ROOT__."/service.php/Browse"; ?>",
-    	selector: '#chronology',
-    	chronology: '<?= $this->request->getParameter('id', pString); ?>'
-    };
-</script>
