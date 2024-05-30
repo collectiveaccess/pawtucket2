ec1ba2009cc58cff92173f1bae1a2d63a5624e94
Maria <maria@whirl-i-gig.com>
2024-05-30 14:05:43 -0400

    midwest

--- themes/erfgoed_midwest/views/pageFormat/pageFooter.php
+++ themes/erfgoed_midwest/views/pageFormat/pageFooter.php
@@ -34,9 +34,8 @@
 					<div class="col-sm-12">
 						<ul class="list-inline">
 							<li><a href="https://www.midwest.be/cultuur-en-erfgoed" target="_blank"><b>WWW.MIDWEST.BE</b></a></li>
-							<li><i class="fa fa-map-marker" aria-hidden="true"></i> Ooststraat 35, 8800 Roeselare</li>
+							<li><i class="fa fa-map-marker" aria-hidden="true"></i> Spanjestraat 141/2 8800 Roeselare</li>
 							<li><i class="fa fa-paper-plane" aria-hidden="true"></i> <?php print caNavLink($this->request, _t("Contact"), "", "", "Contact", "Form"); ?></li>
-							<li><i class="fa fa-phone" aria-hidden="true"></i> +32(0)51 26 87 49</li>
 							<li><i class="fa fa-check" aria-hidden="true"></i> <?php print caNavLink($this->request, _t("Disclaimer"), "", "", "About", "Disclaimer"); ?></li>
 							<li><?php print caGetThemeGraphic($this->request, "Vlaanderen_verbeelding_werkt.png"); ?></li>
 						</ul>
