68864eea53a39969409eb36a41340c1d8bbe6d1c
Seth <seth@whirl-i-gig.com>
2024-06-21 14:24:17 -0400

    Add search field bundle module

--- themes/default/js/advancedSearchFieldBundle.js
+++ themes/default/js/advancedSearchFieldBundle.js
@@ -1,64 +1,75 @@
 function makeAdvancedSearchFieldBundle(options) {
 	for(let k in options['bundles']) {
 		const bundle_def = options['bundles'][k];
+		
 		let container_id = bundle_def['id'];
-		let container = document.getElementById(container_id);
+		let template_id = container_id + '-template';
 		
-		const firstOption = Object.keys(bundle_def['options']).shift();
-		
-		let select = document.getElementById(container_id + '-select');
-		let input = document.getElementById(container_id + '-input');
-		let addControl = document.getElementById(container_id + '-add');
-		let addButton = document.getElementById(container_id + '-add-button');
-		
-	 	select.innerHTML += bundle_def['select'];
- 		input.innerHTML += bundle_def['inputs'][firstOption];
- 		addButton.onclick = function(e) {
- 			cloneElement(container_id, options);
- 		};
- 	
-		select.onchange = function(e) {
-			input.innerHTML = bundle_def['inputs'][e.target.options[e.target.selectedIndex].value];
-			e.preventDefault();
-		}
+		cloneElement(k, container_id, template_id, true, options);
 	}
 }
 
-function cloneElement(container_id, options) {
-	// Get the element to be cloned
+function cloneElement(key, container_id, template_id, isInit, options) {
+	const k = key;
+	const bundle_def = options['bundles'][k];
+	
+	let template = document.getElementById(template_id);
 	let container = document.getElementById(container_id);
-
-	// Clone the element
-	let clone = container.cloneNode(true);
+	
+	let clone = template.cloneNode(true);
+	clone.removeAttribute('style');
+	clone.removeAttribute('id');
 
 	// Create a remove button
 	let removeBtn = document.createElement('a');
 
 	// Create icon
-	let removeIcon = document.createElement('i');
-	removeIcon.classList.add("bi");
-	removeIcon.classList.add("bi-dash-lg");
-
-	// Append the remove icon to the remove button
-	removeBtn.appendChild(removeIcon);
-
-	// Function to remove entire elment on click
-	removeBtn.onclick = function() {
-		this.parentNode.parentNode.remove();
-	};
-
-	// Remove existing add button in clone
-	let subParent = clone.querySelector('#' + container_id + '-add');
-	let addBtn = subParent.querySelector('#' + container_id + '-add-button');
-	if (addBtn) {
-		subParent.removeChild(addBtn);
+	let subParent = clone.querySelector('.' + container_id + '-add');
+	if(!isInit) {
+		let removeIcon = document.createElement('i');
+		removeIcon.classList.add("bi");
+		removeIcon.classList.add("bi-dash-lg");
+		
+		// Append the remove icon to the remove button
+		removeBtn.appendChild(removeIcon);
+		
+		// Function to remove entire elment on click
+		removeBtn.onclick = function() {
+			this.parentNode.parentNode.remove();
+		};
+		
+		// Remove existing add button in clone
+		let addBtn = subParent.querySelector('.' + container_id + '-add-button');
+		if (addBtn) {
+			subParent.removeChild(addBtn);
+		}
+	} else {
+		let addButton = clone.querySelector('.' + container_id + '-add-button');
+		addButton.onclick = function(e) {
+ 			const tid = template_id;
+ 			cloneElement(key, container_id, template_id, false, options);
+ 		};
+	}
+		
+	let input = clone.querySelector('.' + container_id + '-input');
+	const firstOption = Object.keys(bundle_def['options']).shift();
+	
+ 	input.innerHTML = bundle_def['inputs'][firstOption];
+	let selectContainer = clone.querySelector('.' + container_id + '-select');
+	selectContainer.innerHTML = bundle_def['select'];
+	let select = clone.querySelector('.' + container_id + '-select select');
+	select.onchange = function(e) {
+		input.innerHTML =  bundle_def['inputs'][e.target.options[e.target.selectedIndex].value];
+		e.preventDefault();
 	}
 
-	// Append the remove button to the subparent
-	subParent.appendChild(removeBtn);
+	if(!isInit) {
+		// Append the remove button to the subparent
+		subParent.appendChild(removeBtn);
+	}
 
 	// Append the clone to the container
-	document.getElementById('filterfield').appendChild(clone);
+	container.appendChild(clone);
 }
 
 export default makeAdvancedSearchFieldBundle;
