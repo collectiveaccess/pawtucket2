0265f5b7e8891dd5d91eb5107a0166a99822c939
Selina <selinamangaroo@gmail.com>
2024-10-09 16:31:46 +0000

    initial metabolic theme push

---
+++ themes/metabolic/views/Details/ca_collections_default_html.php
@@ -0,0 +1,187 @@
+<?php
+/* ----------------------------------------------------------------------
+ * themes/default/views/bundles/ca_collections_default_html.php : 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2013-2024 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * ----------------------------------------------------------------------
+ */
+$t_item = 				$this->getVar("item");
+$access_values = 		$this->getVar("access_values");
+$options = 				$this->getVar("config_options");
+$comments = 			$this->getVar("comments");
+$tags = 				$this->getVar("tags_array");
+$comments_enabled = 	$this->getVar("commentsEnabled");
+$pdf_enabled = 			$this->getVar("pdfEnabled");
+$inquire_enabled = 		$this->getVar("inquireEnabled");
+$copy_link_enabled = 	$this->getVar("copyLinkEnabled");
+$id =					$t_item->get('ca_collections.collection_id');
+$show_nav = 			($this->getVar("previousLink") || $this->getVar("resultsLink") || $this->getVar("nextLink")) ? true : false;
+$map_options = 			$this->getVar('mapOptions') ?? [];
+
+# --- get collections configuration
+$collections_config = caGetCollectionsConfig();
+$show_hierarchy_viewer = true;
+if($collections_config->get("do_not_display_collection_browser")){
+	$show_hierarchy_viewer = false;	
+}
+# --- get the collection hierarchy parent to use for exportin finding aid
+$top_level_collection_id = array_shift($t_item->get('ca_collections.hierarchy.collection_id', array("returnWithStructure" => true)));
+?>
+<script>
+	pawtucketUIApps['geoMapper'] = <?= json_encode($map_options); ?>;
+</script>
+<?php 
+	if($show_nav){
+?>
+	<div class="row mt-n3">
+		<div class="col text-center text-md-end">
+			<nav aria-label="result">{{{previousLink}}}{{{resultsLink}}}{{{nextLink}}}</nav>
+		</div>
+	</div>
+<?php
+	}
+?>
+	<div class="row<?php print ($show_nav) ? " mt-2 mt-md-n3" : ""; ?>">
+		<div class="col-md-12">
+			<H1 class="fs-3">{{{^ca_collections.preferred_labels.name}}}</H1>
+			{{{<ifdef code="ca_collections.type_id|ca_collections.idno"><div class="fw-medium mb-3 text-capitalize"><ifdef code="ca_collections.type_id">^ca_collections.type_id</ifdef><ifdef code="ca_collections.idno">, ^ca_collections.idno</ifdef></div></ifdef>}}}
+			<hr class="mb-0">
+		</div>
+	</div>
+<?php
+	if($inquire_enabled || $pdf_enabled || $copy_link_enabled){
+?>
+	<div class="row">
+		<div class="col text-center text-md-end">
+			<div class="btn-group" role="group" aria-label="Detail Controls">
+<?php
+				if($inquire_enabled) {
+					print caNavLink($this->request, "<i class='bi bi-envelope me-1'></i> "._t("Inquire"), "btn btn-sm btn-white ps-3 pe-0 fw-medium", "", "Contact", "Form", array("inquire_type" => "item_inquiry", "table" => "ca_collections", "id" => $id));
+				}
+				if($pdf_enabled) {
+					print caDetailLink($this->request, "<i class='bi bi-download me-1'></i> "._t('Download as PDF'), "btn btn-sm btn-white ps-3 pe-0 fw-medium", "ca_collections", $id, array('view' => 'pdf', 'export_format' => '_pdf_ca_collections_summary'));
+				}
+				if($copy_link_enabled){
+?>
+				<button type="button" class="btn btn-sm btn-white ps-3 pe-0 fw-medium"><i class="bi bi-copy"></i> <?= _t('Copy Link'); ?></button>
+<?php
+				}
+?>
+			</div>
+		</div>
+	</div>
+<?php
+	}
+?>
+{{{<ifdef code="ca_object_representations.media.large">
+	<div class="row justify-content-center mb-3">
+		<div class="col">
+			<div class='detailPrimaryImage object-fit-contain'>^ca_object_representations.media.large</div>
+		</div>
+	</div>
+</ifdef>}}}
+	<div class="row row-cols-1 row-cols-md-2">
+		<div class="col">				
+			{{{<dl class="mb-0">
+				<ifdef code="ca_collections.parent_id">
+					<dt>Part of</dt>
+					<dd><unit relativeTo="ca_collections.hierarchy" delimiter=" &gt; "><l>^ca_collections.preferred_labels.name</l></unit></dd>
+				</ifdef>
+
+				<ifdef code="ca_collections.date">
+					<dt><?= _t('Date'); ?></dt>
+					<dd class="label">^ca_collections.date</dd>
+				</ifcount>
+
+				<ifdef code="ca_collections.description">
+					<dt><?= _t('Description'); ?></dt>
+					<dd>^ca_collections.description</dd>
+				</ifdef>
+
+				<?php
+					if($t_item->get("ca_collections.bio_regions")){
+						if($bio_links = caGetBrowseLinks($t_item, 'ca_collections.bio_regions', ['template' => '<l>^ca_collections.bio_regions</l>', 'linkTemplate' => '^LINK'])) {
+				?>
+							<dt><?= _t('Bio Regions'); ?></dt>
+							<dd><?= join(", ", $bio_links); ?></dd>
+				<?php
+						}
+					}
+				?>
+
+			</dl>}}}
+			
+			<?= $this->render("Details/snippets/related_entities_by_rel_type_html.php"); ?>
+		</div>
+		<div class="col">
+			{{{<dl class="mb-0">
+				<ifcount code="ca_collections.related" min="1">
+					<dt><ifcount code="ca_collections.related" min="1" max="1"><?= _t('Related Collections'); ?></ifcount><ifcount code="ca_collections.related" min="2"><?= _t('Related Collections'); ?></ifcount></dt>
+					<unit relativeTo="ca_collections.related" delimiter=""><dd><unit relativeTo="ca_collections.hierarchy" delimiter=" ➔ "><l>^ca_collections.preferred_labels.name</l></unit></dd></unit>
+				</ifcount>
+
+				<ifcount code="ca_occurrences" min="1">
+					<dt><ifcount code="ca_occurrences" min="1" max="1" restrictToTypes="action"><?= _t('Event'); ?></ifcount><ifcount code="ca_occurrences" min="2" restrictToTypes="action"><?= _t('Events'); ?></ifcount></dt>
+					<unit relativeTo="ca_occurrences" delimiter="" restrictToTypes="action"><dd><l>^ca_occurrences.preferred_labels</l> (^relationship_typename)</dd></unit>
+				</ifcount>
+				<ifcount code="ca_occurrences" min="1">
+					<dt><ifcount code="ca_occurrences" min="1" max="1" restrictToTypes="exhibition"><?= _t('Exhibition'); ?></ifcount><ifcount code="ca_occurrences" min="2" restrictToTypes="exhibition"><?= _t('Exhibitions'); ?></ifcount></dt>
+					<unit relativeTo="ca_occurrences" delimiter="" restrictToTypes="exhibition"><dd><l>^ca_occurrences.preferred_labels</l> (^relationship_typename)</dd></unit>
+				</ifcount>
+				<ifcount code="ca_occurrences" min="1">
+					<dt><ifcount code="ca_occurrences" min="1" max="1" restrictToTypes="lecture_presentation"><?= _t('Lecture/Presentation'); ?></ifcount><ifcount code="ca_occurrences" min="2" restrictToTypes="lecture_presentation"><?= _t('Lectures/Presentations'); ?></ifcount></dt>
+					<unit relativeTo="ca_occurrences" delimiter="" restrictToTypes="lecture_presentation"><dd><l>^ca_occurrences.preferred_labels</l> (^relationship_typename)</dd></unit>
+				</ifcount>
+				<ifcount code="ca_occurrences" min="1">
+					<dt><ifcount code="ca_occurrences" min="1" max="1" restrictToTypes="publication"><?= _t('Publication'); ?></ifcount><ifcount code="ca_occurrences" min="2" restrictToTypes="publication"><?= _t('Publications'); ?></ifcount></dt>
+					<unit relativeTo="ca_occurrences" delimiter="" restrictToTypes="publication"><dd><l>^ca_occurrences.preferred_labels</l> (^relationship_typename)</dd></unit>
+				</ifcount>
+
+			</dl>}}}					
+		</div>
+	</div>
+
+<?php
+	if ($show_hierarchy_viewer) {	
+?>
+		<div hx-trigger="load" hx-get="<?php print caNavUrl($this->request, '', 'Collections', 'collectionHierarchy', array('collection_id' => $t_item->get('collection_id'))); ?>"  ></div>
+<?php				
+	}									
+?>				
+
+<!-- {{{<ifcount code="ca_occurrences" min="1">
+	<dl class="row">
+		<dt class="col-12 mt-3 mb-2"><ifcount code="ca_occurrences" min="1" max="1"><?= _t('Related Occurrence'); ?></ifcount><ifcount code="ca_occurrences" min="2"><?= _t('Related Occurrences'); ?></ifcount></dt>
+		<unit relativeTo="ca_occurrences" delimiter=""><dd class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 text-center"><l class="pt-3 pb-4 d-flex align-items-center justify-content-center bg-body-tertiary h-100 w-100 text-black">^ca_occurrences.preferred_labels<br>^relationship_typename</l></dd></unit>
+	</dl>
+</ifcount>}}} -->
+
+{{{<ifcount code="ca_objects" min="1">
+	<div class="row">
+		<div class="col"><h2>Assets</h2><hr/></div>
+	</div>
+	<div class="row" id="browseResultsContainer">	
+		<div hx-trigger='load' hx-swap='outerHTML' hx-get="<?php print caNavUrl($this->request, '', 'Search', 'objects', array('search' => 'ca_collections.collection_id:'.$t_item->get("ca_collections.collection_id"))); ?>">
+			<div class="spinner-border htmx-indicator m-3" role="status" class="text-center"><span class="visually-hidden">Loading...</span></div>
+		</div>
+</ifcount>}}}
---
+++ themes/metabolic/views/Details/ca_occurrences_action_html.php
@@ -0,0 +1,177 @@
+<?php
+/* ----------------------------------------------------------------------
+ * themes/default/views/bundles/ca_occurrences_default_html.php : 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2013-2022 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * ----------------------------------------------------------------------
+ */
+ 
+$t_item = 			$this->getVar("item");
+$access_values = 	$this->getVar("access_values");
+$options = 			$this->getVar("config_options");
+$comments = 		$this->getVar("comments");
+$tags = 			$this->getVar("tags_array");
+$comments_enabled = $this->getVar("commentsEnabled");
+$pdf_enabled = 		$this->getVar("pdfEnabled");
+$inquire_enabled = 	$this->getVar("inquireEnabled");
+$copy_link_enabled = 	$this->getVar("copyLinkEnabled");
+$id =				$t_item->get('ca_occurrences.occurrence_id');
+$show_nav = 		($this->getVar("previousLink") || $this->getVar("resultsLink") || $this->getVar("nextLink")) ? true : false;
+$map_options = $this->getVar('mapOptions') ?? [];
+?>
+<script>
+	pawtucketUIApps['geoMapper'] = <?= json_encode($map_options); ?>;
+</script>
+
+<?php
+	if($show_nav){
+?>
+	<div class="row mt-n3">
+		<div class="col text-center text-md-end">
+			<nav aria-label="result">{{{previousLink}}}{{{resultsLink}}}{{{nextLink}}}</nav>
+		</div>
+	</div>
+<?php
+	}
+?>
+	<div class="row<?php print ($show_nav) ? " mt-2 mt-md-n3" : ""; ?>">
+		<div class="col-md-12">
+			<H1 class="fs-3">{{{^ca_occurrences.preferred_labels.name}}}</H1>
+			{{{<ifdef code="ca_occurrences.type_id|ca_occurrences.idno"><div class="fw-medium mb-3 text-capitalize"><ifdef code="ca_occurrences.type_id">^ca_occurrences.type_id</ifdef><ifdef code="ca_occurrences.idno">, ^ca_occurrences.idno</ifdef></div></ifdef>}}}
+			<hr class="mb-0">
+		</div>
+	</div>
+<?php
+	if($inquire_enabled || $pdf_enabled || $copy_link_enabled){
+?>
+	<div class="row">
+		<div class="col text-center text-md-end">
+			<div class="btn-group" role="group" aria-label="Detail Controls">
+<?php
+				if($inquire_enabled) {
+					print caNavLink($this->request, "<i class='bi bi-envelope me-1'></i> "._t("Inquire"), "btn btn-sm btn-white ps-3 pe-0 fw-medium", "", "Contact", "Form", array("inquire_type" => "item_inquiry", "table" => "ca_occurrences", "id" => $id));
+				}
+				if($pdf_enabled) {
+					print caDetailLink($this->request, "<i class='bi bi-download me-1'></i> "._t('Download as PDF'), "btn btn-sm btn-white ps-3 pe-0 fw-medium", "ca_occurrences", $id, array('view' => 'pdf', 'export_format' => '_pdf_ca_occurrences_summary'));
+				}
+				if($copy_link_enabled){
+?>
+				<button type="button" class="btn btn-sm btn-white ps-3 pe-0 fw-medium"><i class="bi bi-copy"></i> <?= _t('Copy Link'); ?></button>
+<?php
+				}
+?>
+			</div>
+		</div>
+	</div>
+<?php
+	}
+?>
+{{{<ifdef code="ca_object_representations.media.large">
+	<div class="row justify-content-center mb-3">
+		<div class="col">
+			<div class='detailPrimaryImage object-fit-contain'>^ca_object_representations.media.large</div>
+		</div>
+	</div>
+</ifdef>}}}
+	<div class="row row-cols-1 row-cols-md-2">
+		<div class="col">				
+			{{{<dl class="mb-0">
+				<ifdef code="ca_occurrences.idno">
+					<dt><?= _t('Identifier'); ?></dt>
+					<dd>^ca_occurrences.idno</dd>
+				</ifdef>
+				<ifdef code="ca_occurrences.date">
+					<dt><?= _t('Date'); ?></dt>
+					<dd>^ca_occurrences.date</dd>
+				</ifdef>
+				<ifdef code="ca_occurrences.description">
+					<dt><?= _t('About'); ?></dt>
+					<dd>^ca_occurrences.description</dd>
+				</ifdef>
+				<ifdef code="ca_occurrences.description">
+					<dt><?= _t('About'); ?></dt>
+					<dd>^ca_occurrences.description</dd>
+				</ifdef>
+				<ifdef code="ca_occurrences.external_link">
+					<dt><?= _t('External Link'); ?></dt>
+					<dd>
+						<unit relativeTo="ca_occurrences.external_link" delimiter="<br/>">
+							<a href="^ca_occurrences.external_link" target="_blank" rel="noopener">^ca_occurrences.external_link</a> 
+						</unit>
+					</dd>
+				</ifdef>
+				<ifdef code="ca_occurrences.phase">
+					<dt><?= _t('Phase'); ?></dt>
+					<dd>^ca_occurrences.phase%delimiter=,_</dd>
+				</ifdef>
+			</dl>}}}
+
+			<?php
+				if($t_item->get("ca_occurrences.subject")){
+					if($subs = caGetBrowseLinks($t_item, 'ca_occurrences.subject', ['template' => '<l>^ca_occurrences.subject</l>', 'linkTemplate' => '^LINK'])) {
+			?>
+						<dt><?= _t('Themes'); ?></dt>
+						<dd><?= join(", ", $subs); ?></dd>
+			<?php
+					}
+				}
+			?>
+
+		</div>
+		<div class="col">
+			{{{<dl class="mb-0">
+				<ifcount code="ca_collections" min="1">
+					<dt><ifcount code="ca_collections" min="1" max="1"><?= _t('Action'); ?></ifcount><ifcount code="ca_collections" min="2"><?= _t('Actions'); ?></ifcount></dt>
+					<unit relativeTo="ca_collections" delimiter=""><dd><unit relativeTo="ca_collections.hierarchy" delimiter=" ➔ "><l>^ca_collections.preferred_labels.name</l></unit></dd></unit>
+				</ifcount>
+
+				<?= $this->render("Details/snippets/related_entities_by_rel_type_html.php"); ?>
+
+				<ifcount code="ca_occurrences" min="1">
+					<dt><ifcount code="ca_occurrences.related" min="1" max="1" restrictToTypes="action"><?= _t('Event'); ?></ifcount><ifcount code="ca_occurrences.related" min="2" restrictToTypes="action"><?= _t('Events'); ?></ifcount></dt>
+					<unit relativeTo="ca_occurrences.related" delimiter="" restrictToTypes="action"><dd><l>^ca_occurrences.preferred_labels</l> (^relationship_typename)</dd></unit>
+				</ifcount>
+
+				<ifcount code="ca_occurrences" min="1">
+					<dt><ifcount code="ca_occurrences.related" min="1" max="1" restrictToTypes="exhibition"><?= _t('Exhibition'); ?></ifcount><ifcount code="ca_occurrences.related" min="2" restrictToTypes="exhibition"><?= _t('Exhibitions'); ?></ifcount></dt>
+					<unit relativeTo="ca_occurrences.related" delimiter="" restrictToTypes="exhibition"><dd><l>^ca_occurrences.preferred_labels</l> (^relationship_typename)</dd></unit>
+				</ifcount>
+
+				<ifcount code="ca_places" min="1">
+					<dt><ifcount code="ca_places" min="1" max="1"><?= _t('Place'); ?></ifcount><ifcount code="ca_places" min="2"><?= _t('Places'); ?></ifcount></dt>
+					<unit relativeTo="ca_places" delimiter=""><dd><l>^ca_places.preferred_labels</l> (^relationship_typename)</dd></unit>
+				</ifcount>
+			</dl>}}}					
+		</div>
+	</div>
+
+{{{<ifcount code="ca_objects" min="1">
+	<div class="row">
+		<div class="col"><h2>Related Objects</h2><hr></div>
+	</div>
+	<div class="row" id="browseResultsContainer">	
+		<div hx-trigger='load' hx-swap='outerHTML' hx-get="<?php print caNavUrl($this->request, '', 'Search', 'objects', array('search' => 'ca_occurrences.occurrence_id:'.$t_item->get("ca_occurrences.occurrence_id"))); ?>">
+			<div class="spinner-border htmx-indicator m-3" role="status" class="text-center"><span class="visually-hidden">Loading...</span></div>
+		</div>
+	</div>
+</ifcount>}}}
