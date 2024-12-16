61f41f7eba0df12cbfbf7bc522b36bab2872e5ab
Maria <maria@whirl-i-gig.com>
2024-12-16 17:22:33 +0000

    change markup around related entities snippet

--- themes/default/views/Details/snippets/related_entities_by_rel_type_html.php
+++ themes/default/views/Details/snippets/related_entities_by_rel_type_html.php
@@ -1,13 +1,11 @@
 <?php
-	$t_object = 			$this->getVar("item");
+	$t_item = 			$this->getVar("item");
 	$va_access_values = 	$this->getVar("access_values");
 	$va_restrict_to_relationship_types = 	$this->getVar("restrict_to_relationship_types");
 	
-	$va_entities = $t_object->get("ca_entities", array("returnWithStructure" => 1, "checkAccess" => $va_access_values, "restrictToRelationshipTypes" => $va_restrict_to_relationship_types));
+	$va_entities = $t_item->get("ca_entities", array("returnWithStructure" => 1, "checkAccess" => $va_access_values, "restrictToRelationshipTypes" => $va_restrict_to_relationship_types));
+	# --- note this needs to be placed within <dl></dl> tags
 	if(is_array($va_entities) && sizeof($va_entities)){
-?>
-		<dl class="mb-0">
-<?php
 		$va_entities_by_type = array();
 		foreach($va_entities as $va_entity_info){
 			$va_entities_by_type[$va_entity_info["relationship_typename"]][] = caDetailLink($this->request, $va_entity_info["displayname"], "", "ca_entities", $va_entity_info["entity_id"]);
@@ -15,8 +13,5 @@
 		foreach($va_entities_by_type as $vs_type => $va_entity_links){
 			print "<dt class='text-capitalize'>".$vs_type."</dt><dd>".join(", ", $va_entity_links)."</dd>";
 		}
-?>
-		</dl>
-<?php
 	}
 ?>					
