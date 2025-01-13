54a62a3b081b6e25cdd3468d6a5cfe8941b528b4
CollectiveAccess <seth@collectiveaccess.org>
2025-01-13 15:52:11 -0500

    Working ban screen

--- themes/default/views/Ban/verify_html.php
+++ themes/default/views/Ban/verify_html.php
@@ -28,14 +28,13 @@
 if($this->request->isAjax()) { return; }
 $errors = $this->getVar('errors');
 
-if(is_array($errors)) {
-	foreach($errors as $e) {
-		print "Error: $e\n<br/>";
-	}
+if(is_array($errors) && sizeof($errors)){
+	print "<div class='alert alert-danger'>".implode("<br/>", $errors)."</div>";
 }
 ?>
 
 <?= caFormTag($this->request, 'confirm', 'banCaptcha', 'Ban', 'post', 'multipart/form-data', '_top', ['disableUnsavedChangesWarning' => true]); ?>
+	<div class="bg-light px-4 pt-4 pb-2 mb-4">
 <?php
 	if(!$this->request->isLoggedIn() && defined("__CA_GOOGLE_RECAPTCHA_KEY__") && __CA_GOOGLE_RECAPTCHA_KEY__){
 ?>
@@ -48,57 +47,56 @@ if(is_array($errors)) {
                 	}
                 });
         	};
-        	jQuery(document).ready(function() {
-        		jQuery('#banContinueButton').on('click', function() {
-        			jQuery('#banCaptcha').submit(); 
-        			return false;
-        		});
-        	});
 		</script>
 		<script src='https://www.google.com/recaptcha/api.js?onload=gCaptchaRender&render=explicit' async defer></script>
 		<div class="row">
-			<div class="col-sm-12 col-md-offset-1 col-md-10">
+			<div class="col-sm-12 offset-md-3 col-md-10">
 				<div class='banHeadline'>
 					<h1><?= _t('Please confirm that you are not a robot'); ?></h1>
 				</div>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12 col-md-offset-3 col-md-6">
+			<div class="col-sm-12 offset-md-3 col-md-6">
 				<div class='banDescription'>
 					<?= _t("We have a hunch that you might be a robot scanning this site for data. Please click on the <i>I'm not a robot</i> button and follow the provided instructions to confirm your status as a sentient human being."); ?>
 				</div>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12 col-md-offset-1 col-md-10">
+			<div class="col-sm-12 offset-md-3 col-md-10">
 				<div class='form-group<?= (($errors["recaptcha"]) ? " has-error" : ""); ?>'>
 					<div id="regCaptcha" class="col-sm-8 col-sm-offset-4"></div>
 				</div>
 			</div>
 		</div><!-- end row -->
 		
-		<div id='regCaptchaSubmit' class='banContinue'>
-			<span class="btn btn-default"><a href="#" id='banContinueButton'><?= _t('Click here to continue'); ?></a></span>
+		<div class="row">
+			<div class="col-sm-12 offset-md-3 col-md-10">
+				<div id='regCaptchaSubmit' class='banContinue'>
+					<button href="#" class="btn btn-primary" id="banContinueButton" onclick="document.getElementById('banCaptcha').submit(); return false;"><?= _t('Click here to continue'); ?></button>
+				</div>
+			</div>
 		</div>
 <?php
 	} else {
 ?>
 	<div class="row">
-			<div class="col-sm-12 col-md-offset-1 col-md-10">
-				<div class='banHeadline'>
-					<h1><?= _t('We think you might be a robot'); ?></h1>
-				</div>
+		<div class="col-sm-12 offset-md-3 col-md-10">
+			<div class='banHeadline'>
+				<h1><?= _t('We think you might be a robot'); ?></h1>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-sm-12 col-md-offset-3 col-md-6">
-				<div class='banDescription'>
-					<?= _t("We have a hunch that you might be a robot scanning this site for data. If you're a real, live sentient human being please contact the site administrator at <a href='mailto:%1'>%1</a> to regain access.", __CA_ADMIN_EMAIL__); ?>
-				</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12 offset-md-3 col-md-6">
+			<div class='banDescription'>
+				<?= _t("We have a hunch that you might be a robot scanning this site for data. If you're a real, live sentient human being please contact the site administrator at <a href='mailto:%1'>%1</a> to regain access.", __CA_ADMIN_EMAIL__); ?>
 			</div>
 		</div>
+	</div>
 <?php
 	}
 ?>
+	</div>
 </form>
