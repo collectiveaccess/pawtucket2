86f6a8f0e566e8a0b18ca73bc2a5cb9d2cba1a6e
seth <Seth>
2025-03-01 11:49:25 -0500

    Reenable ajax load

--- app/controllers/BrowseController.php
+++ app/controllers/BrowseController.php
@@ -418,7 +418,7 @@
 				    'color' => '#cc0000', 
 				    'labelTemplate' => caGetOption('labelTemplate', $va_view_info['display'], null),
 				    'contentTemplate' => caGetOption('contentTemplate', $va_view_info['display'], null),
-				    //'ajaxContentUrl' => (caGetOption('title_template', $va_view_info['display'], null) || caGetOption('description_template', $va_view_info['display'], null)) ? caNavUrl($this->request, '*', '*', 'AjaxGetMapItem', array('browse' => $ps_function,'view' => $ps_view)) : null,
+				    'ajaxContentUrl' => (caGetOption('title_template', $va_view_info['display'], null) || caGetOption('description_template', $va_view_info['display'], null)) ? caNavUrl($this->request, '*', '*', 'AjaxGetMapItem', array('browse' => $ps_function,'view' => $ps_view)) : null,
 				    'excludeRelationshipTypes' => caGetOption('excludeRelationshipTypes', $va_view_info['display'], null),
 				    'ajaxContentUrl' => caGetOption('ajaxContentUrl', $va_view_info['display'], null),
 				);
