a4a4dae74de414512ef5d752cec55da255992f4a
Callie Holmes <ceholmes@uga.edu>
2025-03-10 09:10:47 -0400

    Editing licensing page information per RA document

--- themes/uga/views/Licensing/Index.php
+++ themes/uga/views/Licensing/Index.php
@@ -6,57 +6,68 @@
 	<div class="col-sm-8">
 	<p>If you are interested in obtaining a copy of a collection item, please contact Ruta Abolins at <a href="mailto:abolins@uga.edu">abolins@uga.edu</a> or call 706-542-4757.</p>
 <p><a href="../Peabody/Index">The Peabody Awards Collection</a> consists almost entirely of copyrighted material. If you are interested in something from this collection please check if it is available directly from the copyright holder. </p>
-<p>We do make copies of newsfilm, home movies, town films, and some other collections upon request, without the need for copyright holder permission. Please contact us directly for that information.</p>
+<p>We charge usage fees based on our ownership of the physical materials and ability to provide access to the intellectual content.</p>
 <p><strong>Research &amp; Stock Footage</strong><br>
-  Producers and researchers can order stock footage from the collections at Walter J. Brown Media Archive &amp; Peabody Awards Collection by contacting Ruta Abolins at the archive at <a href="mailto:abolins@uga.edu">abolins@uga.edu</a> or 706-542-4757. Please indicate the topic and how the material will be used; the archives states the procedure for ordering and the fees charged by the archives for use. Reuse of moving and still images, including those found on the Walter J. Brown Media Archive &amp; Peabody Awards Collection website, requires permission.</p>
-<p><strong>Screeners:</strong><br>
-  A fee is charged for screeners from certain collections; in general, the ocst is $20.00 per item. Screeners are made available via download with embedded watermark and timecode.</p>
-<p><strong>Technical Services:</strong><br>
-  If it is necessary to go to an outside vendor for duplication, laboratory or facility will be specified by the Walter J. Brown Media Archives &amp; Peabody Awards Collection. All costs of duplication must be paid by client. If an item must be digitized to higher resolution, such as 16mm newsfilm, a lab fee of $250.00 will be charged for each clip.</p>
-<p><strong>Screen Credit:</strong><br>
+  Producers and researchers can order stock footage from the collections at Walter J. Brown Media Archive & Peabody Awards Collection. The archive has a form for ordering clean footage.  Contact Ruta Abolins at <a href="mailto:abolins@uga.edu">abolins@uga.edu</a> or 706-542-4757. <br>
+  The first half-hour of research is always free.
+  Reuse of video, still images, and audio found on the Walter J. Brown Media Archive &amp; Peabody Awards Collection website requires permission.</p>
+<p><strong>Screeners</strong><br>
+  Screeners are made available via download with embedded watermark and timecode by downloading the item from the video screen in the item record. If the item has not been digitized click on "request item" at the bottom of the record for each item you are interested in. There is usually no charge. It generally takes 2-4 weeks from the time a request is submitted for a screener to be uploaded to our website.</p>
+<p><strong>Technical Services</strong><br>
+  If it is necessary to go to an outside vendor for duplication, laboratory or facility will be specified by the Walter J. Brown Media Archives &amp; Peabody Awards Collection. All costs of duplication must be paid by client. If an item must be digitized to higher resolution, such as 16mm newsfilm, a lab fee of $250.00 will be charged for each reel.</p>
+<p><strong>Screen Credit</strong><br>
 Unless a waiver is received, the completed work must carry the following credit:</p>
 <p>[Collection Name], University of Georgia Libraries.</p>
-  
- <p>A copy of the completed work must be donated to the Walter J. Brown Media Archives &amp; Peabody Awards Collection.</p>
-</div>
 
-<div class="col-sm-3 col-sm-offset-1">
 <H1>Licensing Fee Schedule</H1>	
-<h3>Usage Fees Per Second, Per Order</h3>
-  <em>Student</em><br>
-  School production only: $10<br>
-  <br>
-  <em>Streaming on website</em><br>
-  Corporate $25<br>
-  Educational $20<br>
-  <br>
+
+<p>A minimum charge of 10 seconds applies to all projects. In some cases this fee may be waived and a new per-second rate or flat fee can be negotiated.</p>
+<p>To purchase rights for dissemination to more than one market, compute the highest rate first. Additional outlets are available at 50% of listed per-second rate.</p>
+<p>Regional productions may be eligible for a discount on use fees. Please inquire for more information.</p>
+<p>A rush fee of $250 will be charged if content is needed in less than 5 business days.</p>
+
+<h3>Usage Fees Per Second, Per Order (with a 10 second minimum)</h3>
   <em>Museum</em><br>
-  Profit or non-profit $25<br>
+  Profit or non-profit $25 per second<br>
   <br>
-  <em>Television</em><br>
-  Local: $30<br>
-  North America: $50<br>
-  Worldwide: $75<br>
+
+  <em>Non-broadcast / website use</em><br>
+  Corporate $25 per second<br>
+  Educational $20 per second<br>
   <br>
+
   <em>Commercials</em><br>
-  Local $30<br>
-  National $60<br>
-  Worldwide $80<br>
-  <em><br>
-  Theatrical Release Only</em> $75<br>
-  <em><br>
-  Home Video/DVD Only</em><br>
-  All Markets $50<br>
-  <em><br>
-  Non-broadcast</em><br>
-  Corporate $25<br>
-  Educational $20<br>
+  Local $30 per second<br>
+  National $60 per second<br>
+  Worldwide $80 per second <br>
   <br>
-  <em>All Rights, All Media</em><br>
-  $95<br>
+
+  <em>Streaming video (YouTube, etc.)</em><br>
+  $10 per second<br>
   <br>
-  <em>Other Uses:</em> <br>
-  Please email <a href="mailto:abolins@uga.edu">Ruta Abolins</a> or call 706-542-4757</p>
+  
+  <em>Broadcast Television (TV, Cable, Satellite)</em><br>
+  Local $30 per second<br>
+  National $50 per second<br>
+  Worldwide $80 per second<br>
+  <br>
+
+  <em>Film Festivals</em><br>
+  $50 per second<br>
+  <br>
+
+  <em>Theatrical Release Only</em> <br>
+  $75 per second<br>
+  <br>
+
+  <em>DVD/Blu-Ray Only</em><br>
+  500 or less (local, single source sale) $20 per second<br>
+  500+ (wholesale, multiple sales outlets) $35 per second<br>
+  <br>
+
+  <em>All Rights, All Media, in perpetuity</em><br>
+  $95 per second<br>
+  <br>
 		
-	</div>
+
 </div>
