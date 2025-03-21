bf5b19c0f33f58f84ce4a7321b18945313f5420f
NadeenMK <nadeenmkhalfe@gmail.com>
2025-03-21 17:28:17 +0200

    Updated styles, login, register, footer, header, and app.conf

--- themes/default/views/pageFormat/pageFooter.php
+++ themes/default/views/pageFormat/pageFooter.php
@@ -1,91 +1,84 @@
 <?php
 /* ----------------------------------------------------------------------
- * views/pageFormat/pageFooter.php : 
- * ----------------------------------------------------------------------
- * CollectiveAccess
- * Open-source collections management software
- * ----------------------------------------------------------------------
- *
- * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2015-2022 Whirl-i-Gig
- *
- * For more information visit http://www.CollectiveAccess.org
- *
- * This program is free software; you may redistribute it and/or modify it under
- * the terms of the provided license as published by Whirl-i-Gig
- *
- * CollectiveAccess is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- *
- * This source code is free and modifiable under the terms of 
- * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
- * the "license.txt" file for details, or visit the CollectiveAccess web site at
- * http://www.CollectiveAccess.org
- *
- * ----------------------------------------------------------------------
- */
+ * views/pageFormat/pageFooter.php
+ * ---------------------------------------------------------------------- */
 ?>
-		<div style="clear:both; height:1px;"><!-- empty --></div>
-		</div><!-- end pageArea --></div><!-- end main --></div><!-- end col --></div><!-- end row --></div><!-- end container -->
-		<footer id="footer" role="contentinfo">
-			<ul class="list-inline pull-right social">
-				<li><i class="fa fa-twitter" aria-label="<?php print _t("Twitter"); ?>"></i></li>
-				<li><i class="fa fa-facebook-square" aria-label="<?php print _t("Facebook"); ?>"></i></li>
-				<li><i class="fa fa-youtube-play" aria-label="<?php print _t("YouTube"); ?>"></i></li>
-			</ul>
-			<div>
-				Footer text here
-			</div>
-			<ul class="list-inline">
-				<?= ((CookieOptionsManager::cookieManagerEnabled()) ? "<li>".caNavLink($this->request, _t("Manage Cookies"), "", "", "Cookies", "manage")."</li>" : ""); ?></li>
-				<li><a href="#">Link</a></li>
-				<li><a href="#">Link</a></li>
-			</ul>
-			<div><small>&copy; <a href="https://www.collectiveaccess.org">CollectiveAccess 2022</a></small></div>
-		</footer><!-- end footer -->
-<?php
-	//
-	// Output HTML for debug bar
-	//
-	if(Debug::isEnabled()) {
-		print Debug::$bar->getJavascriptRenderer()->render();
-	}
-?>
-	
-		<?= TooltipManager::getLoadHTML(); ?>
-		<div id="caMediaPanel" role="complementary"> 
-			<div id="caMediaPanelContentArea">
-			
-			</div>
-		</div>
-		<script type="text/javascript">
-			/*
-				Set up the "caMediaPanel" panel that will be triggered by links in object detail
-				Note that the actual <div>'s implementing the panel are located here in views/pageFormat/pageFooter.php
-			*/
-			var caMediaPanel;
-			jQuery(document).ready(function() {
-				if (caUI.initPanel) {
-					caMediaPanel = caUI.initPanel({ 
-						panelID: 'caMediaPanel',										/* DOM ID of the <div> enclosing the panel */
-						panelContentID: 'caMediaPanelContentArea',		/* DOM ID of the content area <div> in the panel */
-						onCloseCallback: function(data) {
-							if(data && data.url) {
-								window.location = data.url;
-							}
-						},
-						exposeBackgroundColor: '#FFFFFF',						/* color (in hex notation) of background masking out page content; include the leading '#' in the color spec */
-						exposeBackgroundOpacity: 0.7,							/* opacity of background color masking out page content; 1.0 is opaque */
-						panelTransitionSpeed: 400, 									/* time it takes the panel to fade in/out in milliseconds */
-						allowMobileSafariZooming: true,
-						mobileSafariViewportTagID: '_msafari_viewport',
-						closeButtonSelector: '.close'					/* anything with the CSS classname "close" will trigger the panel to close */
-					});
-				}
-			});
-			/*(function(e,d,b){var a=0;var f=null;var c={x:0,y:0};e("[data-toggle]").closest("li").on("mouseenter",function(g){if(f){f.removeClass("open")}d.clearTimeout(a);f=e(this);a=d.setTimeout(function(){f.addClass("open")},b)}).on("mousemove",function(g){if(Math.abs(c.x-g.ScreenX)>4||Math.abs(c.y-g.ScreenY)>4){c.x=g.ScreenX;c.y=g.ScreenY;return}if(f.hasClass("open")){return}d.clearTimeout(a);a=d.setTimeout(function(){f.addClass("open")},b)}).on("mouseleave",function(g){d.clearTimeout(a);f=e(this);a=d.setTimeout(function(){f.removeClass("open")},b)})})(jQuery,window,200);*/
-		</script>
-		<?= $this->render("Cookies/banner_html.php"); ?>
-	</body>
+<div style="clear:both; height:1px;"><!-- empty --></div>
+</div><!-- end pageArea --></div><!-- end main --></div><!-- end col --></div><!-- end row --></div><!-- end container -->
+
+<footer class="footer" role="contentinfo">
+<link rel="stylesheet" href="/pawtucket2/themes/default/assets/pawtucket/css/fonts.css" type="text/css" media="all">	
+<link rel="stylesheet" href="/pawtucket2/themes/default/assets/pawtucket/css/main.css" type="text/css" media="all">
+
+    <div class="container">
+        <div class="footer-content">
+            <!-- Left Section - Project Name -->
+            <div class="footer-section">
+                <p class="project-name">مشروع الأرشفة الإلكتروني الخاص بالجامعة العربية الامريكية</p>
+            </div>
+
+            <!-- Center Section - University Logo & Name -->
+            <div class="footer-section text-center">
+                <img src="<?= __CA_URL_ROOT__ ?>/themes/default/assets/pawtucket/graphics/logos/aaup_logo.svg" alt="AAUP Logo" class="logo-img">
+                <p class="university-name">الجامعة العربية الامريكية</p>
+            </div>
+
+            <!-- Right Section - Contact Info -->
+            <div class="footer-section contact-info">
+                <a href="mailto:infa@aaup.edu" class="contact-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="contact-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                        <polyline points="22,6 12,13 2,6"></polyline>
+                    </svg>
+                    infa@aaup.edu
+                </a>
+                <a href="tel:+97042418888" class="contact-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="contact-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                    </svg>
+                    +970 4 2418888
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <style>
+    </style>
+</footer>
+
+<?php if(Debug::isEnabled()): ?>
+    <?php print Debug::$bar->getJavascriptRenderer()->render(); ?>
+<?php endif; ?>
+
+<?= TooltipManager::getLoadHTML(); ?>
+<div id="caMediaPanel" role="complementary"> 
+    <div id="caMediaPanelContentArea">
+    </div>
+</div>
+
+<script type="text/javascript">
+    var caMediaPanel;
+    jQuery(document).ready(function() {
+        if (caUI.initPanel) {
+            caMediaPanel = caUI.initPanel({ 
+                panelID: 'caMediaPanel',
+                panelContentID: 'caMediaPanelContentArea',
+                onCloseCallback: function(data) {
+                    if(data && data.url) {
+                        window.location = data.url;
+                    }
+                },
+                exposeBackgroundColor: '#FFFFFF',
+                exposeBackgroundOpacity: 0.7,
+                panelTransitionSpeed: 400,
+                allowMobileSafariZooming: true,
+                mobileSafariViewportTagID: '_msafari_viewport',
+                closeButtonSelector: '.close'
+            });
+        }
+    });
+</script>
+
+<?= $this->render("Cookies/banner_html.php"); ?>
+</body>
 </html>
--- themes/default/views/pageFormat/pageHeader.php
+++ themes/default/views/pageFormat/pageHeader.php
@@ -1,158 +1,80 @@
 <?php
-/* ----------------------------------------------------------------------
- * views/pageFormat/pageHeader.php : 
- * ----------------------------------------------------------------------
- * CollectiveAccess
- * Open-source collections management software
- * ----------------------------------------------------------------------
- *
- * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2014-2022 Whirl-i-Gig
- *
- * For more information visit http://www.CollectiveAccess.org
- *
- * This program is free software; you may redistribute it and/or modify it under
- * the terms of the provided license as published by Whirl-i-Gig
- *
- * CollectiveAccess is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- *
- * This source code is free and modifiable under the terms of 
- * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
- * the "license.txt" file for details, or visit the CollectiveAccess web site at
- * http://www.CollectiveAccess.org
- *
- * ----------------------------------------------------------------------
- */
 $lightboxDisplayName = caGetLightboxDisplayName();
 $lightbox_sectionHeading = ucFirst($lightboxDisplayName["section_heading"]);
 
-# Collect the user links: they are output twice, once for toggle menu and once for nav
+# Collect the user links
 $user_links = array();
-if($this->request->isLoggedIn()){
-	$user_links[] = '<li role="presentation" class="dropdown-header">'.trim($this->request->user->get("fname")." ".$this->request->user->get("lname")).', '.$this->request->user->get("email").'</li>';
-	$user_links[] = '<li class="divider nav-divider"></li>';
-	if(caDisplayLightbox($this->request)){
-		$user_links[] = "<li>".caNavLink($this->request, $lightbox_sectionHeading, '', '', 'Lightbox', 'Index', array())."</li>";
-	}
-	$user_links[] = "<li>".caNavLink($this->request, _t('User Profile'), '', '', 'LoginReg', 'profileForm', array())."</li>";
-	
-	if ($this->request->config->get('use_submission_interface')) {
-		$user_links[] = "<li>".caNavLink($this->request, _t('Submit content'), '', '', 'Contribute', 'List', array())."</li>";
-	}
-	$user_links[] = "<li>".caNavLink($this->request, _t('Logout'), '', '', 'LoginReg', 'Logout', array())."</li>";
-} else {	
-	if (!$this->request->config->get(['dontAllowRegistrationAndLogin', 'dont_allow_registration_and_login']) || $this->request->config->get('pawtucket_requires_login')) { $user_links[] = "<li><a href='#' onclick='caMediaPanel.showPanel(\"".caNavUrl($this->request, '', 'LoginReg', 'LoginForm', array())."\"); return false;' >"._t("Login")."</a></li>"; }
-	if (!$this->request->config->get(['dontAllowRegistrationAndLogin', 'dont_allow_registration_and_login']) && !$this->request->config->get('dontAllowRegistration')) { $user_links[] = "<li><a href='#' onclick='caMediaPanel.showPanel(\"".caNavUrl($this->request, '', 'LoginReg', 'RegisterForm', array())."\"); return false;' >"._t("Register")."</a></li>"; }
+if ($this->request->isLoggedIn()) {
+    $user_links[] = '<li role="presentation" class="dropdown-header">' . trim($this->request->user->get("fname") . " " . $this->request->user->get("lname")) . ', ' . $this->request->user->get("email") . '</li>';
+    $user_links[] = '<li class="divider nav-divider"></li>';
+    $user_links[] = "<li>" . caNavLink($this->request, _t('User Profile'), '', '', 'LoginReg', 'profileForm', array()) . "</li>";
+    $user_links[] = "<li>" . caNavLink($this->request, _t('Logout'), '', '', 'LoginReg', 'Logout', array()) . "</li>";
+} else {
+    $user_links[] = "<li><a href='#' onclick='caMediaPanel.showPanel(\"" . caNavUrl($this->request, '', 'LoginReg', 'LoginForm', array()) . "\"); return false;' >" . _t("Login") . "</a></li>";
+    $user_links[] = "<li><a href='#' onclick='caMediaPanel.showPanel(\"" . caNavUrl($this->request, '', 'LoginReg', 'RegisterForm', array()) . "\"); return false;' >" . _t("Register") . "</a></li>";
 }
 $has_user_links = (sizeof($user_links) > 0);
-
 ?><!DOCTYPE html>
-<html lang="en">
-	<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
-	<?= MetaTagManager::getHTML(); ?>
-	<?= AssetLoadManager::getLoadHTML($this->request); ?>
+<html lang="ar">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <?= MetaTagManager::getHTML(); ?>
+    <?= AssetLoadManager::getLoadHTML($this->request); ?>
+    <title><?= (MetaTagManager::getWindowTitle()) ? MetaTagManager::getWindowTitle() : $this->request->config->get("app_display_name"); ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/pawtucket2/themes/default/assets/pawtucket/css/fonts.css" type="text/css" media="all">	
+	<link rel="stylesheet" href="/pawtucket2/themes/default/assets/pawtucket/css/main.css" type="text/css" media="all">
 
-	<title><?= (MetaTagManager::getWindowTitle()) ? MetaTagManager::getWindowTitle() : $this->request->config->get("app_display_name"); ?></title>
-	
-	<script type="text/javascript">
-		jQuery(document).ready(function() {
-    		jQuery('#browse-menu').on('click mouseover mouseout mousemove mouseenter',function(e) { e.stopPropagation(); });
-    	});
-	</script>
-<?php
-	if(Debug::isEnabled()) {		
-		//
-		// Pull in JS and CSS for debug bar
-		// 
-		$o_debugbar_renderer = Debug::$bar->getJavascriptRenderer();
-		$o_debugbar_renderer->setBaseUrl(__CA_URL_ROOT__.$o_debugbar_renderer->getBaseUrl());
-		print $o_debugbar_renderer->renderHead();
-	}
-?>
+    
+
 </head>
 <body>
-	<div id="skipNavigation"><a href="#main">Skip to main content</a></div>
-	<nav class="navbar navbar-default yamm" role="navigation">
-		<div class="container menuBar">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-<?php
-	if ($has_user_links) {
-?>
-				<button type="button" class="navbar-toggle navbar-toggle-user" data-toggle="collapse" data-target="#user-navbar-toggle">
-					<span class="sr-only">User Options</span>
-					<span class="glyphicon glyphicon-user"></span>
-				</button>
-<?php
-	}
-?>
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-main-navbar-collapse-1">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-<?php
-				print caNavLink($this->request, caGetMenuBarLogo(), "navbar-brand", "", "","");
-?>
-			</div>
+<header>
 
-		<!-- Collect the nav links, forms, and other content for toggling -->
-			<!-- bs-user-navbar-collapse is the user menu that shows up in the toggle menu - hidden at larger size -->
-<?php
-	if ($has_user_links) {
-?>
-			<div class="collapse navbar-collapse" id="user-navbar-toggle">
-				<ul class="nav navbar-nav" role="list" aria-label="<?= _t("Mobile User Navigation"); ?>">
-					<?= join("\n", $user_links); ?>
-				</ul>
-			</div>
-<?php
-	}
-?>
-			<div class="collapse navbar-collapse" id="bs-main-navbar-collapse-1">
-<?php
-	if ($has_user_links) {
-?>
-				<ul class="nav navbar-nav navbar-right" id="user-navbar" role="list" aria-label="<?= _t("User Navigation"); ?>">
-					<li class="dropdown" style="position:relative;">
-						<a href="#" class="dropdown-toggle icon" data-toggle="dropdown"><span class="glyphicon glyphicon-user" aria-label="<?= _t("User options"); ?>"></span></a>
-						<ul class="dropdown-menu" role="list"><?= join("\n", $user_links); ?></ul>
-					</li>
-				</ul>
-<?php
-	}
-?>
-				<form class="navbar-form navbar-right" role="search" action="<?= caNavUrl($this->request, '', 'MultiSearch', 'Index'); ?>" aria-label="<?= _t("Search"); ?>">
-					<div class="formOutline">
-						<div class="form-group">
-							<input type="text" class="form-control" id="headerSearchInput" placeholder="Search" name="search" autocomplete="off" aria-label="<?= _t("Search text"); ?>" />
-						</div>
-						<button type="submit" class="btn-search" id="headerSearchButton"><span class="glyphicon glyphicon-search" aria-label="<?= _t("Submit"); ?>"></span></button>
-					</div>
-				</form>
-				<script type="text/javascript">
-					$(document).ready(function(){
-						$('#headerSearchButton').prop('disabled',true);
-						$('#headerSearchInput').on('keyup', function(){
-							$('#headerSearchButton').prop('disabled', this.value == "" ? true : false);     
-						})
-					});
-				</script>
-				<ul class="nav navbar-nav navbar-right menuItems" role="list" aria-label="<?= _t("Primary Navigation"); ?>">
-					<li <?= ($this->request->getController() == "About") ? 'class="active"' : ''; ?>><?= caNavLink($this->request, _t("About"), "", "", "About", "Index"); ?></li>
-					<?= $this->render("pageFormat/browseMenu.php"); ?>	
-					<li <?= (($this->request->getController() == "Search") && ($this->request->getAction() == "advanced")) ? 'class="active"' : ''; ?>><?= caNavLink($this->request, _t("Advanced Search"), "", "", "Search", "advanced/objects"); ?></li>
-					<li <?= ($this->request->getController() == "Gallery") ? 'class="active"' : ''; ?>><?= caNavLink($this->request, _t("Gallery"), "", "", "Gallery", "Index"); ?></li>
-					<li <?= ($this->request->getController() == "Collections") ? 'class="active"' : ''; ?>><?= caNavLink($this->request, _t("Collections"), "", "", "Collections", "index"); ?></li>					
-					<li <?= ($this->request->getController() == "Contact") ? 'class="active"' : ''; ?>><?= caNavLink($this->request, _t("Contact"), "", "", "Contact", "Form"); ?></li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- end container -->
-	</nav>
-	<div class="container"><div class="row"><div class="col-xs-12">
-		<div role="main" id="main"><div id="pageArea" <?= caGetPageCSSClasses(); ?>>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container">
+            <div class="navbar-container d-flex justify-content-between align-items-center">
+                <!-- Logo on the left -->
+                <a class="navbar-brand" href="<?= caNavUrl($this->request, '', 'Home', 'Index'); ?>">
+                    <img src="<?= __CA_URL_ROOT__ ?>/themes/default/assets/pawtucket/graphics/logos/aaup_logo.svg" alt="AAUP Logo" class="logo-img">
+                </a>
+                
+                <!-- Title in the absolute center -->
+                <div class="title-container">
+                    <h1 class="site-title">ذاكرة فلسطين</h1>
+                </div>
+
+                <!-- Buttons on the right -->
+                <div class="ml-auto">
+                    <?php if (!$this->request->isLoggedIn()): ?>
+                        <a href="#" class="btn btn-outline-warning btn-login" onclick="caMediaPanel.showPanel('<?= caNavUrl($this->request, '', 'LoginReg', 'LoginForm', array()); ?>'); return false;">تسجيل الدخول</a>
+                        <a href="#" class="btn btn-warning btn-register" onclick="caMediaPanel.showPanel('<?= caNavUrl($this->request, '', 'LoginReg', 'RegisterForm', array()); ?>'); return false;">إنشاء حساب</a>
+                    <?php else: ?>
+                        <div class="dropdown">
+                            <button class="btn btn-outline-warning dropdown-toggle" type="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <?= trim($this->request->user->get("fname") . " " . $this->request->user->get("lname")) ?>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                                <?= caNavLink($this->request, _t('الملف الشخصي'), '', '', 'LoginReg', 'profileForm', array(), ['class' => 'dropdown-item']) ?>
+                                <?= caNavLink($this->request, _t('تسجيل الخروج'), '', '', 'LoginReg', 'Logout', array(), ['class' => 'dropdown-item']) ?>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<!-- Rest of your content goes here -->
+
+<!-- Bootstrap JS and dependencies -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
