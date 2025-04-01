f5aa9482efce035262d327f6420617b9fe0d93f7
Maria <maria@whirl-i-gig.com>
2025-04-01 13:55:59 +0000

    meetjesland home page image

--- themes/erfgoed_meetjesland_new/views/Front/front_page_html.php
+++ themes/erfgoed_meetjesland_new/views/Front/front_page_html.php
@@ -33,7 +33,7 @@
  $va_access_values = $this->getVar("access_values");
  $vs_hero = $this->request->getParameter("hero", pString);
  if(!$vs_hero){
- 	$vs_hero = rand(1, 4);
+ 	$vs_hero = rand(1, 3);
  }
 ?>
 
