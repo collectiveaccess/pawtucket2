0b657804ba4dace51e977f77e883da75c54f257e
Maria <maria@whirl-i-gig.com>
2025-04-04 21:23:35 +0000

    reformat cookie manager

--- themes/default/views/Cookies/form_manage_html.php
+++ themes/default/views/Cookies/form_manage_html.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2021 Whirl-i-Gig
+ * Copyright 2025 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -34,67 +34,54 @@ if(!($config->get("cookiesIntroGlobalValue") && $intro = $this->getVar($config->
 }
 ?>
 
-<div class="row justify-content-center">
-    <div class="col-12 col-lg-10">
         <h1><?= _t("Manage Cookies"); ?></h1>
-        <div class="cookieIntro"><?php print $intro; ?></div>
+        <div class="mb-4 mt-3 fs-4"><?php print $intro; ?></div>
         <form id="CookieForm" action="<?= caNavUrl($this->request, '*', '*', 'save'); ?>" class="needs-validation" novalidate method="POST">
 <?php
     foreach ($cookies_by_category as $category_code => $category_info) {
 ?>
-            <div class="row mt-4">
-                <div class="col-12">
-                    <hr />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 bg-light py-2">
-                    <label class="form-label"><?= caGetOption('title', $category_info, '???'); ?></label>
-                    <div class="cookieByCategory">
-                        <div class="cookieCount">
-                            <?= caGetOption('cookieCount', $category_info, ''); ?> <i class="bi bi-arrow-right-circle-fill" aria-hidden="true"></i>
-                        </div>
-
-                        <div class="cookiesList d-none">
-                            <div class="row">
-                                <div class="col">
-                                    <b>Name</b>
-                                </div>
-                                <div class="col">
-                                    <b>Description</b>
-                                </div>
-                            </div>
-                        <?php
-                                foreach ($category_info['cookies'] as $cookie_code => $cookie_info) {
-                        ?>
-                            <div class="row">
-                                <div class="col">
-                                    <?= caGetOption('name', $cookie_info, '???'); ?>
-                                </div>
-                                <div class="col">
-                                    <?= caGetOption('description', $cookie_info, '???'); ?>
-                                </div>
-                            </div>
-                        <?php
-                                }
-                        ?>
-                        </div>
-
-                    </div>
-                    <div><?= caGetOption('description', $category_info, ''); ?></div>
-                </div>
-
-                <div class="col-12 text-center my-3">
+		<div class="bg-light p-4 mb-4">
+			<div class="row">
+                <div class="col-sm-12 col-md-8">
+                    <div class="fw-bold fs-4"><?= caGetOption('title', $category_info, '???'); ?></div>
+						<button class="btn btn-white btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#cookieDesc<?= $category_code; ?>" aria-expanded="false" aria-controls="#cookieDesc<?= $category_code; ?>"><?= caGetOption('cookieCount', $category_info, ''); ?> <i class="bi bi-arrow-right-circle-fill" aria-hidden="true"></i></button>
+						<div id="cookieDesc<?= $category_code; ?>" class="collapse p-3 pb-0 bg-white mt-3">
+							<div class="row fw-bold">
+								<div class="col-3">
+									Name
+								</div>
+								<div class="col-9">
+									Description
+								</div>
+							</div>
+<?php
+				foreach ($category_info['cookies'] as $cookie_code => $cookie_info) {
+?>
+							<div class="row pb-3">
+								<div class="col-3">
+									<?= caGetOption('name', $cookie_info, '???'); ?>
+								</div>
+								<div class="col-9">
+									<?= caGetOption('description', $cookie_info, '???'); ?>
+								</div>
+							</div>
+<?php
+				}
+?>
+						</div>
+						<div class="pt-3"><?= caGetOption('description', $category_info, ''); ?></div>
+					</div>
+                <div class="col-sm-12 col-md-4 text-center pt-3 pt-md-0">
 <?php
         if (!caGetOption('required', $category_info, false)) {
             $allow = (bool)CookieOptionsManager::allow($category_code);
 ?>
 					<div class="btn-group rounded" role="group" aria-label="Toggle">
-						<button type="button" class="btn btn-success<?= $allow ? ' btn-success' : ' btn-light'; ?>" data-value="1" data-code="<?= $category_code; ?>">
+						<button type="button" class="btn <?= $allow ? ' btn-success' : ' btn-light bg-white'; ?>" data-value="1" data-code="<?= $category_code; ?>">
 							<?= _t('ON'); ?>
 						</button>
 						<input type="hidden" name="<?= "cookie_options_{$category_code}"; ?>" id="<?= "cookie_options_{$category_code}"; ?>" value="<?= $allow ? 1 : 0; ?>" />
-						<button type="button" class="btn btn-success<?= !$allow ? ' btn-success' : ' btn-light'; ?>" data-value="0" data-code="<?= $category_code; ?>">
+						<button type="button" class="btn <?= !$allow ? ' btn-success' : ' btn-light bg-white'; ?>" data-value="0" data-code="<?= $category_code; ?>">
 							<?= _t('OFF'); ?>
 						</button>
 					</div>
@@ -104,25 +91,20 @@ if(!($config->get("cookiesIntroGlobalValue") && $intro = $this->getVar($config->
 ?>
                 </div>
             </div>
+        
+		</div>
 <?php
     }
 ?>
-            <div class="row">
-                <div class="col-12">
-                    <hr />
-                </div>
-            </div>
             <div class="text-center mb-4">
-                <button type="submit" class="btn btn-primary">
+                <button type="submit" class="btn btn-light text-capitalize">
                     <?= _t('Update'); ?>
                 </button>
-                <button class="btn btn-secondary" name="accept_all" value="1">
+                <button class="ms-2 btn btn-success" name="accept_all" value="1">
                     <?= _t('Accept All'); ?>
                 </button>
             </div>
         </form>
-    </div>
-</div>
 
 <script>
     document.addEventListener('DOMContentLoaded', function () {
@@ -137,12 +119,12 @@ if(!($config->get("cookiesIntroGlobalValue") && $intro = $this->getVar($config->
                 const value = button.dataset.value;
 
                 parentGroup.querySelectorAll('.btn').forEach((btn) => {
-                    btn.classList.remove('btn-success', 'btn-light', 'active'); // Remove all related classes
-                    btn.classList.add('btn-light'); // Set inactive state
+                    btn.classList.remove('btn-success', 'btn-light', 'active', 'bg-white'); // Remove all related classes
+                    btn.classList.add('btn-light', 'bg-white'); // Set inactive state
                 });
 
                 // Set the clicked button as active
-                button.classList.remove('btn-light'); // Remove inactive state
+                button.classList.remove('btn-light', 'bg-white'); // Remove inactive state
                 button.classList.add('btn-success', 'active'); // Set active state
 
                 // Update the hidden input value
@@ -154,16 +136,4 @@ if(!($config->get("cookiesIntroGlobalValue") && $intro = $this->getVar($config->
         });
     });
 
-    // Handle cookies list toggle
-    const cookieCategories = document.querySelectorAll('.cookieByCategory');
-
-    cookieCategories.forEach((category) => {
-        category.addEventListener('click', function (event) {
-            const cookiesList = category.querySelector('.cookiesList');
-            if (cookiesList) {
-                cookiesList.classList.toggle('d-none'); //toggling visibility
-            }
-            event.preventDefault();
-        });
-    });
 </script>
