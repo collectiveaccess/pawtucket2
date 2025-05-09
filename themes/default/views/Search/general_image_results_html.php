02ba53dbac4dccbc673ec2c7160cec12a8a50d16
Maria <maria@whirl-i-gig.com>
2025-05-09 20:43:06 +0000

    fix default theme general search non object media display

--- themes/default/views/Search/general_image_results_html.php
+++ themes/default/views/Search/general_image_results_html.php
@@ -27,95 +27,93 @@
  */
  
 	$qr_results 		= $this->getVar('result');
-	$va_block_info 		= $this->getVar('blockInfo');
-	$vs_block 			= $this->getVar('block');
-	$vn_num_items_to_show 	= (int)$this->getVar('numItemsToShow');
+	$block_info 		= $this->getVar('blockInfo');
+	$block 			= $this->getVar('block');
+	$num_items_to_show 	= (int)$this->getVar('numItemsToShow');
 $vb_has_more 		= (bool)$this->getVar('hasMore');
-	$vs_search 			= (string)$this->getVar('search');
+	$search 			= (string)$this->getVar('search');
 	$o_config = $this->getVar("config");
 	$o_browse_config = caGetBrowseConfig();
-	$va_browse_types = array_keys($o_browse_config->get("browseTypes"));
-	$vs_caption_template = $this->getVar('resultCaption');
-	$va_access_values = caGetUserAccessValues($this->request);
-	$vs_table 			= $this->getVar('table');
-	$vs_pk				= $this->getVar('primaryKey');
-	$va_options = $va_block_info["options"];
+	$browse_types = array_keys($o_browse_config->get("browseTypes"));
+	$caption_template = $this->getVar('resultCaption');
+	$access_values = caGetUserAccessValues($this->request);
+	$table 			= $this->getVar('table');
+	$pk				= $this->getVar('primaryKey');
+	$options = $block_info["options"];
 	
 	$o_icons_conf = caGetIconsConfig();
-	$va_object_type_specific_icons = $o_icons_conf->getAssoc("placeholders");
-	if(!($vs_default_placeholder = $o_icons_conf->get("placeholder_media_icon"))){
-		$vs_default_placeholder = "<div class='display-1 text-center d-flex bg-light ca-placeholder'><i class='bi bi-card-image align-self-center w-100' aria-label='media placeholder'></i></div>";
+	$object_type_specific_icons = $o_icons_conf->getAssoc("placeholders");
+	if(!($default_placeholder = $o_icons_conf->get("placeholder_media_icon"))){
+		$default_placeholder = "<div class='display-1 text-center d-flex bg-light ca-placeholder'><i class='bi bi-card-image align-self-center w-100' aria-label='media placeholder'></i></div>";
 	}
-	$vs_image_format = $this->getVar('imageFormat');
-	$vs_image_class = "";
-	if($vs_image_format == "contain"){
-		$vs_image_class = "card-img-top object-fit-contain px-3 pt-3 rounded-0";
+	$image_format = $this->getVar('imageFormat');
+	$image_class = "";
+	if($image_format == "contain"){
+		$image_class = "card-img-top object-fit-contain px-3 pt-3 rounded-0";
 	}else{
-		$vs_image_class = "card-img-top object-fit-cover rounded-0";
+		$image_class = "card-img-top object-fit-cover rounded-0";
 	}
 	
 
 	if ($qr_results->numHits() > 0) {
-		$vn_c = 0;
-		print "<div class='row'><div class='col-12 mt-3 mb-2 text-capitalize fw-bold'>".$qr_results->numHits()." ".(($qr_results->numHits() == 1) ? $va_block_info["labelSingular"] : $va_block_info["labelPlural"])."</div></div>";
+		$c = 0;
+		print "<div class='row'><div class='col-12 mt-3 mb-2 text-capitalize fw-bold'>".$qr_results->numHits()." ".(($qr_results->numHits() == 1) ? $block_info["labelSingular"] : $block_info["labelPlural"])."</div></div>";
 		print "<div class='row'>";
-		if ($vs_table != 'ca_objects') {
-			$va_ids = array();
-			while($qr_results->nextHit() && ($vn_c < $vn_num_items_to_show)) {
-				$va_ids[] = $qr_results->get($vs_pk);
-				$vn_c++;
+		if ($table != 'ca_objects') {
+			$ids = array();
+			while($qr_results->nextHit() && ($c < $num_items_to_show)) {
+				$ids[] = $qr_results->get($pk);
+				$c++;
 			}
-			$va_images = caGetDisplayImagesForAuthorityItems($vs_table, $va_ids, array('version' => 'small', 'relationshipTypes' => caGetOption('selectMediaUsingRelationshipTypes', $va_options, null), 'objectTypes' => caGetOption('selectMediaUsingTypes', $va_options, null), 'checkAccess' => $va_access_values));
+			$images = caGetDisplayImagesForAuthorityItems($table, $ids, array('version' => 'small', 'relationshipTypes' => caGetOption('selectMediaUsingRelationshipTypes', $options, null), 'objectTypes' => caGetOption('selectMediaUsingTypes', $options, null), 'checkAccess' => $access_values));
 		
-			$vn_c = 0;	
+			$c = 0;	
 			$qr_results->seek(1);
 		}
 		
 		$t_list_item = new ca_list_items();
 		while($qr_results->nextHit()) {
-			$vn_id = $qr_results->get("{$vs_table}.{$vs_pk}");
+			$id = $qr_results->get("{$table}.{$pk}");
 			
-			$vs_caption 	= $qr_results->getWithTemplate($vs_caption_template, array("checkAccess" => $va_access_values));
-			$vs_thumbnail = "";
-			$vs_type_placeholder = "";
-			$vs_typecode = "";
+			$caption 	= $qr_results->getWithTemplate($caption_template, array("checkAccess" => $access_values));
+			$thumbnail = "";
+			$type_placeholder = "";
+			$typecode = "";
 			
-			if ($vs_table == 'ca_objects') {
-				if(!($vs_thumbnail = $qr_results->get('ca_object_representations.media.medium', array("checkAccess" => $va_access_values, "class" => $vs_image_class)))){
-					$t_list_item->load($qr_results->get("type_id"));
-					$vs_typecode = $t_list_item->get("idno");
-					if($vs_type_placeholder = caGetPlaceholder($vs_typecode, "placeholder_media_icon")){
-						$vs_thumbnail = $vs_type_placeholder;
-					}else{
-						$vs_thumbnail = $vs_default_placeholder;
-					}
+			$image = $qr_results->get('ca_object_representations.media.medium', array("checkAccess" => $access_values, "class" => $image_class));
+			if(!$image){
+				if($images[$id]){
+					$image = str_replace("<img", "<img class='".$image_class."'", $images[$id]);
 				}
-				$vs_rep_detail_link 	= caDetailLink($this->request, $vs_thumbnail, '', $vs_table, $vn_id);				
-			} else {
-				if($va_images[$vn_id]){
-					$vs_thumbnail = $va_images[$vn_id];
-				}else{
-					$vs_thumbnail = $vs_default_placeholder;
-				}
-				$vs_rep_detail_link 	= caDetailLink($this->request, $vs_thumbnail, '', $vs_table, $vn_id);			
 			}
-			$vs_detail_button_link = caDetailLink($this->request, "<i class='bi bi-arrow-right-square'></i>", 'link-dark mx-1', $vs_table, $vn_id, null, array("title" => _t("View Record"), "aria-label" => _t("View Record")));
+				
+			if(!$image){
+				$t_list_item->load($qr_results->get("type_id"));
+				$typecode = $t_list_item->get("idno");
+				if($type_placeholder = caGetPlaceholder($typecode, "placeholder_media_icon")){
+					$image = $type_placeholder;
+				}else{
+					$image = $default_placeholder;
+				}
+			}
+			$rep_detail_link 	= caDetailLink($this->request, $image, '', $table, $id);
+			$detail_button_link = caDetailLink($this->request, "<i class='bi bi-arrow-right-square'></i>", 'link-dark mx-1', $table, $id, null, array("title" => _t("View Record"), "aria-label" => _t("View Record")));
 			print "
 		<div class='col-md-4 col-lg-3 d-flex'>
-			<div id='row{$vn_id}' class='card flex-grow-1 width-100 rounded-0 shadow border-0 mb-4'>
-			  {$vs_rep_detail_link}
+			<div id='row{$id}' class='card flex-grow-1 width-100 rounded-0 shadow border-0 mb-4'>
+			  {$rep_detail_link}
 				<div class='card-body'>
-					{$vs_caption}
+					{$caption}
 				</div>
 				<div class='card-footer text-end bg-transparent'>
-					{$vs_detail_button_link}
+					{$detail_button_link}
 				</div>
 			 </div>	
 		</div><!-- end col -->";				
-			$vn_c++;
-			if($vn_c == $vn_num_items_to_show){
-				if($qr_results->numHits() > $vn_num_items_to_show){
-					print "<div class='row justify-content-center'><div class='col-12 col-sm-6 col-md-4 col-lg-3 mb-3 text-center'>".caNavLink($this->request, _t("Full Results")."  <i class='ps-2 bi bi-box-arrow-up-right' aria-label='link out'></i>", "pt-3 pb-4 px-2 d-flex align-items-center justify-content-center bg-dark h-100 w-100 text-white", "", "Search", $vs_block, array("search" => $vs_search))."</div></div>";
+			$c++;
+			if($c == $num_items_to_show){
+				if($qr_results->numHits() > $num_items_to_show){
+					print "<div class='row justify-content-center'><div class='col-12 col-sm-6 col-md-4 col-lg-3 mb-3 text-center'>".caNavLink($this->request, _t("Full Results")."  <i class='ps-2 bi bi-box-arrow-up-right' aria-label='link out'></i>", "pt-3 pb-4 px-2 d-flex align-items-center justify-content-center bg-dark h-100 w-100 text-white", "", "Search", $block, array("search" => $search))."</div></div>";
 				}
 				
 				break;
