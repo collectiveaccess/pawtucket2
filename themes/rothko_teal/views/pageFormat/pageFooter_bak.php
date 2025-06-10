0edef431856fc4c9ab9f51d24602b871850dd7a2
CollectiveAccess <seth@collectiveaccess.org>
2025-06-10 15:31:06 -0400

    Theme update

--- themes/rothko_teal/views/pageFormat/pageFooter_bak.php
+++ themes/rothko_teal/views/pageFormat/pageFooter_bak.php
@@ -28,7 +28,14 @@
 ?>
 		<div style="clear:both; height:1px;"><!-- empty --></div>
 
-
+<?php
+	//
+	// Output HTML for debug bar
+	//
+	if(Debug::isEnabled()) {
+		print Debug::$bar->getJavascriptRenderer()->render();
+	}
+?>
 	</div><!-- end pageArea --></div><!-- end col --></div><!-- end row --></div><!-- end container -->
 	<div id="footer">
 		<div class="container">
