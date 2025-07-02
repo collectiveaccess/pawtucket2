00df54aec01e7614cc62a91bcddf1ae64f605332
Maria <maria@whirl-i-gig.com>
2025-07-02 18:40:45 +0000

    sva collection access check, tfana finding aid show all objects

--- themes/tfana/printTemplates/summary/ca_collections_summary.php
+++ themes/tfana/printTemplates/summary/ca_collections_summary.php
@@ -139,7 +139,7 @@
 	if ($t_item->get("ca_collections.children.collection_id") || $t_item->get("ca_objects.object_id")){
 		print "<hr/><br/>Collection Contents";
 		if ($t_item->get('ca_collections.collection_id')) {
-			print caGetCollectionLevelSummary($this->request, array($t_item->get('ca_collections.collection_id')), 1);
+			print caGetCollectionLevelSummaryTFANA($this->request, array($t_item->get('ca_collections.collection_id')), 1);
 		}
 	}
 	print $this->render("pdfEnd.php");
