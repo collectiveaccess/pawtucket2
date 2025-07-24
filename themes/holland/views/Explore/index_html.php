cf6f233b31173554a297633f3e31ab2ca5c55110
Maria <maria@whirl-i-gig.com>
2025-07-24 20:37:21 +0000

    holland make page load faster in explore

--- themes/holland/views/Explore/index_html.php
+++ themes/holland/views/Explore/index_html.php
@@ -40,34 +40,36 @@
 					}
 				}
 			}
-			if(!$vs_img){
-				$vs_img = "<div class='explorePlaceholder'><i class='fa fa-picture-o fa-2x'></i></div>";
-			}
-			# --- if the category has children, link to explore section with that id so can show children
-			if($qr_categories->get("ca_list_items.children.item_id")){
-				$vs_tmp = "<div class='col-sm-2'><div class='exploreCategoryContainer'>".
-							"<H2>".caNavLink($this->request, $qr_categories->get("ca_list_items.preferred_labels.name_plural"), "", "", "Explore", "index", array("item_id" => $qr_categories->get("ca_list_items.item_id")))."</H2>".
-							"<div class='exploreImgContainer'>".caNavLink($this->request, $vs_img, "", "", "Explore", "index", array("item_id" => $qr_categories->get("ca_list_items.item_id")))."</div>
-							<p class='text-center'>".caNavLink($this->request, "View Items", "btn-default btn-sm", "", "Explore", "index", array("item_id" => $qr_categories->get("ca_list_items.item_id")))."</p>
-							</div></div>";
-			}else{
-				if($vb_subcategory || ($vn_root_id == $qr_categories->get("ca_list_items.parent_id"))){
+			#if(!$vs_img){
+			#	$vs_img = "<div class='explorePlaceholder'><i class='fa fa-picture-o fa-2x'></i></div>";
+			#}
+			if($vs_img){
+				# --- if the category has children, link to explore section with that id so can show children
+				if($qr_categories->get("ca_list_items.children.item_id")){
 					$vs_tmp = "<div class='col-sm-2'><div class='exploreCategoryContainer'>".
-								"<H2>".caNavLink($this->request, $qr_categories->get("ca_list_items.preferred_labels.name_plural"), "", "", "browse", "objects", array("facet" => "category_facet", "id" => $qr_categories->get("ca_list_items.item_id")))."</H2>".
-								"<div class='exploreImgContainer'>".caNavLink($this->request, $vs_img, "", "", "browse", "objects", array("facet" => "category_facet", "id" => $qr_categories->get("ca_list_items.item_id")))."</div>
-								<p class='text-center'>".caNavLink($this->request, "View Items", "btn-default btn-sm", "", "browse", "objects", array("facet" => "category_facet", "id" => $qr_categories->get("ca_list_items.item_id")))."</p>
+								"<H2>".caNavLink($this->request, $qr_categories->get("ca_list_items.preferred_labels.name_plural"), "", "", "Explore", "index", array("item_id" => $qr_categories->get("ca_list_items.item_id")))."</H2>".
+								"<div class='exploreImgContainer'>".caNavLink($this->request, $vs_img, "", "", "Explore", "index", array("item_id" => $qr_categories->get("ca_list_items.item_id")))."</div>
+								<p class='text-center'>".caNavLink($this->request, "View Items", "btn-default btn-sm", "", "Explore", "index", array("item_id" => $qr_categories->get("ca_list_items.item_id")))."</p>
 								</div></div>";
+				}else{
+					if($vb_subcategory || ($vn_root_id == $qr_categories->get("ca_list_items.parent_id"))){
+						$vs_tmp = "<div class='col-sm-2'><div class='exploreCategoryContainer'>".
+									"<H2>".caNavLink($this->request, $qr_categories->get("ca_list_items.preferred_labels.name_plural"), "", "", "browse", "objects", array("facet" => "category_facet", "id" => $qr_categories->get("ca_list_items.item_id")))."</H2>".
+									"<div class='exploreImgContainer'>".caNavLink($this->request, $vs_img, "", "", "browse", "objects", array("facet" => "category_facet", "id" => $qr_categories->get("ca_list_items.item_id")))."</div>
+									<p class='text-center'>".caNavLink($this->request, "View Items", "btn-default btn-sm", "", "browse", "objects", array("facet" => "category_facet", "id" => $qr_categories->get("ca_list_items.item_id")))."</p>
+									</div></div>";
+					}
 				}
-			}
-			if($vs_tmp){
-				if($vn_i == 0){
-					print "<div class='row'>";
-				}
-				print $vs_tmp;
-				$vn_i++;
-				if($vn_i == $vn_cols){
-					$vn_i = 0;
-					print "</div><!-- end row -->";
+				if($vs_tmp){
+					if($vn_i == 0){
+						print "<div class='row'>";
+					}
+					print $vs_tmp;
+					$vn_i++;
+					if($vn_i == $vn_cols){
+						$vn_i = 0;
+						print "</div><!-- end row -->";
+					}
 				}
 			}
 		}
