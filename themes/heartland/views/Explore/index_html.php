b38f3d55527a01e6ba0abdaf67885b962379081f
Seth <seth@whirl-i-gig.com>
2025-09-19 03:54:22 +0000

    Update explore page

--- themes/heartland/views/Explore/index_html.php
+++ themes/heartland/views/Explore/index_html.php
@@ -16,10 +16,12 @@
 	$i = 1;
 	foreach($va_types as $va_type){
 		$va_type = $va_type[1];
-		print "<div class='col-sm-2".(($i == 1) ? " col-sm-offset-1" : "")."'>";
-		print caNavLink($this->request, "<div class='exploreType'>".$va_type_icons[$va_type["idno"]]."<br/>".$va_type["name_plural"]."</div>", "", "", "Explore", "type", array("type_id" => $va_type["item_id"]));
-		print "</div>";
-		$i++;
+		if($va_type["idno"] != "audio_commentary"){
+			print "<div class='col-sm-2".(($i == 1) ? " col-sm-offset-1" : "")."'>";
+			print caNavLink($this->request, "<div class='exploreType'>".$va_type_icons[$va_type["idno"]]."<br/>".$va_type["name_plural"]."</div>", "", "", "Explore", "type", array("type_id" => $va_type["item_id"]));
+			print "</div>";
+			$i++;
+		}
 	}
 ?>
 </div>
@@ -37,34 +39,36 @@
 	foreach($va_types as $va_type){
 		$va_type = $va_type[1];
 		$vs_buf = "";
-		if($va_type["idno"] == "interactive"){
-			$vs_buf = "<div class='col-sm-12 col-md-6 col-md-offset-3'>";
-		}else{
-			$vs_buf = "<div class='col-sm-12 col-md-6'>";
-		}
-		$vs_buf .= "<div class='exploreBrowse'>
-						<div class='row'>
-							<div class='col-sm-6'>".caNavLink($this->request, caGetThemeGraphic($this->request, 'explore_'.$va_type["idno"].'.jpg', array("alt" => "Explore ".$va_type["name_plural"])), "", "", "Browse", "objects", array("facet" => "type_facet", "id" => $va_type["item_id"]))."</div>
-							<div class='col-sm-6'>".caNavLink($this->request, $va_type["name_plural"], "exploreBrowseTitle", "", "Browse", "objects", array("facet" => "type_facet", "id" => $va_type["item_id"]))."
-								<p>".$this->getVar("explore_browse_".$va_type["idno"])."</p>
-							</div>
-						</div>
-						<br/><div class='text-center'>".caNavLink($this->request, "Browse ".$va_type["name_plural"], "btn btn-default", "", "Browse", "objects", array("facet" => "type_facet", "id" => $va_type["item_id"]))."</div>
-						</div>
-					</div>";	
-
-		if($va_type["idno"] == "interactive"){
-			# --- put interactive resource at the bottom - centered
-			$vs_interactive = $vs_buf;
-		}else{
-			if($i == 0){
-				print "<div class='row'>";
+		if($va_type["idno"] != "audio_commentary"){
+			if($va_type["idno"] == "interactive"){
+				$vs_buf = "<div class='col-sm-12 col-md-6 col-md-offset-3'>";
+			}else{
+				$vs_buf = "<div class='col-sm-12 col-md-6'>";
 			}
-			print $vs_buf;
-			$i++;
-			if($i == 2){
-				print "</div>";
-				$i = 0;
+			$vs_buf .= "<div class='exploreBrowse'>
+							<div class='row'>
+								<div class='col-sm-6'>".caNavLink($this->request, caGetThemeGraphic($this->request, 'explore_'.$va_type["idno"].'.jpg', array("alt" => "Explore ".$va_type["name_plural"])), "", "", "Browse", "objects", array("facet" => "type_facet", "id" => $va_type["item_id"]))."</div>
+								<div class='col-sm-6'>".caNavLink($this->request, $va_type["name_plural"], "exploreBrowseTitle", "", "Browse", "objects", array("facet" => "type_facet", "id" => $va_type["item_id"]))."
+									<p>".$this->getVar("explore_browse_".$va_type["idno"])."</p>
+								</div>
+							</div>
+							<br/><div class='text-center'>".caNavLink($this->request, "Browse ".$va_type["name_plural"], "btn btn-default", "", "Browse", "objects", array("facet" => "type_facet", "id" => $va_type["item_id"]))."</div>
+							</div>
+						</div>";	
+	
+			if($va_type["idno"] == "interactive"){
+				# --- put interactive resource at the bottom - centered
+				$vs_interactive = $vs_buf;
+			}else{
+				if($i == 0){
+					print "<div class='row'>";
+				}
+				print $vs_buf;
+				$i++;
+				if($i == 2){
+					print "</div>";
+					$i = 0;
+				}
 			}
 		}
 	}
