e0a0bf02198ca2a7c1e42717d6a99e2a91732178
Maria <maria@whirl-i-gig.com>
2025-10-28 13:29:30 -0400

    sept11mm frontpage and detail page revisions

--- themes/sept11mm/views/Front/front_page_html.php
+++ themes/sept11mm/views/Front/front_page_html.php
@@ -38,103 +38,102 @@
  		$vs_hero = rand(1, 3);
 	}
 ?>
-
 <div class="container-flex">
-	<div class="parallax hero<?php print $vs_hero; ?>">
-		<div class="container h-75">
-			<div class="row justify-content-center h-100">
-				<div class="col-md-9 col-lg-6 col-xl-5 d-flex h-100 align-items-center">
-					<div class="bg-white p-5 text-center shadow w-100">
-						<div class="pb-3">
-							<div class="display-4 fw-medium">Inside the Collection</div>
-						</div>
-						<div class="fs-4 pt-1">{{{hp_search_text}}}</div>
-						<form role="search" action="<?= caNavUrl($this->request, '', 'Search', 'GeneralSearch'); ?>">
-							<div class="input-group pb-3">
-								<label for="heroSearchInput" class="form-label visually-hidden">Search</label>
-								<input name="search" type="text" class="form-control rounded-0 border-black" id="heroSearchInput" placeholder="Search" aria-label="Search Bar">
-								<button type="submit" class="btn bg-primary text-white ms-1" id="heroSearchButton" aria-label="Search button"><i class="bi bi-search"></i></button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+	<div class="h-100 hpHero hpHero<?php print $vs_hero; ?> py-5">
+		<div class="container py-5">
+			<div class="row">
+				<div class="col-sm-12 col-lg-6"><div class="bg-white pt-4 pb-5 px-5 mb-5 mb-lg-0">
 <?php
 	$vs_hp_intro_title = $this->getVar("hp_intro_title");
 	$vs_hp_intro = $this->getVar("hp_intro");
 	if($vs_hp_intro_title || $vs_hp_intro){
-?>
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-8 mt-5 pt-5 text-left">
-<?php
-					if($vs_hp_intro_title){
-						print "<div class='display-1 lh-base border-bottom border-black pb-4 mb-4'>".$vs_hp_intro_title."</div>";
-					}
-					if($vs_hp_intro){
-						print "<div class='display-6 lh-base pt-2'>".$vs_hp_intro."</div>";
-					}
-?>		
-				</div>
-			</div>
-		</div>
-<?php
+		if($vs_hp_intro_title){
+			print "<div class='display-3 lh-base border-bottom border-black pb-2 mb-2'>".$vs_hp_intro_title."</div>";
+		}
+		if($vs_hp_intro){
+			print "<div class='fs-4 playfair-display lh-base pt-2'>".$vs_hp_intro."</div>";
+		}
 	}
 ?>
+					
+				</div></div>
+				<div class="col-lg-1"></div>
+				<div class="col-sm-12 col-lg-4">
+				 	<div class="pb-3 collectionLink">
+				 		<?php print caNavLink($this->request, _t("Object Collection")." <i class='fs-5 bi bi-chevron-right'></i>", "py-3 py-lg-0 fs-4 btn btn-primary d-flex h-100 align-items-center justify-content-center", "", "Browse", "objects"); ?>
+				 	</div>
+				 	<div class="pb-3 collectionLink">
+				 		<?php print caNavLink($this->request, _t("Oral History Collection")." <i class='fs-5 bi bi-chevron-right'></i>", "py-3 py-lg-0 fs-4 btn btn-primary d-flex h-100 align-items-center justify-content-center", "", "Browse", "oral_histories"); ?>
+				 	</div>
+				 	<div class="pb-3 collectionLink">
+				 		<?php print caNavLink($this->request, _t("World Trade Center Site Memorial Competition Collection")." <i class='fs-5 bi bi-chevron-right'></i>", "py-3 py-lg-0 fs-4 lh-sm btn btn-primary d-flex h-100 align-items-center justify-content-center", "", "Browse", "boards"); ?>
+					</div>
+				 	<div class="collectionLink">
+						<?php print caNavLink($this->request, _t("Featured Galleries")." <i class='fs-5 bi bi-chevron-right'></i>", "py-3 py-lg-0 fs-4 btn btn-primary d-flex h-100 align-items-center justify-content-center", "", "Gallery", "Index"); ?>
+					</div>
+				</div>
+				<div class="col-1"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-md-8 mt-5 pt-5 text-left">
-			<div class="img-fluid pb-2">
-				<?php print caGetThemeGraphic($this->request, "hp_objects.jpg", array("alt" => "Image of evidence tag, hat and badge")); ?>
-			</div>
-			<div class="small pb-4 fw-bold fs-6"><div class="float-end ps-3">Photo by Matt Flynn</div>Collection 9/11 Memorial Museum, Gift of Retired Port Authority of NY&NJ Police Officer Sharon A. Miller.</div>
-			<H2 class="pt-2">Archival Objects</H2>
-			<div class="fs-5 pb-2">
-				{{{hp_objects}}}
-			</div>
-			<div class="mb-5 mt-2">
-				<?php print caNavLink($this->request, "Browse Archival Objects", "btn btn-primary", "", "Browse", "objects"); ?>
+			<div class="px-lg-4">
+				<div class="img-fluid pb-2">
+					<?php print caGetThemeGraphic($this->request, "hp_conservation.jpg", array("alt" => "Image of evidence tag, hat and badge")); ?>
+				</div>
+				<H2 class="pt-4">Conservation</H2>
+				<div class="fs-5 pb-2">
+					{{{hp_conservation}}}
+				</div>
+				<div class="mb-5 mt-2">
+					<a href="https://911memorial.org/visit/museum/collection/conservation" class="btn btn-primary">Learn more</a>
+				</div>
 			</div>
 		</div>
 	</div>
 	<div class="row justify-content-center">
 		<div class="col-md-8 mt-5 pt-5 text-left">
-			<div class="img-fluid pb-2">
-				<?php print caGetThemeGraphic($this->request, "hp_OralHistories.jpg", array("alt" => "Image of oral history being recorded")); ?>
-			</div>
-			<div class="small pb-4 fw-bold fs-6 text-end">Photo by Jin S. Lee</div>
-			<H2 class="pt-2">Oral Histories</H2>
-			<div class="fs-5 pb-2">
-				{{{hp_oral_histories}}}
-			</div>
-			<div class="mb-5 mt-2">
-				<?php print caNavLink($this->request, "Browse Oral Histories", "btn btn-primary", "", "Browse", "oral_histories"); ?>
+			<div class="px-lg-4">
+				<div class="img-fluid pb-2">
+					<?php print caGetThemeGraphic($this->request, "hp_donate.jpg", array("alt" => "Image of oral history being recorded")); ?>
+				</div>
+				<div class="row">
+					<div class="col-sm-8 small"><div class="small pb-4 fw-bold fs-6">Collection 9/11 Memorial Museum, Gift of Anthony and Maryann Gambale, in Memory of Giovanna Gambale. <?php print caDetailLink($this->request, "Learn more about this item", "", "ca_objects", 8618); ?>.</div></div>
+					<div class="col-sm-4"><div class="small pb-4 fw-bold fs-6 text-end text-uppercase">Photo by Michael Hnatov</div></div>
+				</div>
+				<H2 class="pt-4">Give to the Collection</H2>
+				<div class="fs-5 pb-2">
+					{{{hp_donate}}}
+				</div>
+				<div class="mb-5 mt-2">
+					<a href="https://911memorial.org/support/donate/contribute-collection" class="btn btn-primary">Learn more</a>
+				</div>
 			</div>
 		</div>
 	</div>	
 	<div class="row justify-content-center">
 		<div class="col-md-8 mt-5 pt-5 text-left">
-			<div class="img-fluid pb-2">
-				<?php print caGetThemeGraphic($this->request, "hp_boards.jpg", array("alt" => "Image of LMDC Board")); ?>
-			</div>
-			<div class="small pb-4 fw-bold fs-6">"Reflecting Absence" by Michael Arad</div>
-			<H2 class="pt-2">LMDC Boards</H2>
-			<div class="fs-5 pb-2">
-				{{{hp_boards}}}
-			</div>
-			<div class="mb-5 mt-2">
-				<?php print caNavLink($this->request, "Browse LMDC Boards", "btn btn-primary", "", "Browse", "boards"); ?>
+			<div class="px-lg-4">
+				<div class="img-fluid pb-2">
+					<?php print caGetThemeGraphic($this->request, "hp_loans.jpg", array("alt" => "Image of LMDC Board")); ?>
+				</div>
+				<div class="small pb-4 fw-bold fs-6">Quilts from the collection on view at the National Quilt Museum in Paducah, KY.</div>
+				<H2 class="pt-4">Outgoing Loans</H2>
+				<div class="fs-5 pb-2">
+					{{{hp_loans}}}
+				</div>
+				<div class="mb-5 mt-2">
+					<a href="https://911memorial.org/visit/museum/outgoing-loans" class="btn btn-primary">Learn more</a>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
-<?php
-	# --- display galleries as a grid?
-	print $this->render("Front/gallery_grid_html.php");
-?>
+
 <div class="container-flex">
 	<div class="fade-out bg-black bg-opacity-25 text-bg-dark p-3 text-center shadow w-100 fixed-bottom display-4"><i class="bi bi-chevron-down"></i></div>
 </div>
