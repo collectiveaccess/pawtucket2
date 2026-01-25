b8ed2e0952c8eb2b1d0587b391de563f21673e48
CollectiveAccess <seth@collectiveaccess.org>
2026-01-24 22:58:35 -0500

    Add additional fields

--- app/helpers/themeHelpers.php
+++ app/helpers/themeHelpers.php
@@ -1614,6 +1614,18 @@ function caGetAnalyticsIntegrationValueList() : array {
 			'tooltip' => _t('Content to be added to the &lt;head&gt; section on all Pawtucket pages. Your analytics provider should have provided this content.'),
 			'width' => '670px',
 			'height' => '200px'
+		],
+		'bodyStart' => [
+			'name' => _t('Analytics code (start of &lt;body&gt;)'),
+			'tooltip' => _t('Content to be added to the end of the &lt;body&gt; section on all Pawtucket pages. Your analytics provider should have provided this content.'),
+			'width' => '670px',
+			'height' => '200px'
+		],
+		'bodyEnd' => [
+			'name' => _t('Analytics code (end of &lt;body&gt;)'),
+			'tooltip' => _t('Content to be added to the start of the &lt;body&gt; section on all Pawtucket pages. Your analytics provider should have provided this content.'),
+			'width' => '670px',
+			'height' => '200px'
 		]
 	];
 }
