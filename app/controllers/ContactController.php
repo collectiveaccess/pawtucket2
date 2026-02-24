9f8ac654480101a99849c7e0df49cf554a401d13
CollectiveAccess <seth@collectiveaccess.org>
2026-02-24 17:32:46 -0500

    Pass form values to subject line template; code formatting

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2013-2025 Whirl-i-Gig
+ * Copyright 2013-2026 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -30,15 +30,15 @@ require_once(__CA_LIB_DIR__.'/pawtucket/BasePawtucketController.php');
 
 class ContactController extends BasePawtucketController {
 	# -------------------------------------------------------
-	public function __construct(&$po_request, &$po_response, $pa_view_paths=null) {
-		parent::__construct($po_request, $po_response, $pa_view_paths);
+	public function __construct(&$po_request, &$po_response, $view_paths=null) {
+		parent::__construct($po_request, $po_response, $view_paths);
 		$this->config = caGetContactConfig();
 		if(!$this->config->get("contact_email") && !$this->config->get("contact_form_elements")){
 			$this->notification->addNotification(_t("Contact form is not configured properly"), __NOTIFICATION_TYPE_ERROR__);
 			$this->response->setRedirect(caNavUrl($this->request, '', '', ''));
 		}
 		MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").$this->request->config->get("page_title_delimiter")._t("Contact"));
-		caSetPageCSSClasses(array("contact"));
+		caSetPageCSSClasses(["contact"]);
 	}
 	# -------------------------------------------------------
 	public function Form() {
@@ -51,73 +51,76 @@ class ContactController extends BasePawtucketController {
 		caValidateCSRFToken($this->request);
 		$o_purifier = caGetHTMLPurifier();
 		# --- check for errors
-		$va_errors = array();
+		$errors = [];
 		if($this->config->get("check_security")){
-			$ps_security = $this->request->getParameter("security", pString);
-			if ((!$ps_security)) {
-				$va_errors["security"] = true;
+			$security = $this->request->getParameter("security", pString);
+			if ((!$security)) {
+				$errors["security"] = true;
 			}else{
-				if($ps_security != $_REQUEST["sum"]){
-					$va_errors["security"] = true;
+				if($security != $_REQUEST["sum"]){
+					$errors["security"] = true;
 				}
 			}
-			if($va_errors["security"]){
-				$va_errors["display_errors"]["security_error"] = _t("Please answer the security question");
+			if($errors["security"]){
+				$errors["display_errors"]["security_error"] = _t("Please answer the security question");
 			}
 		}
 		if(!$this->request->isLoggedIn() && defined("__CA_GOOGLE_RECAPTCHA_SECRET_KEY__") && __CA_GOOGLE_RECAPTCHA_SECRET_KEY__){
 			try {
 				caVerifyCaptcha($this->request->getParameter("g-recaptcha-response", pString));
 			} catch(CaptchaException $e) {
-				$va_errors["recaptcha"] = $va_errors["display_errors"]["recaptcha"] = $e->getMessage();
+				$errors["recaptcha"] = $errors["display_errors"]["recaptcha"] = $e->getMessage();
 			}
 		}
 		
 		$opts = [];
 		$from = $this->config->get('from_email') ?: $this->request->config->get("ca_admin_email");
 		
-		$va_fields = $this->config->get("contact_form_elements");
-		$this->view->setVar("contact_form_elements", $va_fields);
-		if(is_array($va_fields) && sizeof($va_fields)){
-			foreach($va_fields as $vs_element_name => $va_options){
-				$vs_element_value = $o_purifier->purify($this->request->getParameter($vs_element_name, pString, ['forcePurify' => true]));
-				if($va_options["required"] && !$vs_element_value){
-					$va_errors[$vs_element_name] = true;
-					$va_errors["display_errors"]["required_error"] = _t("Please enter the required information in the highlighted fields");
+		$fields = $this->config->get("contact_form_elements");
+		$this->view->setVar("contact_form_elements", $fields);
+		$values = [];
+		if(is_array($fields) && sizeof($fields)){
+			foreach($fields as $element_name => $options){
+				$element_value = $o_purifier->purify($this->request->getParameter($element_name, pString, ['forcePurify' => true]));
+				if($options["required"] && !$element_value){
+					$errors[$element_name] = true;
+					$errors["display_errors"]["required_error"] = _t("Please enter the required information in the highlighted fields");
 				}
-				if($va_options["email_address"] ?? null){
+				if($options["email_address"] ?? null){
 					# --- check if entered value is valid email address
-					if (!caCheckEmailAddress($vs_element_value)) {
-						$va_errors["display_errors"]["email_address_error"] = _t("Please enter a valid e-mail address");
-						$va_errors[$vs_element_name] = true;
+					if (!caCheckEmailAddress($element_value)) {
+						$errors["display_errors"]["email_address_error"] = _t("Please enter a valid e-mail address");
+						$errors[$element_name] = true;
 					}
 				}
-				if($va_options['use_as_from_address'] ?? false) {
+				if($options['use_as_from_address'] ?? false) {
 					if(!is_array($opts['replyTo'])) { $opts['replyTo'] = []; }
-					$opts['replyTo'][] = $vs_element_value;
+					$opts['replyTo'][] = $element_value;
 				}
-				$this->view->setVar($vs_element_name, $vs_element_value);
+				$values[$element_name] = $element_value;
+				$this->view->setVar($element_name, $element_value);
 			}
 		}
-		if(sizeof($va_errors) == 0){
+		if(sizeof($errors) == 0){
 			# --- send email
-				$o_view = new View($this->request, array($this->request->getViewsDirectoryPath()));
-				$o_view->setVar("contact_form_elements", $va_fields);
+				$o_view = new View($this->request, [$this->request->getViewsDirectoryPath()]);
+				$o_view->setVar("contact_form_elements", $fields);
+				$o_view->setVar("contact_form_values", $values);
 				# -- generate email subject line from template
-				$vs_subject_line = $o_view->render("mailTemplates/contact_subject.tpl");
+				$subject_line = $o_view->render("mailTemplates/contact_subject.tpl");
 					
 				# -- generate mail text from template - get both the text and the html versions
-				$vs_mail_message_text = $o_view->render("mailTemplates/contact.tpl");
-				$vs_mail_message_html = $o_view->render("mailTemplates/contact_html.tpl");
-				if(caSendmail($this->config->get("contact_email"), $from, $vs_subject_line, $vs_mail_message_text, $vs_mail_message_html, null, null, null, $opts)){
+				$mail_message_text = $o_view->render("mailTemplates/contact.tpl");
+				$mail_message_html = $o_view->render("mailTemplates/contact_html.tpl");
+				if(caSendmail($this->config->get("contact_email"), $from, $subject_line, $mail_message_text, $mail_message_html, null, null, null, $opts)){
 					$this->render("Contact/success_html.php");
 				}else{
-					$va_errors["display_errors"]["send_error"] = _t("Your email could not be sent");
-					$this->view->setVar("errors", $va_errors);
+					$errors["display_errors"]["send_error"] = _t("Your email could not be sent");
+					$this->view->setVar("errors", $errors);
 					$this->form();
 				}
 		}else{
-			$this->view->setVar("errors", $va_errors);
+			$this->view->setVar("errors", $errors);
 			$this->form();
 		}
 	}
