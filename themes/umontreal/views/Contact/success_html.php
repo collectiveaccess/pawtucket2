f8b88a0a70b838643aceb56181687dc82dcb9aad
CollectiveAccess <seth@collectiveaccess.org>
2026-03-18 09:10:11 -0400

    Remove stray themes

--- themes/umontreal/views/Contact/success_html.php
+++
@@ -1,2 +0,0 @@
-<H1><?php print _t("Contact"); ?></H1>
-<h2><?php print _t("Your message has been sent."); ?></H2>
--- themes/umontreal/views/Details/ca_representation_annotations_default_html.php
+++
@@ -1,2 +0,0 @@
-<H4>{{{ca_representation_annotations.preferred_labels.name}}}</H4>
-<h6>{{{ca_representation_annotations.startTimecode}}} - {{{ca_representation_annotations.endTimecode}}}</H6>
