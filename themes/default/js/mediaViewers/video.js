d62d52ccad7d2c65d2706a06d61465ef76e01667
seth <Seth>
2026-05-20 15:47:25 -0400

    Transcription search support

--- themes/default/js/mediaViewers/video.js
+++ themes/default/js/mediaViewers/video.js
@@ -13,7 +13,7 @@ let videoViewer = function(id, options=null) {
 		// Methods
 		//
 		//
-		//
+		// 
 		init: function(id, options=null) {
 			baseViewer(that);
 			that.id = id;
@@ -50,19 +50,22 @@ let videoViewer = function(id, options=null) {
 	
 			let poptions = {
 				debug: false,
-				autoplay: false,
+				autoplay: true,
+				muted: true,
 				fullscreen: {
-					enabled: false
+					enabled: true,
+					fallback: true
 				},
 				loop: { 
 					active: true 
 				},
-				controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings']
+				controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'fullscreen']
 			};
 			
 			
 			that[k] = new Plyr('#' + that.id + '_' + source.display_class + overlay_ext + '_plyr', poptions);
-		 
+		 	that[k].canPlay = false;
+		 	
 			that[k].source = {
 				  type: 'video',
 				  title: 'Track',
@@ -75,6 +78,46 @@ let videoViewer = function(id, options=null) {
 				  tracks: tracks,
 				  poster: source.small
 				};
+			that[k].seek_for_transcript = false;
+			if(source.start && (source.start > 0)) {
+				//that[k].on('ready', (event) => {
+				//  that.viewer.play();
+				//});
+				that[k].on('playing', (event) => {
+					if(!that[k].seek_for_transcript) {
+				  		that[k].currentTime = parseFloat(source.start);
+						that.viewer.pause();
+				  		that[k].seek_for_transcript = true;
+				  	}
+				});
+			}
+			that[k].viewerIsReady = false;
+			
+			that[k].on('ready', (event) => {
+				if(!that[k].viewerIsReady) {
+					console.log("[DEBUG] Viewer is ready");
+					that[k].viewerIsReady = true;
+				}
+			});
+			that[k].on('canplay', (event) => {
+				console.log("[DEBUG] Can play video");
+				that[k].canPlay = true;
+			});
+			
+			if(options.transcript_container_id && options.transcript_id) {
+				const el = document.getElementById(options.transcript_container_id);
+				console.log("el", el);
+				if(source.vttCaptions) {
+					const transcript_url = options.transcript_url.replace(/%25representation_id/, '' + source.id);	
+					htmx.ajax('GET', transcript_url, '#' + options.transcript_id);
+					if(el) {
+						el.style.display = 'block';
+					}
+				} else if(el) {
+					el.style.display = 'none';	
+				}
+			} 
+			
 			return that[k];
 		},
 		
@@ -84,6 +127,42 @@ let videoViewer = function(id, options=null) {
 		onShowOverlay: function() {
 			if(that.viewer) {
 				that.viewer.stop();
+			}
+		},
+		
+		//
+		//
+		//
+		seek: function(t) {
+			if(that.viewer) {
+				that.viewer.pause();
+				if(!that.viewer.canPlay || !that.viewer.viewerIsReady) {
+					that.viewer.seekTo = t;
+					that.viewer.on('playing', (event) => {
+						if(that.viewer.seekTo) {
+							that.viewer.currentTime = parseFloat(that.viewer.seekTo);
+							that.viewer.play();
+							that.viewer.seekTo = null;
+						}
+					});
+				} else {
+					console.log("[DEBUG] Set seek to", t);
+					that.viewer.seekTo = null;
+					that.viewer.currentTime = t;
+					that.viewer.play();
+				}
+			}
+		},
+		
+		play: function(t) {
+			if(that.viewer) {
+				that.viewer.play();
+			}
+		},
+		
+		pause: function(t) {
+			if(that.viewer) {
+				that.viewer.pause();
 			}
 		},
 		
