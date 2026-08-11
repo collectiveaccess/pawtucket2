2592ff09e10d0a6f566c2458fadd3ea4bbf8f3fa
Seth <seth@whirl-i-gig.com>
2026-08-11 20:52:32 +0000

    Update theme

--- themes/openspace/views/Front/front_page_html.php
+++ themes/openspace/views/Front/front_page_html.php
@@ -82,35 +82,7 @@ if(!$hero){
 <?php
 	}
 
-	# --- display galleries as a grid?
-	print $this->render("Front/gallery_grid_html.php");
-
-	# --- display galleries as a slideshow?
-	#TODO: this is broken
-	#print $this->render("Front/gallery_slideshow_html.php");
-
-	# --- display 1 featured gallery
-	print $this->render("Front/featured_gallery_html.php");
-
 ?>
-<div class="container">
-	<div class="row justify-content-center text-center">
-		<div class="col-md-10 hpExplore my-5 py-5">
-			<H2 class="mb-3">Explore The Archive</H2>
-			<div class="row">
-				<div class="col-md-4">
-					<?php print caNavLink($this->request, "<div>".caGetThemeGraphic($this->request, "hero_1.jpg", array("alt" => "explore image", "class" => "object-fit-cover w-100 shadow"))."<div class='fw-medium fs-4 pt-1'>Browse Objects</div></div>", "", "", "Browse", "objects"); ?>
-				</div>
-				<div class="col-md-4">
-					<?php print caNavLink($this->request, "<div>".caGetThemeGraphic($this->request, "hero_1.jpg", array("alt" => "explore image", "class" => "object-fit-cover w-100 shadow"))."<div class='fw-medium fs-4 pt-1'>Gallery</div></div>", "", "", "Gallery", "Index"); ?>
-				</div>
-				<div class="col-md-4">
-					<?php print caNavLink($this->request, "<div>".caGetThemeGraphic($this->request, "hero_1.jpg", array("alt" => "explore image", "class" => "object-fit-cover w-100 shadow"))."<div class='fw-medium fs-4 pt-1'>Collections</div></div>", "", "", "Collections", "Index"); ?>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 <?php
 	# --- display slideshow of random images
 	print $this->render("Front/featured_set_slideshow_html.php");
@@ -119,38 +91,7 @@ if(!$hero){
 	print $this->render("Front/featured_set_grid_html.php");
 
 ?>
-<div class="container">
-	<div class="row justify-content-center my-5">
-		<div class="col-10">
-			<div id="carouselExampleIndicators" class="carousel slide">
-				<div class="carousel-indicators">
-					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-				</div>
-				<div class="carousel-inner">
-					<div class="carousel-item active">
-						<?php print caGetThemeGraphic($this->request, "hero_1.jpg", array("alt" => "explore image", "class" => "d-block w-100")); ?>
-					</div>
-					<div class="carousel-item">
-						<?php print caGetThemeGraphic($this->request, "hero_1.jpg", array("alt" => "explore image", "class" => "d-block w-100")); ?>
-					</div>
-					<div class="carousel-item">
-						<?php print caGetThemeGraphic($this->request, "hero_1.jpg", array("alt" => "explore image", "class" => "d-block w-100")); ?>
-					</div>
-				</div>
-				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Previous</span>
-				</button>
-				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Next</span>
-				</button>
-			</div>
-		</div>
-	</div>
-</div>
+
 <div class="container-flex">
 	<div class="fade-out bg-black bg-opacity-25 text-bg-dark p-3 text-center shadow w-100 fixed-bottom display-4"><i class="bi bi-chevron-down"></i></div>
 </div>
