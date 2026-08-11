e5665e1a3042c7a65f477c47732cf55d658eaa58
Seth <seth@whirl-i-gig.com>
2026-08-11 15:07:27 +0000

    Add information service helpers

--- app/helpers/preload.php
+++ app/helpers/preload.php
@@ -95,6 +95,7 @@ require_once(__CA_APP_DIR__."/helpers/logHelpers.php");
 require_once(__CA_APP_DIR__."/helpers/requestHelpers.php");
 require_once(__CA_APP_DIR__."/helpers/themeHelpers.php");
 require_once(__CA_APP_DIR__."/helpers/lightboxHelpers.php");
+require_once(__CA_APP_DIR__."/helpers/informationServiceHelpers.php");
 require_once(__CA_APP_DIR__."/helpers/initializeLocale.php");
 
 if (isset($_COOKIE['CA_'.__CA_APP_NAME__.'_ui_locale'])) {
