7c52f0aad7fbcf2b329759d11a944cf4e9798789
Maria Passarotti <maria@whirl-i-gig.com>
2026-09-08 14:59:58 +0000

    whha map option

--- themes/whha/views/Browse/browse_refine_subview_html.php
+++ themes/whha/views/Browse/browse_refine_subview_html.php
@@ -70,6 +70,14 @@ if((is_array($va_facets) && sizeof($va_facets)) || ($vs_criteria) || ($qr_res->n
 		</form>
 <?php
 	}
+	print "<H2 class='fs-4 px-3 pt-2'>"._t("Show")."</H2>";		
+?>
+	<div class="btn-group px-3 mb-2">
+  		<a href="#" aria-current="page" class="btn btn-white p-1 me-1 fst-normal active">All</a>
+  		<a href="#" class="btn btn-white p-1 me-1 fst-normal">Birth</a>
+  		<a href="#" class="btn btn-white p-1 fst-normal">Burial</a>
+	</div>
+<?php	
 	if((is_array($va_facets) && sizeof($va_facets)) || ($vs_criteria)){
 		print "<H2 class='fs-4 px-3 py-2'>"._t("Filter by")."</H2>";
 		
