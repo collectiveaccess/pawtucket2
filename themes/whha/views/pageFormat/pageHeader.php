12076d490e149ab4c34508c51d36089be7d27dad
Maria Passarotti <maria@whirl-i-gig.com>
2026-09-16 22:13:48 +0000

    whha GA4 tracking tag

--- themes/whha/views/pageFormat/pageHeader.php
+++ themes/whha/views/pageFormat/pageHeader.php
@@ -53,6 +53,15 @@ if($this->request->isLoggedIn()){
 ?><!DOCTYPE html>
 <html lang="en" class="h-100">
 	<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SQW9LDGRCF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SQW9LDGRCF');
+</script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 	<?= MetaTagManager::getHTML(); ?>
